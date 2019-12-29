36d67da3c59b8c3d1333e51066902a2d3267e1e4
Rockie_Yuen <Rockie_Yuen@outlook.com>
2019-12-29 23:14:12 +0800

    ch

--- script_test/run_sql/run_sql_test.py
+++ script_test/run_sql/run_sql_test.py
@@ -8,12 +8,15 @@ conn = pymysql.Connect(host='127.0.0.1', user='root', password='123456')
 cursor = conn.cursor()
 
 
-def run_sql(param_dic):
+# format path Todo
+
+
+def run_sql(sql_param):
     with open(r'.\rs_test_sql.sql', 'r',
               encoding='utf8') as fr:
         for sql in fr.read().split(';'):
             if len(sql) > 10:
-                sql = (sql + ';').replace('/n', '').format_map(param_dic)
+                sql = (sql + ';').replace('/n', '').format_map(sql_param)
                 print(sql)
                 cursor.execute(sql)
 
@@ -27,20 +30,22 @@ def format_param_dict(param_lst):
     end_date = (date.today() + timedelta(days=-1)).strftime('%Y-%m-%d')
 
     if len(param_lst) > 1:
-        param_dic = eval(param_lst[1])
-        print(type(param_dic), '/n', param_dic)
-        param_dic['start_date'] = param_dic['start_date'] if param_dic.get('start_date') else '2000-01-01'
-        param_dic['end_date'] = param_dic['end_date'] if param_dic.get('end_date') else end_date
-        param_dic['platform_id'] = param_dic['platform_id'] if param_dic.get('platform_id') else str(platform_id)[:-2] + ')'
+        param = eval(param_lst[1])
+        print(type(param), '/n', param)
+        param['start_date'] = param['start_date'] if param.get('start_date') else '2000-01-01'
+        param['end_date'] = param['end_date'] if param.get('end_date') else end_date
+        param['platform_id'] = param['platform_id'] if param.get('platform_id') else str(platform_id)[
+                                                                                     :-2] + ')'
     else:
-        param_dic = {'start_date': '2019-01-01',
-                     'end_date': end_date,
-                     'platform_id': str(platform_id)[:-2] + ')'
-                     }
-    return param_dic
+        param = {'start_date': '2019-01-01',
+                 'end_date': end_date,
+                 'platform_id': str(platform_id)[:-2] + ')'
+                 }
+    return param
 
 
 if __name__ == '__main__':
+    print(sys.argv)
     param_dic = format_param_dict(sys.argv)
     print(param_dic)
     run_sql(param_dic)
