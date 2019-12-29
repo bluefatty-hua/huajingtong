589a442affc773ea1242006226ceefb042a39bb0
Rockie_Yuen <Rockie_Yuen@outlook.com>
2019-12-29 21:55:57 +0800

    add script_test

---
+++ script_test/run_sql/rs_test_sql.sql
@@ -0,0 +1,17 @@
+Drop TABLE IF EXISTS warehouse.rs_test_tmp0_20191229;
+CREATE TABLE warehouse.rs_test_tmp0_20191229 AS
+SELECT *
+-- 以
+FROM warehouse.platform
+WHERE pay_from BETWEEN {start_date} AND {end_date}
+  AND ID IN {platform_id}
+LIMIT 1
+;
+
+Drop TABLE IF EXISTS warehouse.rs_test_tmp1_20191229;
+CREATE TABLE warehouse.rs_test_tmp1_20191229 AS
+SELECT *
+FROM warehouse.platform
+WHERE ID IN {platform_id}
+LIMIT 1
+;
---
+++ script_test/run_sql/run_sql_test.py
@@ -0,0 +1,46 @@
+import pymysql
+import sys
+from datetime import date
+from datetime import date
+from datetime import timedelta
+
+conn = pymysql.Connect(host='127.0.0.1', user='root', password='123456')
+cursor = conn.cursor()
+
+
+def run_sql(param_dic):
+    with open(r'.\rs_test_sql.sql', 'r',
+              encoding='utf8') as fr:
+        for sql in fr.read().split(';'):
+            if len(sql) > 10:
+                sql = (sql + ';').replace('/n', '').format_map(param_dic)
+                print(sql)
+                cursor.execute(sql)
+
+
+def format_param_dict(param_lst):
+    # 获取所有平台ID
+    cursor.execute('select id from warehouse.platform;')
+    platform_id = tuple([id for id in [list(t)[0] for t in cursor.fetchall()]])
+
+    # 设置默认终止日期 前一天
+    end_date = (date.today() + timedelta(days=-1)).strftime('%Y-%m-%d')
+
+    if len(param_lst) > 1:
+        param_dic = eval(param_lst[1])
+        print(type(param_dic), '/n', param_dic)
+        param_dic['start_date'] = param_dic['start_date'] if param_dic.get('start_date') else '2000-01-01'
+        param_dic['end_date'] = param_dic['end_date'] if param_dic.get('end_date') else end_date
+        param_dic['platform_id'] = param_dic['platform_id'] if param_dic.get('platform_id') else str(platform_id)[:-2] + ')'
+    else:
+        param_dic = {'start_date': '2019-01-01',
+                     'end_date': end_date,
+                     'platform_id': str(platform_id)[:-2] + ')'
+                     }
+    return param_dic
+
+
+if __name__ == '__main__':
+    param_dic = format_param_dict(sys.argv)
+    print(param_dic)
+    run_sql(param_dic)
