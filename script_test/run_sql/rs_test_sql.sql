9f459597147bbfadfc6b2559b6354859186b6c5e
Rockie_Yuen <Rockie_Yuen@outlook.com>
2019-12-30 14:51:57 +0800

    up script

--- script_test/run_sql/rs_test_sql.sql
+++ script_test/run_sql/rs_test_sql.sql
@@ -8,8 +8,8 @@ WHERE pay_from BETWEEN {start_date} AND {end_date}
 LIMIT 1
 ;
 
-Drop TABLE IF EXISTS warehouse.rs_test_tmp1_20191229;
-CREATE TABLE warehouse.rs_test_tmp1_20191229 AS
+Drop TABLE IF EXISTS stage.rs_test_tmp1_20191229;
+CREATE TABLE stage.rs_test_tmp1_20191229 AS
 SELECT *
 FROM warehouse.platform
 WHERE ID IN {platform_id}
