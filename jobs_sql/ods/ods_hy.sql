ae8562ac1cee726340b78e73d5293e1283afd155
Rockie_Yuen <rockie_yuen@outlook.com>
2020-01-02 19:09:02 +0800

    add ods_hy dt

--- jobs_sql/ods/ods_hy.sql
+++ jobs_sql/ods/ods_hy.sql
@@ -1,46 +1,3 @@
-DROP TABLE IF EXISTS stage.hy_anchor_list_recent_day;
-CREATE TABLE stage.hy_anchor_list_recent_day AS
-SELECT al.*
-FROM (SELECT channel_id,
-			 uid, 
-             yy_id, 
-			 MAX(dt) AS recent_date 
-	  FROM spider_huya_backend.anchor_list
-	  GROUP BY channel_id,
-               uid, 
-               yy_id) alrec
-LEFT JOIN spider_huya_backend.anchor_list al ON alrec.channel_id = al.channel_id AND alrec.uid = al.uid AND alrec.yy_id = al.yy_id AND alrec.recent_date = al.dt
-;
-
-
-DROP TABLE IF EXISTS stage.hy_profile_detail_rencent_day;
-CREATE TABLE stage.hy_profile_detail_rencent_day AS 
-SELECT pd.*
-FROM (SELECT channel_id,
-	         l_uid, l_yy,
-             MAX(dt) AS recent_date 
-      FROM spider_huya_backend.profile_detail
-      GROUP BY channel_id,
-               l_uid,
-               l_yy) pdrec
-LEFT JOIN spider_huya_backend.profile_detail pd ON pdrec.channel_id  = pd.channel_id AND pdrec.l_uid = pd.l_uid AND pdrec.l_yy = pd.l_yy AND pdrec.recent_date = pd.dt
-;
-
-
-DROP TABLE IF EXISTS stage.hy_channel_list_recent_day;
-CREATE TABLE stage.hy_channel_list_recent_day AS 
-SELECT cl.*
-FROM (SELECT account_id,
-			 channel_id,
-			 channel_number,
-             MAX(dt) AS recent_date
-	  FROM spider_huya_backend.channel_list
-	  GROUP BY account_id, 
-			   channel_id, 
-               channel_number) clrec
-LEFT JOIN spider_huya_backend.channel_list cl ON clrec.account_id = cl.account_id AND clrec.channel_id = cl.channel_id AND clrec.channel_number = cl.channel_number AND clrec.recent_date = cl.dt
-;
-
 -- 主播信息
 DROP TABLE IF EXISTS warehouse.ods_anchor_hy_info;
 CREATE TABLE warehouse.ods_anchor_hy_info AS
@@ -49,7 +6,7 @@ SELECT 1002 AS platform_id,
        cl.account_id AS backend_account_id,
        cl.channel_number AS guild_id,
        cl.name AS guild_name,
-       al.channel_id,
+       pd.channel_id,
        al.uid AS anchor_uid,
        al.yy_id AS anchor_no,
        al.nick AS anchor_nick_name,
@@ -65,15 +22,16 @@ SELECT 1002 AS platform_id,
        al.avatar AS logo,
        al.dt,
        al.timestamp
--- SELECT *
-FROM stage.hy_anchor_list_recent_day al
-LEFT JOIN stage.hy_profile_detail_rencent_day pd ON al.channel_id = pd.channel_id AND al.uid = pd.l_uid
-LEFT JOIN stage.hy_channel_list_recent_day cl ON al.channel_id = cl.channel_id
--- WHERE al.yy_id = 222433
+FROM spider_huya_backend.anchor_list al
+LEFT JOIN spider_huya_backend.profile_detail pd ON al.channel_id = pd.channel_id AND al.uid = pd.l_uid AND al.dt = pd.dt
+LEFT JOIN spider_huya_backend.channel_list cl ON al.channel_id = cl.channel_id AND al.dt = cl.dt
+WHERE ai.dt BETWEEN '{start_date}' AND '{end_date}'
 ;
 
 
 -- 主播直播和直播收入
+DROP TABLE IF EXISTS warehouse.ods_anchor_hy_live_amt;
+CREATE TABLE warehouse.ods_anchor_hy_live_amt AS
 SELECT ai.platform_id,
        ai.platform_name,
        ai.backend_account_id,
@@ -93,7 +51,7 @@ SELECT ai.platform_id,
 	   pdl.date AS dt,
        pdl.timestamp
 FROM warehouse.ods_anchor_hy_info ai
-LEFT JOIN spider_huya_backend.profile_daily_live_detail pdl ON ai.channel_id = pdl.channel_id AND ai.anchor_uid = pdl.uid
-WHERE pld.date BETWEEN '{start_date}' AND '{end_date}'
+LEFT JOIN spider_huya_backend.profile_daily_live_detail pdl ON ai.channel_id = pdl.channel_id AND ai.anchor_uid = pdl.uid AND ai.dt = pdl.date
+WHERE ai.dt BETWEEN '{start_date}' AND '{end_date}'
 ;
 
