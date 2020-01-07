e587f1bd2f9a2d453c8aa0c0db3382cbba57d11a
Rockie_Yuen <rockie_yuen@outlook.com>
2020-01-07 09:34:27 +0800

    up

--- jobs_sql/ods/ods_now.sql
+++ jobs_sql/ods/ods_now.sql
@@ -1,6 +1,8 @@
 -- 主播信息
-DROP TABLE IF EXISTS warehouse.ods_anchor_now_info;
-CREATE TABLE warehouse.ods_anchor_now_info AS
+# DROP TABLE IF EXISTS warehouse.ods_anchor_now_info;
+# CREATE TABLE warehouse.ods_anchor_now_info AS
+DELETE FROM warehouse.ods_anchor_now_info WHERE dt BETWEEN '{start_date}' AND '{end_date}';
+INSERT INTO warehouse.ods_anchor_now_info
 SELECT 1003 AS platform_id,
        'NOW' AS platform_name,
        ad.backend_account_id,
@@ -28,8 +30,10 @@ WHERE ad.dt BETWEEN '{start_date}' AND '{end_date}'
 
 
 -- 主播直播和收入
-DROP TABLE IF EXISTS warehouse.ods_anchor_now_live_amt;
-CREATE TABLE warehouse.ods_anchor_now_live_amt AS
+# DROP TABLE IF EXISTS warehouse.ods_anchor_now_live_amt;
+# CREATE TABLE warehouse.ods_anchor_now_live_amt AS
+DELETE FROM warehouse.ods_anchor_now_live_amt WHERE dt BETWEEN '{start_date}' AND '{end_date}';
+INSERT INTO warehouse.ods_anchor_now_live_amt
 SELECT ani.platform_id,
        ani.platform_name,
        ani.backend_account_id,
@@ -39,19 +43,22 @@ SELECT ani.platform_id,
        ani.anchor_nick_name,
        ani.anchor_name,
        ai.live_time AS duratiion_hours,
-       ROUND(ai.live_time * 60 * 60, 2) AS duratiion,
+       ROUND(ai.live_time * 60 * 60, 2) AS duration,
        ai.origin_money AS amt,
        ani.settle_method_code,
        ani.settle_method_text,
        DATE_FORMAT(ai.date, '%Y-%m-%d') AS dt,
        ai.timestamp
 FROM warehouse.ods_anchor_now_info ani
-LEFT JOIN spider_now_backend.anchor_income ai ON ani.backend_account_id = ai.backend_account_id AND ani.dt = DATE_FORMAT(ai.date, '%Y-%m-%d')
+LEFT JOIN spider_now_backend.anchor_income ai ON ani.dt = DATE_FORMAT(ai.date, '%Y-%m-%d') AND ani.backend_account_id = ai.backend_account_id AND ani.anchor_no = ai.nowid
 ;
 
 
-DROP TABLE IF EXISTS warehouse.ods_now_anchor_live_detail_daily;
-CREATE TABLE warehouse.ods_now_anchor_live_detail_daily AS
+-- Merge
+# DROP TABLE IF EXISTS warehouse.ods_now_anchor_live_detail_daily;
+# CREATE TABLE warehouse.ods_now_anchor_live_detail_daily AS
+DELETE FROM warehouse.ods_now_anchor_live_detail_daily WHERE dt BETWEEN '{start_date}' AND '{end_date}';
+INSERT INTO warehouse.ods_now_anchor_live_detail_daily
 SELECT ai.platform_id,
        ai.platform_name,
        ai.backend_account_id,
@@ -74,6 +81,7 @@ SELECT ai.platform_id,
        ai.settle_method_text,
        ai.dt
 FROM warehouse.ods_anchor_now_info ai
-LEFT JOIN warehouse.ods_anchor_hy_live_amt al ON ai.backend_account_id = al.backend_account_id AND ai.anchor_uid = al.anchor_uid AND ai.dt = al.dt
+LEFT JOIN warehouse.ods_anchor_now_live_amt al ON ai.backend_account_id = al.backend_account_id AND ai.dt = al.dt AND ai.anchor_no = al.anchor_no
 LEFT JOIN warehouse.platform pf ON ai.platform_id = pf.id
 ;
+
