764f615d576f07a59311cdb1b9de9c05460aec11
Rockie_Yuen <rockie_yuen@outlook.com>
2020-01-07 19:42:07 +0800

    add dw

---
+++ jobs_sql/dw/dw_now.sql
@@ -0,0 +1,24 @@
+-- 数据源 warehouse.ods_now_anchor_live_detail_daily
+-- ===============================================================
+-- 汇总数据
+-- 汇总维度 月-主播
+-- 汇总指标 开播天数，开播时长，虚拟币收入
+
+DROP TABLE IF EXISTS warehouse.dw_sum_now_mon_pf_an;
+CREATE TABLE warehouse.dw_sum_now_mon_pf_an AS
+SELECT YEAR(t.dt)                                                                      AS rpt_year,
+       MONTH(t.dt)                                                                     AS rpt_month,
+       t.platform_id,
+       t.platform_name,
+       t.anchor_no,
+       COUNT(CASE WHEN t.live_status = 1 THEN t.dt ELSE NULL END)                      AS live_days,
+       SUM(CASE WHEN t.duration IS NULL OR t.duration = '' THEN 0 ELSE t.duration END) AS sum_duration,
+       SUM(CASE WHEN t.amt IS NULL OR t.amt = '' THEN 0 ELSE t.amt END)                AS sum_amt
+FROM warehouse.ods_now_anchor_live_detail_daily t
+GROUP BY YEAR(t.dt),
+         MONTH(t.dt),
+         t.backend_account_id,
+         t.platform_id,
+         t.platform_name,
+         t.anchor_no
+;
---
+++ jobs_sql/dw/dw_yy.sql
@@ -0,0 +1,25 @@
+-- 数据源 warehouse.ods_yy_anchor_live_detail_daily
+-- ===============================================================
+-- 汇总数据
+-- 汇总维度 月-主播
+-- 汇总指标 开播天数，开播时长，虚拟币收入，主播佣金，公会佣金
+DROP TABLE IF EXISTS warehouse.dw_sum_yy_mon_pf_an;
+CREATE TABLE warehouse.dw_sum_yy_mon_pf_an AS
+SELECT YEAR(t.dt)                                                 AS rpt_year,
+       MONTH(t.dt)                                                AS rpt_month,
+       t.platform_id,
+       t.platform_name,
+       t.anchor_no,
+       COUNT(CASE WHEN t.live_status = 1 THEN t.dt ELSE NULL END) AS live_days,
+       SUM(t.duration)                               AS sum_duration,
+       SUM(t.virtual_coin)                           AS total_vir_coin,
+       SUM(t.anchor_commission)                      AS total_an_commission,
+       SUM(t.guild_commission)                       AS total_g_commission
+FROM warehouse.ods_yy_anchor_live_detail_daily t
+WHERE t.dt < CURRENT_DATE
+GROUP BY YEAR(t.dt),
+         MONTH(t.dt),
+         t.platform_id,
+         t.platform_name,
+         t.anchor_no
+;
