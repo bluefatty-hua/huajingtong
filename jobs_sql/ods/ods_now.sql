506f9de2fc4c72e3dc3663b91927eb3866e8dbfa
Rockie_Yuen <rockie_yuen@outlook.com>
2020-01-11 18:30:36 +0800

    修改bb ods

--- jobs_sql/ods/ods_now.sql
+++ jobs_sql/ods/ods_now.sql
@@ -1,8 +1,8 @@
 -- 主播信息
--- DROP TABLE IF EXISTS warehouse.ods_anchor_now_info;
--- CREATE TABLE warehouse.ods_anchor_now_info AS
-DELETE FROM warehouse.ods_anchor_now_info WHERE dt BETWEEN '{start_date}' AND '{end_date}';
-INSERT INTO warehouse.ods_anchor_now_info
+DROP TABLE IF EXISTS warehouse.ods_now_anchor_info;
+CREATE TABLE warehouse.ods_now_anchor_info AS
+-- DELETE FROM warehouse.ods_anchor_now_info WHERE dt BETWEEN '{start_date}' AND '{end_date}';
+-- INSERT INTO warehouse.ods_anchor_now_info
 SELECT 1003 AS platform_id,
        'NOW' AS platform_name,
        ad.backend_account_id,
@@ -25,41 +25,15 @@ SELECT 1003 AS platform_id,
        ad.timestamp
 FROM
     spider_now_backend.anchor_detail ad
-WHERE ad.dt BETWEEN '{start_date}' AND '{end_date}'
-;
-
-
--- 主播直播和收入
--- DROP TABLE IF EXISTS warehouse.ods_anchor_now_live_amt;
--- CREATE TABLE warehouse.ods_anchor_now_live_amt AS
-DELETE FROM warehouse.ods_anchor_now_live_amt WHERE dt BETWEEN '{start_date}' AND '{end_date}';
-INSERT INTO warehouse.ods_anchor_now_live_amt
-SELECT ani.platform_id,
-       ani.platform_name,
-       ani.backend_account_id,
-       ani.anchor_uid,
-       ani.anchor_qq_no,
-       ani.anchor_no,
-       ani.anchor_nick_name,
-       ani.anchor_name,
-       ai.live_time AS duratiion_hours,
-       ROUND(ai.live_time * 60 * 60, 2) AS duration,
-       ai.origin_money AS amt,
-       ani.settle_method_code,
-       ani.settle_method_text,
-       DATE_FORMAT(ai.date, '%Y-%m-%d') AS dt,
-       ai.timestamp
-FROM warehouse.ods_anchor_now_info ani
-LEFT JOIN spider_now_backend.anchor_income ai ON ani.dt = DATE_FORMAT(ai.date, '%Y-%m-%d') AND ani.backend_account_id = ai.backend_account_id AND ani.anchor_qq_no = ai.uin
-WHERE ani.dt BETWEEN '{start_date}' AND '{end_date}'
+-- WHERE ad.dt BETWEEN '{start_date}' AND '{end_date}'
 ;
 
 
 -- Merge
--- DROP TABLE IF EXISTS warehouse.ods_anchor_now_live_detail_daily;
--- CREATE TABLE warehouse.ods_anchor_now_live_detail_daily AS
-DELETE FROM warehouse.ods_anchor_now_live_detail_daily WHERE dt BETWEEN '{start_date}' AND '{end_date}';
-INSERT INTO warehouse.ods_anchor_now_live_detail_daily
+DROP TABLE IF EXISTS warehouse.ods_now_anchor_live_detail;
+CREATE TABLE warehouse.ods_now_anchor_live_detail AS
+-- DELETE FROM warehouse.ods_now_anchor_live_detail WHERE dt BETWEEN '{start_date}' AND '{end_date}';
+-- INSERT INTO warehouse.ods_now_anchor_live_detail
 SELECT ai.platform_id,
        ai.platform_name,
        ai.backend_account_id,
@@ -70,39 +44,52 @@ SELECT ai.platform_id,
        ai.anchor_name,
        ai.fans_num,
        ai.fans_group_num,
-       CASE WHEN al.duration > 0 THEN 1 ELSE 0 END AS live_status,
-       al.duration,
-       al.amt,
-       pf.vir_coin_name,
-       pf.vir_coin_rate,
-       pf.include_pf_amt,
-       pf.pf_amt_rate,
+       CASE WHEN ain.live_time > 0 THEN 1 ELSE 0 END AS live_status,
+       CASE WHEN ain.live_time >= 0 THEN ain.live_time ELSE 0 END AS duration_hour,
+       CASE WHEN ain.live_time >= 0 THEN ain.live_time * 60 * 60 ELSE 0 END AS duration,
+       ROUND(CASE WHEN ain.origin_money >= 0 THEN ain.origin_money ELSE 0 END, 2) AS anchor_commission_rmb,
        ai.contract_sign_time,
        ai.settle_method_code,
        ai.settle_method_text,
        ai.dt
-FROM warehouse.ods_anchor_now_info ai
-LEFT JOIN warehouse.ods_anchor_now_live_amt al ON ai.backend_account_id = al.backend_account_id AND ai.dt = al.dt AND ai.anchor_no = al.anchor_no
-LEFT JOIN warehouse.platform pf ON ai.platform_id = pf.id
-WHERE ai.dt BETWEEN '{start_date}' AND '{end_date}'
+FROM warehouse.ods_now_anchor_info ai
+LEFT JOIN spider_now_backend.anchor_income ain ON ai.backend_account_id = ain.backend_account_id AND ai.dt = DATE_FORMAT(ain.date, '%Y-%m-%d') AND ai.anchor_qq_no = ain.uin
+-- WHERE ai.dt BETWEEN '{start_date}' AND '{end_date}'
 ;
 
 
 -- ===================================================================
--- 公会收入
--- DROP TABLE IF EXISTS warehouse.ods_guild_now_amt_daily;
--- CREATE TABLE warehouse.ods_guild_now_amt_daily AS
-DELETE FROM warehouse.ods_guild_now_amt_daily WHERE dt BETWEEN '{start_date}' AND '{end_date}';
-INSERT INTO warehouse.ods_guild_now_amt_daily
+-- 公会每日收入、每日开播主播数
+-- DROP TABLE IF EXISTS warehouse.ods_day_now_guild_commission;
+-- CREATE TABLE warehouse.ods_day_now_guild_commission AS
+DELETE FROM warehouse.ods_day_now_guild_commission WHERE dt BETWEEN '{start_date}' AND '{end_date}';
+INSERT INTO warehouse.ods_day_now_guild_commission
 SELECT 1003 AS platform_id,
        'NOW' AS platform_name,
        ui.backend_account_id,
-       ui.on_live AS onlive_an_cnt,
-       ut.origin_money AS g_sum_amt,
-       ut.income AS g_final_amt,
+       ui.on_live AS anchor_live_cnt,
+       ut.origin_money AS guild_commission_rmb,
+       ut.income AS guild_salary_rmb,
        DATE_FORMAT(ui.day, '%Y-%m-%d') AS dt
 FROM spider_now_backend.union_stat_info_by_day ui
 LEFT JOIN spider_now_backend.union_total_income ut ON ui.backend_account_id = ut.backend_account_id AND ui.day = ut.date
 WHERE DATE_FORMAT(ui.day, '%Y-%m-%d') BETWEEN '{start_date}' AND '{end_date}'
 ;
 
+
+-- 公会每月流水，平均主播流水
+-- DROP TABLE IF EXISTS warehouse.ods_month_now_guild_commission;
+-- CREATE TABLE warehouse.ods_month_now_guild_commission AS
+DELETE FROM warehouse.ods_month_now_guild_commission WHERE dt BETWEEN '{start_date}' AND '{end_date}';
+INSERT INTO warehouse.ods_month_now_guild_commission
+SELECT 1003 AS platform_id,
+       'NOW' AS platform_name,
+       backend_account_id,
+       DATE_FORMAT(ui.date, '%Y-%m-%d') AS dt,
+       ui.anchor_num AS anchor_cnt,
+       ui.cur_month_total_journal AS guild_commission_rmb,
+       ui.average_journal AS average_anchor_commission_rmb,
+       ui.living_rate AS anchor_live_rate
+FROM spider_now_backend.union_stat_info_by_month ui
+WHERE DATE_FORMAT(ui.date, '%Y-%m-%d') BETWEEN '{start_date}' AND '{end_date}'
+;
