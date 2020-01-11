50fe5d6616f88e1f0e6b67c5cb7924b66b7b80ae
Page <4061207@qq.com>
2020-01-11 18:21:03 +0800

    update jobs_sql/dw/dw_hy.sql.

--- jobs_sql/dw/dw_hy.sql
+++ jobs_sql/dw/dw_hy.sql
@@ -111,74 +111,31 @@ GROUP BY anchor_uid,channel_id
 
 
 
+-- DROP TABLE IF EXISTS warehouse.dw_month_huya_guild_live;
+-- CREATE TABLE warehouse.dw_month_huya_guild_live AS
+delete from warehouse.dw_month_huya_guild_live where dt ='{month.start}' 
+insert into warehouse.dw_month_huya_guild_live
+SELECT
+  t2.`platform_id`,
+  t2.`platform_name`,
+  t1.`channel_id`,
+  t2.`channel_num`,
+  t2.`ow`,
+  t2.`channel_name`,
+  t2.`logo`,
+  t2.`desc`,
+  t2.`create_time`,
+  t2.`is_platinum`,
+  t2.`sign_count`,
+  t2.`sign_limit`,
+  '{month.start}' AS dt,
+  SUM(`revenue`) AS revenue,
+  SUM(IF(gift_calc_month= '{month.start}', `gift_income`,0)) AS gift_income,
+  SUM(IF(guard_calc_month= '{month.start}', `guard_income`,0)) AS guard_income,
+  SUM(IF(noble_calc_month= '{month.start}', `noble_income`,0)) AS noble_income
+FROM `warehouse`.`ods_day_huya_guild_live` t1
+LEFT OUTER JOIN `warehouse`.`ods_month_huya_guild_info` t2
+	ON t1.channel_id= t2.channel_id AND t2.dt= '{month.start}'
+WHERE t1.dt BETWEEN '{month.start}' AND '{month.end}'
+GROUP BY t1.channel_id
 
-
-
--- 数据源 warehouse.ods_bb_anchor_live_detail_daily
--- ===============================================================
--- 汇总数据
--- 汇总维度 月-主播
--- 汇总指标 开播天数，开播时长，虚拟币收入
-DROP TABLE IF EXISTS warehouse.dw_sum_hy_an_mon;
-CREATE TABLE warehouse.dw_sum_hy_an_mon AS
-SELECT DATE_FORMAT(CONCAT(YEAR(t.dt), '-', MONTH(t.dt), '-01'), '%Y-%m-%d')        AS rpt_month,
-       t.platform_id,
-       t.platform_name,
-       t.anchor_no,
-       COUNT(CASE WHEN t.live_status = 1 THEN t.dt ELSE NULL END) AS live_days,
-       SUM(t.duration)                                            AS sum_duration,
-       ROUND(SUM(t.amt), 2)                                                 AS sum_amt
-FROM warehouse.ods_anchor_hy_live_detail_daily t
-WHERE t.dt < CURRENT_DATE
-GROUP BY DATE_FORMAT(CONCAT(YEAR(t.dt), '-', MONTH(t.dt), '-01'), '%Y-%m-%d'),
-         t.platform_id,
-         t.platform_name,
-         t.anchor_no
-;
-
--- 汇总数据源 ods_anchor_hy_live_detail_daily
--- 汇总维度 月-公会或频道
--- 汇总指标 主播数，开播主播数，主播收入
-DROP TABLE IF EXISTS warehouse.dw_sum_hy_g_mon;
-CREATE TABLE warehouse.dw_sum_hy_g_mon AS
-SELECT DATE_FORMAT(CONCAT(YEAR(t.dt), '-', MONTH(t.dt), '-01'), '%Y-%m-%d')                                                                AS rpt_month,
-       t.platform_id,
-       t.platform_name,
-       t.channel_id,
-       COUNT(DISTINCT t.anchor_no)                                                AS an_cnt,
-       COUNT(DISTINCT CASE WHEN t.live_status = 1 THEN t.anchor_no ELSE NULL END) AS an_live_cnt,
-       ROUND(SUM(t.amt), 2)                                                       AS sum_amt
-FROM warehouse.ods_anchor_hy_live_detail_daily t
-WHERE t.dt < CURRENT_DATE
-GROUP BY DATE_FORMAT(CONCAT(YEAR(t.dt), '-', MONTH(t.dt), '-01'), '%Y-%m-%d'),
-         t.platform_id,
-         t.platform_name,
-         t.channel_id
-;
-
-
--- ==========================================================================
--- 公会或频道
--- 数据来源 ods_guild_hy_amt_daily
--- 汇总维度 月-公会或频道
--- 汇总指标 公会流水，公会收入，礼物|贵族|守护公会分成，统计天数
-DROP TABLE IF EXISTS warehouse.dw_sum_hy_g_amt_mon;
-CREATE TABLE warehouse.dw_sum_hy_g_amt_mon AS
-SELECT t.platform_id,
-       t.platform_name,
-       DATE_FORMAT(CONCAT(YEAR(t.dt), '-', MONTH(t.dt), '-01'), '%Y-%m-%d') AS rpt_month,
-       t.channel_id,
-       t.channel_num,
-       SUM(total_amt) AS g_total_amt,
-       SUM(t.g_gift_vir_coin + t.g_guard_vir_coin + t.g_nobel_vir_coin) / 1000 AS g_final_amt,
-       SUM(t.g_gift_vir_coin) AS g_gift_vir_coin,
-       SUM(t.g_guard_vir_coin) AS g_guard_vir_coin,
-       SUM(t.g_nobel_vir_coin) AS g_nobel_vir_coin,
-       COUNT(DISTINCT t.dt) AS dt_cnt
-FROM warehouse.ods_guild_hy_amt_daily t
-GROUP BY t.platform_id,
-         t.platform_name,
-         DATE_FORMAT(CONCAT(YEAR(t.dt), '-', MONTH(t.dt), '-01'), '%Y-%m-%d'),
-         t.channel_id,
-         t.channel_num
-;
