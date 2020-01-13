46f519fb0d559ff1759c7732908393d82d0d53d7
Page <4061207@qq.com>
2020-01-13 10:01:38 +0800

    dw

--- jobs_sql/dw/dw_yy.sql
+++ jobs_sql/dw/dw_yy.sql
@@ -1,20 +1,20 @@
 -- 公会蓝钻收入
 -- 汇总维度 月-公会-主播
 -- 汇总指标 主播总蓝钻 公会分成蓝钻
--- DROP TABLE IF EXISTS warehouse.dw_month_yy_guild_anchor_virtual_coin;
--- CREATE TABLE warehouse.dw_month_yy_guild_anchor_virtual_coin AS
+-- DROP TABLE IF EXISTS warehouse.dw_yy_month_guild_anchor_live_bluediamond;
+-- CREATE TABLE warehouse.dw_yy_month_guild_anchor_live_bluediamond AS
 DELETE
-FROM warehouse.dw_month_yy_guild_anchor_virtual_coin
+FROM warehouse.dw_yy_month_guild_anchor_live_bluediamond
 WHERE dt BETWEEN CONCAT(YEAR('{start_date}'), '-', MONTH('{start_date}'), '-01') AND '{end_date}';
-INSERT INTO warehouse.dw_month_yy_guild_anchor_virtual_coin
+INSERT INTO warehouse.dw_yy_month_guild_anchor_live_bluediamond
 SELECT dt,
        platform_id,
        platform_name,
        backend_account_id,
        anchor_no,
-       SUM(anchor_virtual_coin) AS anchor_virtual_coin,
-       SUM(guild_virtual_coin)  AS guild_virtual_coin
-FROM warehouse.ods_yy_guild_virtual_coin_detail
+       SUM(anchor_bluediamond) AS anchor_bluediamond,
+       SUM(guild_bluediamond)  AS guild_bluediamond
+FROM warehouse.ods_yy_guild_live_bluediamond
 WHERE dt BETWEEN CONCAT(YEAR('{start_date}'), '-', MONTH('{start_date}'), '-01') AND '{end_date}'
 GROUP BY dt,
          platform_id,
@@ -24,15 +24,63 @@ GROUP BY dt,
 ;
 
 
+-- 汇总维度 月-公会
+-- 汇总指标 主播总蓝钻 公会分成蓝钻
+-- DROP TABLE IF EXISTS warehouse.dw_yy_month_guild_live_bluediamond;
+-- CREATE TABLE warehouse.dw_yy_month_guild_live_bluediamond AS
+DELETE
+FROM warehouse.dw_yy_month_guild_live_bluediamond
+WHERE dt BETWEEN CONCAT(YEAR('{start_date}'), '-', MONTH('{start_date}'), '-01') AND '{end_date}';
+INSERT INTO warehouse.dw_yy_month_guild_live_bluediamond
+SELECT dt,
+       platform_id,
+       platform_name,
+       backend_account_id,
+       SUM(anchor_bluediamond) AS anchor_bluediamond,
+       SUM(guild_bluediamond)  AS guild_bluediamond
+FROM warehouse.ods_yy_guild_live_bluediamond
+WHERE dt BETWEEN CONCAT(YEAR('{start_date}'), '-', MONTH('{start_date}'), '-01') AND '{end_date}'
+GROUP BY dt,
+         platform_id,
+         platform_name,
+         backend_account_id
+;
+
+
+-- 公会蓝钻收入
+-- 汇总维度 月-主播
+-- 汇总指标 主播总蓝钻 公会分成蓝钻
+-- DROP TABLE IF EXISTS warehouse.dw_yy_month_anchor_live_bluediamond;
+-- CREATE TABLE warehouse.dw_yy_month_anchor_live_bluediamond AS
+DELETE
+FROM warehouse.dw_yy_month_anchor_live_bluediamond
+WHERE dt BETWEEN CONCAT(YEAR('{start_date}'), '-', MONTH('{start_date}'), '-01') AND '{end_date}';
+INSERT INTO warehouse.dw_yy_month_anchor_live_bluediamond
+SELECT dt,
+       platform_id,
+       platform_name,
+       anchor_no,
+       SUM(anchor_bluediamond) AS anchor_bluediamond,
+       SUM(guild_bluediamond)  AS guild_bluediamond
+FROM warehouse.ods_yy_guild_live_bluediamond
+WHERE dt BETWEEN CONCAT(YEAR('{start_date}'), '-', MONTH('{start_date}'), '-01') AND '{end_date}'
+GROUP BY dt,
+         platform_id,
+         platform_name,
+         anchor_no
+;
+
+
+
 -- 公会佣金收入
 -- 汇总维度 月-公会-主播
 -- 汇总指标 公会分成佣金
--- DROP TABLE IF EXISTS warehouse.dw_month_yy_guild_anchor_commission;
--- CREATE TABLE warehouse.dw_month_yy_guild_anchor_commission AS
+-- DROP TABLE IF EXISTS warehouse.dw_yy_month_guild_anchor_live_commission;
+-- CREATE TABLE warehouse.dw_yy_month_guild_anchor_live_commission AS
 DELETE
-FROM warehouse.dw_month_yy_guild_anchor_commission
+FROM warehouse.dw_yy_month_guild_anchor_live_commission
 WHERE dt BETWEEN CONCAT(YEAR('{start_date}'), '-', MONTH('{start_date}'), '-01') AND '{end_date}';
-INSERT INTO warehouse.dw_month_yy_guild_anchor_commission
+INSERT INTO warehouse.dw_yy_month_guild_anchor_live_commission
 SELECT dt,
        platform_id,
        platform_name,
@@ -40,7 +88,7 @@ SELECT dt,
        channel_num,
        anchor_no,
        SUM(guild_commission) AS guild_commission
-FROM warehouse.ods_yy_guild_commission_detail
+FROM warehouse.ods_yy_guild_live_commission
 WHERE dt BETWEEN CONCAT(YEAR('{start_date}'), '-', MONTH('{start_date}'), '-01') AND '{end_date}'
 GROUP BY dt,
          platform_id,
@@ -50,46 +98,46 @@ GROUP BY dt,
          anchor_no
 ;
 
--- =================================================================
--- 公会主播月蓝钻收入
--- 汇总维度 月-主播
--- 汇总指标 主播总蓝钻 公会分成蓝钻
--- DROP TABLE IF EXISTS warehouse.dw_month_yy_guild_virtual_coin;
--- CREATE TABLE warehouse.dw_month_yy_guild_virtual_coin AS
+
+-- 公会佣金收入
+-- 汇总维度 月-公会
+-- 汇总指标 公会分成佣金
+-- DROP TABLE IF EXISTS warehouse.dw_yy_month_guild_live_commission;
+-- CREATE TABLE warehouse.dw_yy_month_guild_live_commission AS
 DELETE
-FROM warehouse.dw_month_yy_guild_virtual_coin
+FROM warehouse.dw_yy_month_guild_live_commission
 WHERE dt BETWEEN CONCAT(YEAR('{start_date}'), '-', MONTH('{start_date}'), '-01') AND '{end_date}';
-INSERT INTO warehouse.dw_month_yy_guild_virtual_coin
+INSERT INTO warehouse.dw_yy_month_guild_live_commission
 SELECT dt,
        platform_id,
        platform_name,
-       anchor_no,
-       SUM(anchor_virtual_coin) AS anchor_virtual_coin,
-       SUM(guild_virtual_coin)  AS guild_virtual_coin
-FROM warehouse.ods_yy_guild_virtual_coin_detail
+       backend_account_id,
+       channel_num,
+       SUM(guild_commission) AS guild_commission
+FROM warehouse.ods_yy_guild_live_commission
 WHERE dt BETWEEN CONCAT(YEAR('{start_date}'), '-', MONTH('{start_date}'), '-01') AND '{end_date}'
 GROUP BY dt,
          platform_id,
          platform_name,
-         anchor_no
+         backend_account_id,
+         channel_num
 ;
 
-
--- 公会月佣金收入
+-- 公会佣金收入
 -- 汇总维度 月-主播
 -- 汇总指标 公会分成佣金
--- DROP TABLE IF EXISTS warehouse.dw_month_yy_guild_commission;
--- CREATE TABLE warehouse.dw_month_yy_guild_commission AS
+-- DROP TABLE IF EXISTS warehouse.dw_yy_month_anchor_live_commission;
+-- CREATE TABLE warehouse.dw_yy_month_anchor_live_commission AS
 DELETE
-FROM warehouse.dw_month_yy_guild_commission
+FROM warehouse.dw_yy_month_anchor_live_commission
 WHERE dt BETWEEN CONCAT(YEAR('{start_date}'), '-', MONTH('{start_date}'), '-01') AND '{end_date}';
-INSERT INTO warehouse.dw_month_yy_guild_commission
+INSERT INTO warehouse.dw_yy_month_anchor_live_commission
 SELECT dt,
        platform_id,
        platform_name,
        anchor_no,
        SUM(guild_commission) AS guild_commission
-FROM warehouse.ods_yy_guild_commission_detail
+FROM warehouse.ods_yy_guild_live_commission
 WHERE dt BETWEEN CONCAT(YEAR('{start_date}'), '-', MONTH('{start_date}'), '-01') AND '{end_date}'
 GROUP BY dt,
          platform_id,
@@ -98,15 +146,17 @@ GROUP BY dt,
 ;
 
 
+
+
 -- 主播月收入
 -- 汇总维度 月-公会—主播
 -- 汇总指标 开播天数，开播时长，虚拟币收入，主播佣金，公会佣金
--- DROP TABLE IF EXISTS warehouse.dw_month_yy_guild_anchor_salary;
--- CREATE TABLE warehouse.dw_month_yy_guild_anchor_salary AS
+-- DROP TABLE IF EXISTS warehouse.dw_yy_month_guild_live;
+-- CREATE TABLE warehouse.dw_yy_month_guild_live AS
 DELETE
-FROM warehouse.dw_month_yy_guild_anchor_salary
+FROM warehouse.dw_yy_month_guild_live
 WHERE dt = CONCAT(YEAR('{start_date}'), '-', MONTH('{start_date}'), '-01');
-INSERT INTO warehouse.dw_month_yy_guild_anchor_salary
+INSERT INTO warehouse.dw_yy_month_guild_live
 SELECT ad.dt,
        ad.platform_id,
        ad.platform_name,
@@ -155,22 +205,24 @@ FROM (SELECT DATE_FORMAT(CONCAT(YEAR(dt), '-', MONTH(dt), '-01'), '%Y-%m-%d')   
 -- 主播月收入
 -- 汇总维度 月-主播
 -- 汇总指标 主播数，开播主播数，主播虚拟币收入,主播佣金收入，公会佣金收入
--- DROP TABLE IF EXISTS warehouse.dw_month_yy_anchor_salary;
--- CREATE TABLE warehouse.dw_month_yy_anchor_salary AS
+-- DROP TABLE IF EXISTS warehouse.dw_yy_month_anchor_live;
+-- CREATE TABLE warehouse.dw_yy_month_anchor_live AS
 DELETE
-FROM warehouse.dw_month_yy_anchor_salary
+FROM warehouse.dw_yy_month_anchor_live
 WHERE dt = CONCAT(YEAR('{start_date}'), '-', MONTH('{start_date}'), '-01');
-INSERT INTO warehouse.dw_month_yy_anchor_salary
+INSERT INTO warehouse.dw_yy_month_anchor_live
 SELECT ad.dt,
        ad.platform_id,
        ad.platform_name,
        ad.anchor_no,
        ad.anchor_uid,
+       ad.backend_account_id,
+       channel_num,
        ad.live_days,
        ad.duration,
-       ad.anchor_virtual_coin,
-       gv.anchor_virtual_coin AS anchor_virtual_coin_true,
-       gv.guild_virtual_coin  AS guild_virtual_coin_true,
+       ad.bluediamond,
+       gv.anchor_bluediamond AS anchor_bluediamond_true,
+       gv.guild_bluediamond  AS guild_bluediamond_true,
        ad.anchor_commission,
        ad.guild_commission,
        gc.guild_commission    AS guild_commission_ture,
@@ -181,20 +233,23 @@ FROM (
                 t.platform_name,
                 t.anchor_no,
                 t.anchor_uid,
+                t.backend_account_id,
+                max(channel_num) as channel_num,
                 COUNT(DISTINCT CASE WHEN t.live_status = 1 THEN t.dt ELSE NULL END)         AS live_days,
                 SUM(CASE WHEN duration >= 0 THEN duration ELSE 0 END)                       AS duration,
-                SUM(CASE WHEN t.virtual_coin >= 0 THEN t.virtual_coin ELSE 0 END)           AS anchor_virtual_coin,
+                SUM(CASE WHEN t.bluediamond >= 0 THEN t.bluediamond ELSE 0 END)           AS bluediamond,
                 SUM(CASE WHEN t.anchor_commission >= 0 THEN t.anchor_commission ELSE 0 END) AS anchor_commission,
                 SUM(CASE WHEN t.guild_commission >= 0 THEN t.guild_commission ELSE 0 END)   AS guild_commission,
                 COUNT(DISTINCT t.dt)                                                        AS dt_cnt
-         FROM warehouse.ods_day_yy_anchor_live_detail t
+         FROM warehouse.dw_yy_day_anchor_live t
          WHERE dt BETWEEN CONCAT(YEAR('{start_date}'), '-', MONTH('{start_date}'), '-01') AND '{end_date}'
          GROUP BY DATE_FORMAT(CONCAT(YEAR(t.dt), '-', MONTH(t.dt), '-01'), '%Y-%m-%d'),
                   t.platform_id,
                   t.platform_name,
                   t.anchor_no,
-                  t.anchor_uid) ad
-         LEFT JOIN warehouse.dw_month_yy_guild_virtual_coin gv ON ad.dt = gv.dt AND ad.anchor_no = gv.anchor_no
-         LEFT JOIN warehouse.dw_month_yy_guild_commission gc ON ad.dt = gc.dt AND ad.anchor_no = gc.anchor_no
+                  t.anchor_uid,
+                  t.backend_account_id) ad
+         LEFT JOIN warehouse.dw_yy_month_anchor_live_bluediamond gv ON ad.dt = gv.dt AND ad.anchor_no = gv.anchor_no
+         LEFT JOIN warehouse.dw_yy_month_anchor_live_commission gc ON ad.dt = gc.dt AND ad.anchor_no = gc.anchor_no
 ;
 
