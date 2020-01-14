cb8049caf86ddfa3923419f019505e47674b0d4c
Rockie_Yuen <rockie_yuen@outlook.com>
2020-01-14 15:19:55 +0800

    up

--- jobs_sql/dw/dw_bb_month.sql
+++ jobs_sql/dw/dw_bb_month.sql
@@ -3,9 +3,9 @@
 -- DROP TABLE IF EXISTS warehouse.dw_month_bb_anchor_live;
 -- CREATE TABLE warehouse.dw_month_bb_anchor_live AS
 DELETE
-FROM warehouse.dw_month_bb_anchor_live
+FROM warehouse.dw_bb_month_anchor_live
 WHERE DATE_FORMAT(dt, '%Y%m') BETWEEN DATE_FORMAT('{start_date}', '%Y%m') AND DATE_FORMAT('{end_date}', '%Y%m');
-INSERT INTO warehouse.dw_month_bb_anchor_live
+INSERT INTO warehouse.dw_bb_month_anchor_live
 SELECT DATE_FORMAT(CONCAT(YEAR(t.dt), '-', MONTH(t.dt), '-01'), '%Y-%m-%d') AS dt,
        t.platform_id,
        t.platform_name,
@@ -14,7 +14,7 @@ SELECT DATE_FORMAT(CONCAT(YEAR(t.dt), '-', MONTH(t.dt), '-01'), '%Y-%m-%d') AS d
        COUNT(CASE WHEN t.live_status = 1 THEN t.dt ELSE NULL END)           AS live_days,
        SUM(t.duration)                                                      AS duration,
        SUM(t.anchor_total_coin)                                             AS anchor_virtual_coin
-FROM warehouse.ods_day_bb_anchor_live t
+FROM warehouse.ods_bb_day_anchor_live t
 WHERE DATE_FORMAT(dt, '%Y%m') BETWEEN DATE_FORMAT('{start_date}', '%Y%m') AND DATE_FORMAT('{end_date}', '%Y%m')
 GROUP BY DATE_FORMAT(CONCAT(YEAR(t.dt), '-', MONTH(t.dt), '-01'), '%Y-%m-%d'),
          t.platform_id,
@@ -26,23 +26,24 @@ GROUP BY DATE_FORMAT(CONCAT(YEAR(t.dt), '-', MONTH(t.dt), '-01'), '%Y-%m-%d'),
 
 -- 汇总维度 月-公会
 -- 汇总指标 主播数，开播主播数，虚拟币收入
--- DROP TABLE IF EXISTS warehouse.dw_month_bb_guild_live;
--- CREATE TABLE warehouse.dw_month_bb_guild_live AS
+-- DROP TABLE IF EXISTS warehouse.dw_bb_month_guild_live;
+-- CREATE TABLE warehouse.dw_bb_month_guild_live AS
 DELETE
-FROM warehouse.dw_month_bb_guild_live
+FROM warehouse.dw_bb_month_guild_live
 WHERE DATE_FORMAT(dt, '%Y%m') BETWEEN DATE_FORMAT('{start_date}', '%Y%m') AND DATE_FORMAT('{end_date}', '%Y%m');
-INSERT INTO warehouse.dw_month_bb_guild_live
+INSERT INTO warehouse.dw_bb_month_guild_live
 SELECT t.dt,
        t.platform_id,
        t.platform_name,
        t.backend_account_id,
        t.anchor_cnt,
        t.anchor_live_cnt,
-       t.anchor_virtual_coin  AS anchor_virtual_coin,
        t1.type,
        t1.guild_salary_rmb    AS guild_salart_rmb_ture,
        t1.guild_virtual_coin  AS guild_virtual_coin_ture,
+       t.anchor_virtual_coin,
        t1.anchor_virtual_coin AS anchor_vitual_coin_ture,
+       t.anchor_income,
        t1.anchor_base_coin AS anchor_base_coin,
        t1.guild_award_coin AS guild_award_coin,
        t1.operate_award_punish_coin AS operate_award_punish_coin,
@@ -55,13 +56,14 @@ FROM (SELECT DATE_FORMAT(CONCAT(YEAR(t.dt), '-', MONTH(t.dt), '-01'), '%Y-%m-%d'
              t.backend_account_id,
              COUNT(DISTINCT t.anchor_no)                                                AS anchor_cnt,
              COUNT(DISTINCT CASE WHEN t.live_status = 1 THEN t.anchor_no ELSE NULL END) AS anchor_live_cnt,
-             SUM(t.anchor_total_coin)                                                   AS anchor_virtual_coin
-      FROM warehouse.ods_day_bb_anchor_live t
+             SUM(t.anchor_total_coin)                                                   AS anchor_virtual_coin,
+             SUM(t.anchor_income_virtual_coin) AS anchor_income
+      FROM warehouse.ods_bb_day_anchor_live t
       GROUP BY DATE_FORMAT(CONCAT(YEAR(t.dt), '-', MONTH(t.dt), '-01'), '%Y-%m-%d'),
                t.platform_id,
                t.platform_name,
                t.backend_account_id) t
-         LEFT JOIN warehouse.ods_month_bb_guild_live t1
+         LEFT JOIN warehouse.ods_bb_month_guild_live t1
                    ON t.dt = t1.dt AND t.backend_account_id = t1.backend_account_id
 WHERE t1.type = '公会总收益'
   AND DATE_FORMAT(t.dt, '%Y%m') BETWEEN DATE_FORMAT('{start_date}', '%Y%m') AND DATE_FORMAT('{end_date}', '%Y%m')
