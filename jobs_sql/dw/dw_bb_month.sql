b0a27b9deb46de2c22165083dac2a4ade848fd50
Rockie_Yuen <rockie_yuen@outlook.com>
2020-01-15 18:34:04 +0800

    修复B站缺少未结算月公会流水

--- jobs_sql/dw/dw_bb_month.sql
+++ jobs_sql/dw/dw_bb_month.sql
@@ -39,15 +39,15 @@ SELECT t.dt,
        t.anchor_cnt,
        t.anchor_live_cnt,
        t1.type,
-       t1.guild_salary_rmb    AS guild_salart_rmb_ture,
-       t1.guild_virtual_coin  AS guild_virtual_coin_ture,
+       t1.guild_salary_rmb          AS guild_salart_rmb_ture,
+       t1.guild_virtual_coin        AS guild_virtual_coin_ture,
        t.anchor_virtual_coin,
-       t1.anchor_virtual_coin AS anchor_vitual_coin_ture,
+       t1.anchor_virtual_coin       AS anchor_vitual_coin_ture,
        t.anchor_income,
-       t1.anchor_base_coin AS anchor_base_coin,
-       t1.guild_award_coin AS guild_award_coin,
+       t1.anchor_base_coin          AS anchor_base_coin,
+       t1.guild_award_coin          AS guild_award_coin,
        t1.operate_award_punish_coin AS operate_award_punish_coin,
-       t1.special_coin AS special_virtual_coin,
+       t1.special_coin              AS special_virtual_coin,
        t1.anchor_change_vir_coin,
        t1.guild_change_coin
 FROM (SELECT DATE_FORMAT(CONCAT(YEAR(t.dt), '-', MONTH(t.dt), '-01'), '%Y-%m-%d')       AS dt,
@@ -57,16 +57,14 @@ FROM (SELECT DATE_FORMAT(CONCAT(YEAR(t.dt), '-', MONTH(t.dt), '-01'), '%Y-%m-%d'
              COUNT(DISTINCT t.anchor_no)                                                AS anchor_cnt,
              COUNT(DISTINCT CASE WHEN t.live_status = 1 THEN t.anchor_no ELSE NULL END) AS anchor_live_cnt,
              SUM(t.anchor_total_coin)                                                   AS anchor_virtual_coin,
-             SUM(t.anchor_income_virtual_coin) AS anchor_income
+             SUM(t.anchor_income_virtual_coin)                                          AS anchor_income
       FROM warehouse.ods_bb_day_anchor_live t
       GROUP BY DATE_FORMAT(CONCAT(YEAR(t.dt), '-', MONTH(t.dt), '-01'), '%Y-%m-%d'),
                t.platform_id,
                t.platform_name,
                t.backend_account_id) t
-         LEFT JOIN warehouse.ods_bb_month_guild_live t1
+         LEFT JOIN (SELECT * FROM warehouse.ods_bb_month_guild_live WHERE type = '公会总收益') t1
                    ON t.dt = t1.dt AND t.backend_account_id = t1.backend_account_id
-WHERE t1.type = '公会总收益'
-  AND DATE_FORMAT(t.dt, '%Y%m') BETWEEN DATE_FORMAT('{start_date}', '%Y%m') AND DATE_FORMAT('{end_date}', '%Y%m')
-;
+WHERE DATE_FORMAT(t.dt, '%Y%m') BETWEEN DATE_FORMAT('2019-01-01', '%Y%m') AND DATE_FORMAT('2020-01-14', '%Y%m')
 
 
