666d5d18cfb65f5548fc89d9a5923a30ce16bc17
Rockie_Yuen <rockie_yuen@outlook.com>
2020-01-15 16:37:59 +0800

    up

--- jobs_sql/dw/dw_hy_month.sql
+++ jobs_sql/dw/dw_hy_month.sql
@@ -3,19 +3,19 @@
 -- 汇总月guild、anchor info数据
 -- DROP TABLE IF EXISTS stage.stage_huya_month_guild_info;
 -- CREATE TABLE stage.stage_huya_month_guild_info AS
-delete from stage.stage_huya_month_guild_info where dt >='{month.start}' AND dt<='{month.end}';
+delete from stage.stage_huya_month_guild_info where dt >='{start_date}' AND dt<='{end_date}';
 insert into stage.stage_huya_month_guild_info
 SELECT channel_id,MAX(dt) AS dt
 FROM warehouse.dw_huya_day_guild_info
-WHERE dt >='{month.start}' AND dt<='{month.end}'
+WHERE dt >='{start_date}' AND dt<='{end_date}'
 GROUP BY channel_id;
 
 -- DROP TABLE IF EXISTS warehouse.dw_huya_month_guild_info;
 -- CREATE TABLE warehouse.dw_huya_month_guild_info AS
-delete from warehouse.dw_huya_month_guild_info where dt >='{month.start}' AND dt<='{month.end}';
+delete from warehouse.dw_huya_month_guild_info where dt >='{start_date}' AND dt<='{end_date}';
 insert into warehouse.dw_huya_month_guild_info
 select 
-  '{month.start}' as `dt`,
+  '{start_date}' as `dt`,
   t1.`channel_id`,
   `channel_num`,
   `platform_id`,
@@ -32,25 +32,25 @@ select
 from `warehouse`.`dw_huya_day_guild_info`  t1
 join stage.stage_huya_month_guild_info t2
 on t1.dt= t2.dt and t1.channel_id = t2.channel_id
-where t1.dt >='{month.start}' AND t1.dt<='{month.end}';
+where t1.dt >='{start_date}' AND t1.dt<='{end_date}';
 
 
 -- DROP TABLE IF EXISTS stage.stage_huya_month_anchor_info;
 -- CREATE TABLE stage.stage_huya_month_anchor_info AS
-delete from stage.stage_huya_month_anchor_info where dt >='{month.start}' AND dt<='{month.end}';
+delete from stage.stage_huya_month_anchor_info where dt >='{start_date}' AND dt<='{end_date}';
 insert into stage.stage_huya_month_anchor_info
 SELECT anchor_uid,MAX(dt) AS dt,channel_id
 FROM warehouse.dw_huya_day_anchor_info
-WHERE dt >='{month.start}' AND dt<='{month.end}'
+WHERE dt >='{start_date}' AND dt<='{end_date}'
 GROUP BY anchor_uid,channel_id;
 
 
 -- DROP TABLE IF EXISTS warehouse.dw_huya_month_anchor_info;
 -- CREATE TABLE warehouse.dw_huya_month_anchor_info AS
-delete from warehouse.dw_huya_month_anchor_info where dt >='{month.start}' AND dt<='{month.end}';
+delete from warehouse.dw_huya_month_anchor_info where dt >='{start_date}' AND dt<='{end_date}';
 insert into warehouse.dw_huya_month_anchor_info
 select  
-  '{month.start}' as `dt`,
+  '{start_date}' as `dt`,
   t1.`channel_id`,
   `channel_num`,
   t1.`anchor_uid`,
@@ -69,16 +69,16 @@ select
 from `warehouse`.`dw_huya_day_anchor_info`  t1
 join stage.stage_huya_month_anchor_info t2
 on t1.dt= t2.dt and t1.anchor_uid = t2.anchor_uid and t1.channel_id= t2.channel_id
-where t1.dt >='{month.start}' AND t1.dt<='{month.end}';
+where t1.dt >='{start_date}' AND t1.dt<='{end_date}';
 
 
 
 -- DROP TABLE IF EXISTS warehouse.dw_huya_month_anchor_live;
 -- CREATE TABLE warehouse.dw_huya_month_anchor_live AS
-delete from warehouse.dw_huya_month_anchor_live where dt ='{month.start}';
+delete from warehouse.dw_huya_month_anchor_live where dt ='{start_date}';
 insert into warehouse.dw_huya_month_anchor_live
 SELECT
- '{month.start}' AS dt,
+ '{start_date}' AS dt,
   
   t1.channel_id,
   t2.channel_num,
@@ -117,66 +117,66 @@ FROM (select anchor_uid,channel_id,
   MAX(vir_coin_rate) AS vir_coin_rate,
   MAX(include_pf_amt) AS include_pf_amt,
   MAX(pf_amt_rate) AS pf_amt_rate
-  from `warehouse`.`dw_huya_day_anchor_live` where dt BETWEEN '{month.start}' AND '{month.end}' GROUP BY anchor_uid,channel_id) t1
+  from `warehouse`.`dw_huya_day_anchor_live` where dt BETWEEN '{start_date}' AND '{end_date}' GROUP BY anchor_uid,channel_id) t1
 LEFT JOIN warehouse.dw_huya_month_anchor_info t2
-        ON t1.channel_id = t2.channel_id AND t1.anchor_uid = t2.anchor_uid AND t2.dt = '{month.start}';
+        ON t1.channel_id = t2.channel_id AND t1.anchor_uid = t2.anchor_uid AND t2.dt = '{start_date}';
 
 
 -- DROP TABLE IF EXISTS stage.stage_huya_month_guild_live_revenue;
 -- CREATE TABLE stage.stage_huya_month_guild_live_revenue AS
-delete from stage.stage_huya_month_guild_live_revenue where dt ='{month.start}';
+delete from stage.stage_huya_month_guild_live_revenue where dt ='{start_date}';
 insert into stage.stage_huya_month_guild_live_revenue
 SELECT
   t1.`channel_id`,
   SUM(ifnull(`revenue`,0)) AS revenue,
-  '{month.start}' as dt
+  '{start_date}' as dt
 FROM `warehouse`.`dw_huya_day_guild_live` t1
-WHERE t1.dt BETWEEN '{month.start}' AND '{month.end}'
+WHERE t1.dt BETWEEN '{start_date}' AND '{end_date}'
 GROUP BY t1.channel_id;
 
 -- DROP TABLE IF EXISTS stage.stage_huya_month_guild_live_gift_income;
 -- CREATE TABLE stage.stage_huya_month_guild_live_gift_income AS
-delete from stage.stage_huya_month_guild_live_gift_income where dt ='{month.start}';
+delete from stage.stage_huya_month_guild_live_gift_income where dt ='{start_date}';
 insert into stage.stage_huya_month_guild_live_gift_income
 SELECT
   t1.`channel_id`,
   SUM(ifnull(`gift_income`,0)) AS gift_income,
-  '{month.start}' as dt
+  '{start_date}' as dt
 FROM `warehouse`.`dw_huya_day_guild_live` t1
-WHERE gift_calc_month=  '{month.start}' 
+WHERE gift_calc_month=  '{start_date}' 
 GROUP BY t1.channel_id;
 
 -- DROP TABLE IF EXISTS stage.stage_huya_month_guild_live_guard_income;
 -- CREATE TABLE stage.stage_huya_month_guild_live_guard_income AS
-delete from stage.stage_huya_month_guild_live_guard_income where dt ='{month.start}';
+delete from stage.stage_huya_month_guild_live_guard_income where dt ='{start_date}';
 insert into stage.stage_huya_month_guild_live_guard_income
 SELECT
   t1.`channel_id`,
   SUM(ifnull(`guard_income`,0)) AS guard_income,
-  '{month.start}' as dt
+  '{start_date}' as dt
 FROM `warehouse`.`dw_huya_day_guild_live` t1
-WHERE guard_calc_month = '{month.start}' 
+WHERE guard_calc_month = '{start_date}' 
 GROUP BY t1.channel_id;
 
 -- DROP TABLE IF EXISTS stage.stage_huya_month_guild_live_noble_income;
 -- CREATE TABLE stage.stage_huya_month_guild_live_noble_income AS
-delete from stage.stage_huya_month_guild_live_noble_income where dt ='{month.start}';
+delete from stage.stage_huya_month_guild_live_noble_income where dt ='{start_date}';
 insert into stage.stage_huya_month_guild_live_noble_income
 SELECT
   t1.`channel_id`,
   SUM(ifnull(`noble_income`,0)) AS noble_income,
-  '{month.start}' as dt
+  '{start_date}' as dt
 FROM `warehouse`.`dw_huya_day_guild_live` t1
-WHERE noble_calc_month = '{month.start}' 
+WHERE noble_calc_month = '{start_date}' 
 GROUP BY t1.channel_id;
 
 
 -- DROP TABLE IF EXISTS warehouse.dw_huya_month_guild_live;
 -- CREATE TABLE warehouse.dw_huya_month_guild_live AS
-delete from warehouse.dw_huya_month_guild_live where dt ='{month.start}';
+delete from warehouse.dw_huya_month_guild_live where dt ='{start_date}';
 insert into warehouse.dw_huya_month_guild_live
 SELECT
-  '{month.start}' AS dt,
+  '{start_date}' AS dt,
   t1.`channel_id`,
   t2.`channel_num`,
   t2.`platform_id`,
@@ -194,16 +194,16 @@ SELECT
   gift_income,
   guard_income,
   noble_income
-FROM (SELECT channel_id FROM `warehouse`.`dw_huya_day_guild_live` WHERE dt BETWEEN '{month.start}' AND '{month.end}' GROUP BY channel_id) t1
+FROM (SELECT channel_id FROM `warehouse`.`dw_huya_day_guild_live` WHERE dt BETWEEN '{start_date}' AND '{end_date}' GROUP BY channel_id) t1
 join stage.stage_huya_month_guild_live_revenue t3
-  on t3.channel_id = t1.channel_id and t3.dt = '{month.start}' 
+  on t3.channel_id = t1.channel_id and t3.dt = '{start_date}' 
 join stage.stage_huya_month_guild_live_gift_income t4
-  on t4.channel_id = t1.channel_id and t4.dt = '{month.start}'
+  on t4.channel_id = t1.channel_id and t4.dt = '{start_date}'
 join stage.stage_huya_month_guild_live_guard_income t5
-  on t5.channel_id = t1.channel_id and t5.dt = '{month.start}'
+  on t5.channel_id = t1.channel_id and t5.dt = '{start_date}'
 join stage.stage_huya_month_guild_live_noble_income t6
-  on t6.channel_id = t1.channel_id and t6.dt = '{month.start}'
+  on t6.channel_id = t1.channel_id and t6.dt = '{start_date}'
 LEFT OUTER JOIN `warehouse`.`dw_huya_month_guild_info` t2
-	ON t1.channel_id= t2.channel_id AND t2.dt= '{month.start}'
+	ON t1.channel_id= t2.channel_id AND t2.dt= '{start_date}'
 
 
