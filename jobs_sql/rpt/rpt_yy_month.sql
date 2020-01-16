762f61c3cf06d28fee8ff859cbad81fdcd3fca59
Rockie_Yuen <rockie_yuen@outlook.com>
2020-01-16 17:11:24 +0800

    修复yy当月流水缺失

--- jobs_sql/rpt/rpt_yy_month.sql
+++ jobs_sql/rpt/rpt_yy_month.sql
@@ -23,5 +23,28 @@ select t0.dt,
        t0.anchor_bluediamond_true - t0.guild_bluediamond_true AS anchor_income_orig
 from warehouse.dw_yy_month_guild_live t0
 lEFT JOIN warehouse.platform pf ON pf.id = t0.platform_id
+WHERE DATE_FORMAT(dt, '%Y-%m') <> DATE_FORMAT('{end_date}', '%Y-%m')
+UNION ALL
+select t0.dt,
+       t0.platform_id,
+       pf.platform_name AS platform,
+       t0.channel_num,
+       t0.anchor_cnt,
+       t0.anchor_live_cnt AS live_cnt,
+       -- 平台流水
+       t0.anchor_bluediamond AS anchor_bluediamond_revenue,
+       ROUND(t0.guild_commission_ture / 1000, 2) AS guild_commission_revenue,
+       ROUND((t0.anchor_bluediamond + t0.guild_commission_ture) / 500, 2) AS revenue,
+       t0.anchor_bluediamond + t0.guild_commission_ture AS revenue_orig,
+       -- 公会收入
+       t0.guild_income_bluediamond AS guild_income_bluediamond,
+       ROUND((t0.guild_income_bluediamond + t0.guild_commission_ture) / 1000, 2) AS guild_income,
+       t0.guild_income_bluediamond + t0.guild_commission_ture AS guild_income_orig,
+       -- 主播收入
+       ROUND((t0.anchor_bluediamond - t0.guild_income_bluediamond) / 1000, 2) AS anchor_income,
+       t0.anchor_bluediamond - t0.guild_income_bluediamond AS anchor_income_orig
+from warehouse.dw_yy_month_guild_live t0
+lEFT JOIN warehouse.platform pf ON pf.id = t0.platform_id
+WHERE DATE_FORMAT(dt, '%Y-%m') = DATE_FORMAT('end_date', '%Y-%m')
 ;
 
