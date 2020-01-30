d1ac40b08852834861541a96ba6f9860db3488e0
Rockie_Yuen <rockie_yuen@outlook.com>
2020-01-30 23:33:21 +0800

    up

--- script/run_sql/rs_test_sql.sql
+++ script/run_sql/rs_test_sql.sql
@@ -4,8 +4,8 @@ CREATE TABLE stage.rs_test_tmp0_20191229 AS
 SELECT *
 -- 测
 FROM warehouse.platform
-WHERE pay_from BETWEEN {start_date} AND {end_date}
-  AND ID IN ({platform_id})
+WHERE pay_from BETWEEN '{start_date}' AND '{end_date}'
+  AND ID IN ('{platform_id}')
 LIMIT 1
 ;
 
@@ -13,6 +13,6 @@ Drop TABLE IF EXISTS stage.rs_test_tmp1_20191229;
 CREATE TABLE stage.rs_test_tmp1_20191229 AS
 SELECT *
 FROM warehouse.platform
-WHERE ID IN ({platform_id})
+WHERE ID IN ('{platform_id}')
 LIMIT 1
 ;
