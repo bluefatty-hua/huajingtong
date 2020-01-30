061713c9d24beff27c5252af734d8cba78c41939
Rockie_Yuen <rockie_yuen@outlook.com>
2020-01-31 01:55:58 +0800

    up

--- script/run_sql/rs_test_sql.sql
+++ script/run_sql/rs_test_sql.sql
@@ -2,7 +2,7 @@ Drop TABLE IF EXISTS stage.rs_test_tmp0_20191229;
 CREATE TABLE stage.rs_test_tmp0_20191229 AS
 SELECT *
 -- 测
-FROM warehouse.platform
+FROM warehouse.ods_now_month_guild
 WHERE dt BETWEEN '{start_date}' AND '{end_date}'
 LIMIT 1
 ;
@@ -10,6 +10,6 @@ LIMIT 1
 Drop TABLE IF EXISTS stage.rs_test_tmp1_20191229;
 CREATE TABLE stage.rs_test_tmp1_20191229 AS
 SELECT *
-FROM warehouse.platform
+FROM warehouse.ods_now_month_guild
 LIMIT 1
 ;
