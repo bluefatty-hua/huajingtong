8909d2877860e13cc485a45e59f9b9af80309679
Rockie_Yuen <rockie_yuen@outlook.com>
2020-01-31 01:54:02 +0800

    up

--- script/run_sql/rs_test_sql.sql
+++ script/run_sql/rs_test_sql.sql
@@ -1,11 +1,9 @@
-
 Drop TABLE IF EXISTS stage.rs_test_tmp0_20191229;
 CREATE TABLE stage.rs_test_tmp0_20191229 AS
 SELECT *
 -- 测
 FROM warehouse.platform
-WHERE pay_from BETWEEN '{start_date}' AND '{end_date}'
-  AND ID IN ('{platform_id}')
+WHERE dt BETWEEN '{start_date}' AND '{end_date}'
 LIMIT 1
 ;
 
@@ -13,6 +11,5 @@ Drop TABLE IF EXISTS stage.rs_test_tmp1_20191229;
 CREATE TABLE stage.rs_test_tmp1_20191229 AS
 SELECT *
 FROM warehouse.platform
-WHERE ID IN ('{platform_id}')
 LIMIT 1
 ;
--- script/run_sql/run_sql_test.py
+++ script/run_sql/run_sql_test.py
@@ -2,9 +2,7 @@
 
 # 注意传参格式 eg: slq_file_path ['{'start_date':'2019-01-01','end_date':'2019-12-31','platform_id':'1004,1005,1000'}']
 import argparse
-
 import pymysql
-import sys
 from datetime import datetime
 from datetime import date
 from datetime import timedelta
@@ -13,13 +11,14 @@ import os
 import logging
 from log import init_logging
 from config import LOG_DIR
+from config import XJL_ETL_DB
 import sys
 
 reload(sys)
 sys.setdefaultencoding('utf8')
 
 # 链接数据库
-conn = pymysql.Connect(host='127.0.0.1', user='wh_user', password='Nd^93)9f@445Fv')
+conn = pymysql.Connect(host=XJL_ETL_DB['host'], port=XJL_ETL_DB['port'], user=XJL_ETL_DB['user'], password=XJL_ETL_DB['password'])
 cursor = conn.cursor()
 
 # 设置默认终止日期：前一天, 开始时间：7天前
@@ -36,12 +35,12 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-s', '--start_date', default=start_date, help='开始时间')
 parser.add_argument('-e', '--end_date', default=end_date, help='结束时间')
 parser.add_argument('-p', '--platform_id', default=platform_id, help='指定平台ID')
-parser.add_argument('-l', '--log_file', help='指定的log文件')
+parser.add_argument('-l', '--log_file', default=None, help='指定的log文件')
 parser.add_argument('-f', '--sql_file', help='指定执行SQL文件')
 args = parser.parse_args()
 
 # 配置LOG
-LOG_NAME = args.sql_file.split('/')[-1]
+LOG_NAME = args.sql_file.split('/')[-1].replace('.sql', '.log')
 if args.log_file == None:
     log_path = LOG_DIR + datetime.now().strftime('%Y%m%d')
     if not os.path.exists(log_path):
@@ -53,37 +52,42 @@ init_logging({'console_log_level': logging.INFO, 'file_log_level': logging.INFO,
 
 
 def run_sql(sql_param, file):
+    print('run')
     with io.open(file, 'r', encoding='utf8') as fr:
         for sql in fr.read().split(';'):
             try:
                 if len(sql) > 10:
-                    sql = (sql + ';').replace('/n', '').format(**sql_param)
+                    sql = (sql + ';').replace('/n', '').format(**sql_param) + '\n'
                     cursor.execute(sql)
                     conn.commit()
-                    logging.info('------------------------------DONE------------------------------\n', sql)
+                    logging.info('------------------------------DONE------------------------------\n{}\n'.format(sql))
             except Exception as err:
-                logging.info('------------------------------ERROR SQL------------------------------\n', sql)
+                logging.info('------------------------------ERROR SQL------------------------------\n{}\n'.format(sql))
                 logging.exception(err)
                 logging.info('--ROLLBACK-->>>>>>>>>>>>>>>>>')
                 conn.rollback()
+                break
 
 
 def format_param_dict(args):
-    param = dict()
-    param['start_date'] = args.start_date if args.start_date else start_date
-    param['end_date'] = args.end_date if args.end_date else end_date
-    param['platform_id'] = args.platform_id if args.platform_id else platform_id
-    logging.info('------------------------------PARAM------------------------------\n', param)
+    param = {
+        'start_date': args.start_date,
+        'end_date': args.end_date,
+        'platform_id': args.platform_id
+    }
+    logging.info('------------------------------PARAM------------------------------\n{}\n'.format(param))
     return param
 
 
 if __name__ == '__main__':
-    logging.info('------------------------------START------------------------------')
+    logging.info('------------------------------START------------------------------\n')
+    logging.info('start_time: {}'.format(datetime.now().strftime('%Y-%m-%d %H-%M-%S')))
+
     # 部署项目路径
     # project_path = '/services/xjl_etl/jobs_sql/'  # 项目跟目录/repo/xjl_etl/jobs_sql/
     project_path = '/services/xjl_etl/script/run_sql/'  # TEST
     sql_file = project_path + args.sql_file
-    logging.info('SQl_FILE>>>>>>>>>>>>>>>>>>>>>>>>>', sql_file)
+    logging.info('SQl_FILE: {}'.format(sql_file))
 
     # 格式化参数字典
     param_dic = format_param_dict(args)
@@ -91,6 +95,9 @@ if __name__ == '__main__':
     # 执行SQL脚本
     try:
         run_sql(param_dic, sql_file)
+        conn.commit()
     finally:
         cursor.close()
         conn.close()
+        logging.info('end_time: {}'.format(datetime.now().strftime('%Y-%m-%d %H-%M-%S')))
+
