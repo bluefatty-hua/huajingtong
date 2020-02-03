871db486f5023751921920869aa99ccdcec9f7ae
Rockie_Yuen <rockie_yuen@outlook.com>
2020-02-03 15:59:22 +0800

    up

--- jobs_sql/dw/dw_yy_day.sql
+++ jobs_sql/dw/dw_yy_day.sql
@@ -4,8 +4,8 @@
 -- warehouse.dw_yy_day_anchor_live
 
 
-# DROP TABLE IF EXISTS warehouse.dw_yy_day_anchor_live_duration;
-# CREATE TABLE warehouse.dw_yy_day_anchor_live_duration AS
+-- DROP TABLE IF EXISTS warehouse.dw_yy_day_anchor_live_duration;
+-- CREATE TABLE warehouse.dw_yy_day_anchor_live_duration AS
 DELETE
 FROM warehouse.dw_yy_day_anchor_live_duration
 WHERE dt BETWEEN '{start_date}' AND '{end_date}';
@@ -17,8 +17,8 @@ WHERE dt BETWEEN '{start_date}' AND '{end_date}'
 
 
 -- 按日汇总主播佣金收入及工会分成（佣金）
-# DROP TABLE IF EXISTS warehouse.dw_yy_day_anchor_live_commission;
-# CREATE TABLE warehouse.dw_yy_day_anchor_live_commission AS
+-- DROP TABLE IF EXISTS warehouse.dw_yy_day_anchor_live_commission;
+-- CREATE TABLE warehouse.dw_yy_day_anchor_live_commission AS
 DELETE
 FROM warehouse.dw_yy_day_anchor_live_commission
 WHERE dt BETWEEN '{start_date}' AND '{end_date}';
@@ -30,8 +30,8 @@ WHERE dt BETWEEN '{start_date}' AND '{end_date}'
 
 
 -- 主播收入（蓝钻）
-# DROP TABLE IF EXISTS warehouse.dw_yy_day_anchor_live_bluediamond;
-# CREATE TABLE warehouse.dw_yy_day_anchor_live_bluediamond AS
+-- DROP TABLE IF EXISTS warehouse.dw_yy_day_anchor_live_bluediamond;
+-- CREATE TABLE warehouse.dw_yy_day_anchor_live_bluediamond AS
 DELETE
 FROM warehouse.dw_yy_day_anchor_live_bluediamond
 WHERE dt BETWEEN '{start_date}' AND '{end_date}';
@@ -42,8 +42,8 @@ WHERE dt BETWEEN '{start_date}' AND '{end_date}'
 ;
 
 
-# DROP TABLE IF EXISTS warehouse.dw_yy_day_anchor_live;
-# CREATE TABLE warehouse.dw_yy_day_anchor_live AS
+-- DROP TABLE IF EXISTS warehouse.dw_yy_day_anchor_live;
+-- CREATE TABLE warehouse.dw_yy_day_anchor_live AS
 DELETE
 FROM warehouse.dw_yy_day_anchor_live
 WHERE dt BETWEEN '{start_date}' AND '{end_date}';
@@ -52,3 +52,4 @@ SELECT *
 FROM warehouse.ods_yy_day_anchor_live
 WHERE dt BETWEEN '{start_date}' AND '{end_date}'
 ;
+
