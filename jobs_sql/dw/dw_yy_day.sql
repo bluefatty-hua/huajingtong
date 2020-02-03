0f710531c0938c436e5776e47db4fa8aa0bf74ee
Rockie_Yuen <rockie_yuen@outlook.com>
2020-02-03 15:40:14 +0800

    up

--- jobs_sql/dw/dw_yy_day.sql
+++ jobs_sql/dw/dw_yy_day.sql
@@ -8,11 +8,11 @@
 # CREATE TABLE warehouse.dw_yy_day_anchor_live_duration AS
 DELETE
 FROM warehouse.dw_yy_day_anchor_live_duration
-WHERE dt BETWEEN '2019-01-01' AND '2020-02-02';
+WHERE dt BETWEEN '{start_date}' AND '{end_date}';
 INSERT INTO warehouse.dw_yy_day_anchor_live_duration
 SELECT *
 FROM warehouse.ods_yy_day_anchor_live_duration
-WHERE dt BETWEEN '2019-01-01' AND '2020-02-02'
+WHERE dt BETWEEN '{start_date}' AND '{end_date}'
 ;
 
 
@@ -21,11 +21,11 @@ WHERE dt BETWEEN '2019-01-01' AND '2020-02-02'
 # CREATE TABLE warehouse.dw_yy_day_anchor_live_commission AS
 DELETE
 FROM warehouse.dw_yy_day_anchor_live_commission
-WHERE dt BETWEEN '2019-01-01' AND '2020-02-02';
+WHERE dt BETWEEN '{start_date}' AND '{end_date}';
 INSERT INTO warehouse.dw_yy_day_anchor_live_commission
 SELECT *
 FROM warehouse.ods_yy_day_anchor_live_commission
-WHERE dt BETWEEN '2019-01-01' AND '2020-02-02'
+WHERE dt BETWEEN '{start_date}' AND '{end_date}'
 ;
 
 
@@ -34,11 +34,11 @@ WHERE dt BETWEEN '2019-01-01' AND '2020-02-02'
 # CREATE TABLE warehouse.dw_yy_day_anchor_live_bluediamond AS
 DELETE
 FROM warehouse.dw_yy_day_anchor_live_bluediamond
-WHERE dt BETWEEN '2019-01-01' AND '2020-02-02';
+WHERE dt BETWEEN '{start_date}' AND '{end_date}';
 INSERT INTO warehouse.dw_yy_day_anchor_live_bluediamond
 SELECT *
 FROM warehouse.ods_yy_day_anchor_live_bluediamond
-WHERE dt BETWEEN '2019-01-01' AND '2020-02-02'
+WHERE dt BETWEEN '{start_date}' AND '{end_date}'
 ;
 
 
@@ -46,9 +46,9 @@ WHERE dt BETWEEN '2019-01-01' AND '2020-02-02'
 # CREATE TABLE warehouse.dw_yy_day_anchor_live AS
 DELETE
 FROM warehouse.dw_yy_day_anchor_live
-WHERE dt BETWEEN '2019-01-01' AND '2020-02-02';
+WHERE dt BETWEEN '{start_date}' AND '{end_date}';
 INSERT INTO warehouse.dw_yy_day_anchor_live
 SELECT *
 FROM warehouse.ods_yy_day_anchor_live
-WHERE dt BETWEEN '2019-01-01' AND '2020-02-02'
+WHERE dt BETWEEN '{start_date}' AND '{end_date}'
 ;
