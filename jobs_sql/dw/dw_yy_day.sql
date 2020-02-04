4b4eb7674d38b6fc07b444a07162bedd738e422e
Rockie_Yuen <rockie_yuen@outlook.com>
2020-02-04 10:58:15 +0800

    add

--- jobs_sql/dw/dw_yy_day.sql
+++ jobs_sql/dw/dw_yy_day.sql
@@ -53,3 +53,34 @@ FROM warehouse.ods_yy_day_anchor_live
 WHERE dt BETWEEN '{start_date}' AND '{end_date}'
 ;
 
+
+-- 维度 日-公会
+-- 指标 主播数、开播主播数、主播流水、
+-- DROP TABLE IF EXISTS warehouse.dw_yy_day_guild_live;
+-- CREATE TABLE warehouse.dw_yy_day_guild_live AS
+DELETE
+FROM warehouse.dw_yy_day_guild_live
+WHERE dt BETWEEN '{start_date}' AND '{end_date}';
+INSERT INTO warehouse.dw_yy_day_guild_live
+SELECT al.dt,
+       al.platform_id,
+       al.backend_account_id,
+       al.channel_num,
+       al.comment,
+       COUNT(DISTINCT al.anchor_uid)                                                 AS anchor_cnt,
+       COUNT(DISTINCT CASE WHEN al.live_status = 1 THEN al.anchor_uid ELSE NULL END) AS anchor_live_cnt,
+       SUM(IF(al.duration > 0, al.duration, 0))                                      AS duration,
+       SUM(IF(al.bluediamond > 0, al.bluediamond, 0))                                AS bluediamond,
+       SUM(IF(al.bluediamond > 0, al.bluediamond * al.anchor_settle_rate, 0))        AS anchor_income_bluediamond,
+       SUM(IF(al.bluediamond > 0, al.bluediamond * (1 - al.anchor_settle_rate), 0))  AS guild_income_bluediamond,
+       SUM(IF(al.anchor_commission > 0, al.anchor_commission, 0))                    AS anchor_commission,
+       SUM(IF(al.guild_commission > 0, al.guild_commission, 0))                      AS guild_commission
+FROM warehouse.ods_yy_day_anchor_live al
+WHERE dt BETWEEN '{start_date}' AND '{end_date}'
+-- where comment <> 'from guild_anchor_sign_tran'
+GROUP BY al.dt,
+         al.platform_id,
+         al.backend_account_id,
+         al.channel_num,
+         comment
+;
