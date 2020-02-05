242f7cee28a82359544201b62515b08ec0c8a576
Rockie_Yuen <rockie_yuen@outlook.com>
2020-02-05 12:33:26 +0800

    up

--- jobs_sql/rpt/rpt_yy_month.sql
+++ jobs_sql/rpt/rpt_yy_month.sql
@@ -1,18 +1,19 @@
 -- 公会每月流水、公会收入、主播收入
 -- DROP TABLE IF EXISTS bireport.rpt_month_yy_guild;
 -- CREATE TABLE bireport.rpt_month_yy_guild AS
-DELETE
-FROM bireport.rpt_month_yy_guild
-WHERE DATE_FORMAT(dt, '%Y-%m') BETWEEN DATE_FORMAT('{start_date}', '%Y-%m') AND DATE_FORMAT('{end_date}', '%Y-%m');
-INSERT INTO bireport.rpt_month_yy_guild
-select t0.dt,
+# DELETE
+# FROM bireport.rpt_month_yy_guild
+# WHERE DATE_FORMAT(dt, '%Y-%m') BETWEEN DATE_FORMAT('2019-12-01', '%Y-%m') AND DATE_FORMAT('2020-02-03', '%Y-%m');
+# INSERT INTO bireport.rpt_month_yy_guild
+SELECT t0.dt,
        t0.platform_id,
        pf.platform_name                                                          AS platform,
        t0.channel_num,
        t0.anchor_cnt,
        t0.anchor_live_cnt                                                        AS live_cnt,
        -- 平台流水
-       t0.anchor_bluediamond_true                                                AS anchor_bluediamond_revenue,
+       t0.anchor_bluediamond_true,
+       t0.anchor_bluediamond                                                AS anchor_bluediamond_revenue,
        ROUND(t0.guild_commission_true / 1000, 2)                                 AS guild_commission_revenue,
        ROUND((t0.anchor_bluediamond_true + t0.guild_commission_true) / 500, 2)   AS revenue,
        t0.anchor_bluediamond_true + t0.guild_commission_true                     AS revenue_orig,
@@ -25,9 +26,10 @@ select t0.dt,
        t0.anchor_bluediamond_true - t0.guild_bluediamond_true                    AS anchor_income_orig
 FROM warehouse.dw_yy_month_guild_live t0
          lEFT JOIN warehouse.platform pf ON pf.id = t0.platform_id
-WHERE DATE_FORMAT(dt, '%Y-%m') BETWEEN DATE_FORMAT('{start_date}', '%Y-%m') AND DATE_FORMAT('{end_date}', '%Y-%m')
-  AND DATE_FORMAT(dt, '%Y-%m') <> DATE_FORMAT('{end_date}', '%Y-%m')
-UNION ALL
+WHERE comment = 'orig'
+  AND DATE_FORMAT(dt, '%Y-%m') BETWEEN DATE_FORMAT('2019-12-01', '%Y-%m') AND DATE_FORMAT('2020-02-03', '%Y-%m')
+  AND DATE_FORMAT(dt, '%Y-%m') <> DATE_FORMAT('2020-02-03', '%Y-%m')
+# UNION ALL
 SELECT t0.dt,
        t0.platform_id,
        pf.platform_name                                                          AS platform,
@@ -48,14 +50,15 @@ SELECT t0.dt,
        t0.anchor_bluediamond - t0.guild_income_bluediamond                       AS anchor_income_orig
 FROM warehouse.dw_yy_month_guild_live t0
          lEFT JOIN warehouse.platform pf ON pf.id = t0.platform_id
-WHERE DATE_FORMAT(dt, '%Y-%m') = DATE_FORMAT('{end_date}', '%Y-%m')
+WHERE comment = 'orig'
+  AND DATE_FORMAT(dt, '%Y-%m') = DATE_FORMAT('2020-02-03', '%Y-%m')
 ;
 
 
 DELETE
 FROM bireport.rpt_month_all_guild
 WHERE platform_id = 1000
-  AND DATE_FORMAT(dt, '%Y-%m') BETWEEN DATE_FORMAT('{start_date}', '%Y-%m') AND DATE_FORMAT('{end_date}', '%Y-%m');
+  AND DATE_FORMAT(dt, '%Y-%m') BETWEEN DATE_FORMAT('2019-12-01', '%Y-%m') AND DATE_FORMAT('2020-02-03', '%Y-%m');
 INSERT INTO bireport.rpt_month_all_guild
 SELECT dt,
        platform_id,
@@ -82,5 +85,5 @@ FROM (SELECT dt,
              anchor_income,
              anchor_income_orig
       FROM bireport.rpt_month_yy_guild) t
-WHERE DATE_FORMAT(dt, '%Y-%m') BETWEEN DATE_FORMAT('{start_date}', '%Y-%m') AND DATE_FORMAT('{end_date}', '%Y-%m')
+WHERE DATE_FORMAT(dt, '%Y-%m') BETWEEN DATE_FORMAT('2019-12-01', '%Y-%m') AND DATE_FORMAT('2020-02-03', '%Y-%m')
 ;
