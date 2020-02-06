372075fbf400d88f7cca85d4e9bffa7f6f8f3553
Rockie_Yuen <rockie_yuen@outlook.com>
2020-02-06 16:28:14 +0800

    up

--- jobs_sql/dw/dw_yy_month.sql
+++ jobs_sql/dw/dw_yy_month.sql
@@ -14,70 +14,70 @@
 
 
 -- 取出每月主播数（以每个月最后一天为准）
-DROP TABLE IF EXISTS stage.stage_yy_month_guild_info_max_day;
-CREATE TABLE stage.stage_yy_month_guild_info_max_day AS
-SELECT CONCAT(DATE_FORMAT(dt, '%Y-%m'), '-01') AS dt,
-       backend_account_id,
-       MAX(dt)                                 AS max_dt
-FROM warehouse.dw_yy_day_anchor_live
-WHERE comment = 'orig'
-  AND dt BETWEEN DATE_FORMAT(CONCAT(YEAR('{start_date}'), '-', MONTH('{start_date}'), '-01'),
-                             '%Y-%m-%d') AND '{end_date}'
-GROUP BY CONCAT(DATE_FORMAT(dt, '%Y-%m'), '-01'),
-         backend_account_id
-;
+# DROP TABLE IF EXISTS stage.stage_yy_month_guild_info_max_day;
+# CREATE TABLE stage.stage_yy_month_guild_info_max_day AS
+# SELECT CONCAT(DATE_FORMAT(dt, '%Y-%m'), '-01') AS dt,
+#        backend_account_id,
+#        MAX(dt)                                 AS max_dt
+# FROM warehouse.dw_yy_day_anchor_live
+# WHERE comment = 'orig'
+#   AND dt BETWEEN DATE_FORMAT(CONCAT(YEAR('{start_date}'), '-', MONTH('{start_date}'), '-01'),
+#                              '%Y-%m-%d') AND '{end_date}'
+# GROUP BY CONCAT(DATE_FORMAT(dt, '%Y-%m'), '-01'),
+#          backend_account_id
+# ;
 
 
 -- DROP TABLE IF EXISTS stage.stage_yy_month_anchor_info;
 -- CREATE TABLE stage.stage_yy_month_anchor_info AS
-DELETE
-FROM stage.stage_yy_month_anchor_info
-WHERE dt BETWEEN DATE_FORMAT(CONCAT(YEAR('{start_date}'), '-', MONTH('{start_date}'), '-01'),
-                             '%Y-%m-%d') AND '{end_date}';
-INSERT INTO stage.stage_yy_month_anchor_info
-SELECT t0.dt, t0.backend_account_id, anchor_uid
-FROM stage.stage_yy_month_guild_info_max_day t0
-         LEFT JOIN (SELECT dt, backend_account_id, anchor_uid
-                    FROM warehouse.dw_yy_day_anchor_live
-                    WHERE comment = 'orig'
-                      AND dt BETWEEN DATE_FORMAT(CONCAT(YEAR('{start_date}'), '-', MONTH('{start_date}'), '-01'),
-                                                 '%Y-%m-%d') AND '{end_date}') t1
-                   ON t0.backend_account_id = t1.backend_account_id AND t0.max_dt = t1.dt
-;
+# DELETE
+# FROM stage.stage_yy_month_anchor_info
+# WHERE dt BETWEEN DATE_FORMAT(CONCAT(YEAR('{start_date}'), '-', MONTH('{start_date}'), '-01'),
+#                              '%Y-%m-%d') AND '{end_date}';
+# INSERT INTO stage.stage_yy_month_anchor_info
+# SELECT t0.dt, t0.backend_account_id, anchor_uid
+# FROM stage.stage_yy_month_guild_info_max_day t0
+#          LEFT JOIN (SELECT dt, backend_account_id, anchor_uid
+#                     FROM warehouse.dw_yy_day_anchor_live
+#                     WHERE comment = 'orig'
+#                       AND dt BETWEEN DATE_FORMAT(CONCAT(YEAR('{start_date}'), '-', MONTH('{start_date}'), '-01'),
+#                                                  '%Y-%m-%d') AND '{end_date}') t1
+#                    ON t0.backend_account_id = t1.backend_account_id AND t0.max_dt = t1.dt
+# ;
 
 
 -- 取出每日主播数（以每个月最后一天为准）
 # DROP TABLE IF EXISTS stage.stage_yy_day_anchor_live;
 # CREATE TABLE stage.stage_yy_day_anchor_live
-DELETE
-FROM stage.stage_yy_day_anchor_live
-WHERE dt BETWEEN DATE_FORMAT(CONCAT(YEAR('{start_date}'), '-', MONTH('{start_date}'), '-01'),
-                             '%Y-%m-%d') AND '{end_date}';
-INSERT INTO stage.stage_yy_day_anchor_live
-SELECT al.dt,
-       al.platform_id,
-       al.platform_name,
-       ai.backend_account_id,
-       al.channel_num,
-       al.anchor_uid,
-       al.anchor_no,
-       al.anchor_nick_name,
-       al.anchor_type,
-       al.anchor_type_text,
-       al.duration,
-       al.live_status,
-       al.bluediamond,
-       al.anchor_commission,
-       al.guild_commission,
-       al.anchor_settle_rate,
-       al.comment
-FROM warehouse.dw_yy_day_anchor_live al
-         INNER JOIN stage.stage_yy_month_anchor_info ai ON DATE_FORMAT(ai.dt, '%Y-%m') = DATE_FORMAT(al.dt, '%Y-%m') AND
-                                                         ai.backend_account_id = al.backend_account_id AND
-                                                         ai.anchor_uid = al.anchor_uid
-WHERE ai.dt BETWEEN CONCAT(DATE_FORMAT('{start_date}', '%Y-%m'), '-01') AND '{end_date}'
-  AND al.dt BETWEEN CONCAT(DATE_FORMAT('{start_date}', '%Y-%m'), '-01') AND '{end_date}'
-;
+# DELETE
+# FROM stage.stage_yy_day_anchor_live
+# WHERE dt BETWEEN DATE_FORMAT(CONCAT(YEAR('{start_date}'), '-', MONTH('{start_date}'), '-01'),
+#                              '%Y-%m-%d') AND '{end_date}';
+# INSERT INTO stage.stage_yy_day_anchor_live
+# SELECT al.dt,
+#        al.platform_id,
+#        al.platform_name,
+#        ai.backend_account_id,
+#        al.channel_num,
+#        al.anchor_uid,
+#        al.anchor_no,
+#        al.anchor_nick_name,
+#        al.anchor_type,
+#        al.anchor_type_text,
+#        al.duration,
+#        al.live_status,
+#        al.bluediamond,
+#        al.anchor_commission,
+#        al.guild_commission,
+#        al.anchor_settle_rate,
+#        al.comment
+# FROM warehouse.dw_yy_day_anchor_live al
+#          INNER JOIN stage.stage_yy_month_anchor_info ai ON DATE_FORMAT(ai.dt, '%Y-%m') = DATE_FORMAT(al.dt, '%Y-%m') AND
+#                                                          ai.backend_account_id = al.backend_account_id AND
+#                                                          ai.anchor_uid = al.anchor_uid
+# WHERE ai.dt BETWEEN CONCAT(DATE_FORMAT('{start_date}', '%Y-%m'), '-01') AND '{end_date}'
+#   AND al.dt BETWEEN CONCAT(DATE_FORMAT('{start_date}', '%Y-%m'), '-01') AND '{end_date}'
+# ;
 
 
 -- 公会
@@ -165,7 +165,8 @@ SELECT ad.dt,
        IFNULL(ad.anchor_commission, 0)         AS anchor_commission,
        IFNULL(ad.guild_commission, 0)          AS guild_commission,
        IFNULL(gc.guild_commission, 0)          AS guild_commission_true,
-       ad.dt_cnt
+       ad.dt_cnt,
+       comment
 FROM (SELECT DATE_FORMAT(CONCAT(YEAR(dt), '-', MONTH(dt), '-01'), '%Y-%m-%d')        AS dt,
              platform_id,
              platform_name,
@@ -173,6 +174,7 @@ FROM (SELECT DATE_FORMAT(CONCAT(YEAR(dt), '-', MONTH(dt), '-01'), '%Y-%m-%d')   
              channel_num,
              anchor_no,
              anchor_uid,
+             comment,
              COUNT(CASE WHEN live_status = 1 THEN dt ELSE NULL END)                  AS live_days,
              SUM(CASE WHEN duration >= 0 THEN duration ELSE 0 END)                   AS duration,
              SUM(CASE WHEN bluediamond >= 0 THEN bluediamond ELSE 0 END)             AS anchor_bluediamond,
@@ -185,7 +187,7 @@ FROM (SELECT DATE_FORMAT(CONCAT(YEAR(dt), '-', MONTH(dt), '-01'), '%Y-%m-%d')   
              SUM(CASE WHEN anchor_commission >= 0 THEN anchor_commission ELSE 0 END) AS anchor_commission,
              SUM(CASE WHEN guild_commission >= 0 THEN guild_commission ELSE 0 END)   AS guild_commission,
              COUNT(DISTINCT dt)                                                      AS dt_cnt
-      FROM stage.stage_yy_day_anchor_live
+      FROM warehouse.dw_yy_day_anchor_live
       WHERE dt BETWEEN DATE_FORMAT(CONCAT(YEAR('{start_date}'), '-', MONTH('{start_date}'), '-01'),
                                    '%Y-%m-%d') AND '{end_date}'
       GROUP BY DATE_FORMAT(CONCAT(YEAR(dt), '-', MONTH(dt), '-01'), '%Y-%m-%d'),
@@ -194,7 +196,8 @@ FROM (SELECT DATE_FORMAT(CONCAT(YEAR(dt), '-', MONTH(dt), '-01'), '%Y-%m-%d')   
                backend_account_id,
                channel_num,
                anchor_no,
-               anchor_uid) ad
+               anchor_uid,
+               comment) ad
          LEFT JOIN warehouse.dw_yy_month_guild_anchor_live_bluediamond gv
                    ON ad.dt = gv.dt AND ad.anchor_no = gv.anchor_no AND ad.backend_account_id = gv.backend_account_id
          LEFT JOIN warehouse.dw_yy_month_guild_anchor_live_commission gc
@@ -277,13 +280,15 @@ SELECT ad.dt,
        IFNULL(ad.anchor_commission, 0)         AS anchor_commission,
        IFNULL(ad.guild_commission, 0)          AS guild_commission,
        IFNULL(gc.guild_commission, 0)          AS guild_commission_true,
-       IFNULL(ad.dt_cnt, 0)                    AS dt_cnt
+       IFNULL(ad.dt_cnt, 0)                    AS dt_cnt,
+       comment
 FROM (
          SELECT DATE_FORMAT(CONCAT(YEAR(t.dt), '-', MONTH(t.dt), '-01'), '%Y-%m-%d')        AS dt,
                 t.platform_id,
                 t.platform_name,
                 t.backend_account_id,
-                max(channel_num)                                                            as channel_num,
+                comment,
+                MAX(channel_num)                                                            as channel_num,
                 COUNT(DISTINCT t.anchor_no)                                                 AS anchor_cnt,
                 COUNT(DISTINCT CASE WHEN t.live_status = 1 THEN t.anchor_no ELSE NULL END)  AS anchor_live_cnt,
                 SUM(CASE WHEN t.bluediamond >= 0 THEN t.bluediamond ELSE 0 END)             AS anchor_bluediamond,
@@ -296,13 +301,14 @@ FROM (
                 SUM(CASE WHEN t.anchor_commission >= 0 THEN t.anchor_commission ELSE 0 END) AS anchor_commission,
                 SUM(CASE WHEN t.guild_commission >= 0 THEN t.guild_commission ELSE 0 END)   AS guild_commission,
                 COUNT(DISTINCT t.dt)                                                        AS dt_cnt
-         FROM stage.stage_yy_day_anchor_live t
+         FROM warehouse.dw_yy_day_anchor_live t
          WHERE t.dt BETWEEN DATE_FORMAT(CONCAT(YEAR('{start_date}'), '-', MONTH('{start_date}'), '-01'),
                                         '%Y-%m-%d') AND '{end_date}'
          GROUP BY DATE_FORMAT(CONCAT(YEAR(t.dt), '-', MONTH(t.dt), '-01'), '%Y-%m-%d'),
                   t.platform_id,
                   t.platform_name,
-                  t.backend_account_id
+                  t.backend_account_id,
+                  comment
      ) ad
          LEFT JOIN warehouse.dw_yy_month_guild_live_bluediamond gv
                    ON ad.dt = gv.dt AND ad.backend_account_id = gv.backend_account_id
@@ -389,7 +395,8 @@ SELECT ad.dt,
        ad.anchor_commission,
        ad.guild_commission,
        gc.guild_commission   AS guild_commission_true,
-       ad.dt_cnt
+       ad.dt_cnt,
+       comment
 FROM (
          SELECT DATE_FORMAT(CONCAT(YEAR(t.dt), '-', MONTH(t.dt), '-01'), '%Y-%m-%d')        AS dt,
                 t.platform_id,
@@ -397,14 +404,15 @@ FROM (
                 t.anchor_no,
                 t.anchor_uid,
                 t.backend_account_id,
-                max(channel_num)                                                            as channel_num,
+                t.comment,
+                MAX(t.channel_num)                                                            as channel_num,
                 COUNT(DISTINCT CASE WHEN t.live_status = 1 THEN t.dt ELSE NULL END)         AS live_days,
                 SUM(CASE WHEN duration >= 0 THEN duration ELSE 0 END)                       AS duration,
                 SUM(CASE WHEN t.bluediamond >= 0 THEN t.bluediamond ELSE 0 END)             AS anchor_bluediamond,
                 SUM(CASE WHEN t.anchor_commission >= 0 THEN t.anchor_commission ELSE 0 END) AS anchor_commission,
                 SUM(CASE WHEN t.guild_commission >= 0 THEN t.guild_commission ELSE 0 END)   AS guild_commission,
                 COUNT(DISTINCT t.dt)                                                        AS dt_cnt
-         FROM stage.stage_yy_day_anchor_live t
+         FROM warehouse.dw_yy_day_anchor_live t
          WHERE dt BETWEEN DATE_FORMAT(CONCAT(YEAR('{start_date}'), '-', MONTH('{start_date}'), '-01'),
                                       '%Y-%m-%d') AND '{end_date}'
          GROUP BY DATE_FORMAT(CONCAT(YEAR(t.dt), '-', MONTH(t.dt), '-01'), '%Y-%m-%d'),
@@ -412,7 +420,8 @@ FROM (
                   t.platform_name,
                   t.anchor_no,
                   t.anchor_uid,
-                  t.backend_account_id) ad
+                  t.backend_account_id,
+                  t.comment) ad
          LEFT JOIN warehouse.dw_yy_month_anchor_live_bluediamond gv ON ad.dt = gv.dt AND ad.anchor_no = gv.anchor_no
          LEFT JOIN warehouse.dw_yy_month_anchor_live_commission gc ON ad.dt = gc.dt AND ad.anchor_no = gc.anchor_no
 ;
