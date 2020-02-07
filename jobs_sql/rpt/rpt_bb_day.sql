911fe9157997754064c37bb7a1ff4a68ea943dbc
Page <4061207@qq.com>
2020-02-07 17:22:24 +0800

    update jobs_sql/rpt/rpt_bb_day.sql.

--- jobs_sql/rpt/rpt_bb_day.sql
+++ jobs_sql/rpt/rpt_bb_day.sql
@@ -1,4 +1,27 @@
 DELETE
+FROM bireport.rpt_day_all
+WHERE platform = 'bilibili'
+  AND dt BETWEEN '{start_date}' AND '{end_date}';
+INSERT INTO bireport.rpt_day_all
+SELECT t.dt,
+       t.platform_name,
+       SUM(anchor_cnt)                                                         AS anchor_cnt,
+       SUM(live_cnt)                                                           AS live_cnt,
+       SUM(t.revenue) / 1000                                         AS revenue,
+       SUM(CASE
+               WHEN t.backend_account_id = 3 THEN
+                       t.revenue * gr.anchor_income_rate + t.send_coin + t.special_coin
+               ELSE t.anchor_income + t.send_coin + t.special_coin END) / 1000 AS anchor_income,
+       SUM(t.revenue * gr.guild_income_rate) / 1000                  AS guild_income
+FROM warehouse.dw_bb_day_guild_live t
+         LEFT JOIN stage.bb_guild_income_rate gr ON
+    t.backend_account_id = gr.backend_account_id
+WHERE t.dt BETWEEN '{start_date}' AND '{end_date}'
+GROUP BY t.dt,
+         t.platform_name
+;
+
+DELETE
 FROM bireport.rpt_day_bb_guild
 WHERE dt BETWEEN '{start_date}' AND '{end_date}';
 INSERT INTO bireport.rpt_day_bb_guild
@@ -26,26 +49,13 @@ WHERE t.dt BETWEEN '{start_date}' AND '{end_date}'
 ;
 
 
-DELETE
-FROM bireport.rpt_day_all
-WHERE platform = 'bilibili'
-  AND dt BETWEEN '{start_date}' AND '{end_date}';
-INSERT INTO bireport.rpt_day_all
-SELECT t.dt,
-       t.platform_name,
-       SUM(anchor_cnt)                                                         AS anchor_cnt,
-       SUM(live_cnt)                                                           AS live_cnt,
-       SUM(t.revenue) / 1000                                         AS revenue,
-       SUM(CASE
-               WHEN t.backend_account_id = 3 THEN
-                       t.revenue * gr.anchor_income_rate + t.send_coin + t.special_coin
-               ELSE t.anchor_income + t.send_coin + t.special_coin END) / 1000 AS anchor_income,
-       SUM(t.revenue * gr.guild_income_rate) / 1000                  AS guild_income
-FROM warehouse.dw_bb_day_guild_live t
-         LEFT JOIN stage.bb_guild_income_rate gr ON
-    t.backend_account_id = gr.backend_account_id
-WHERE t.dt BETWEEN '{start_date}' AND '{end_date}'
-GROUP BY t.dt,
-         t.platform_name
-;
+REPLACE INTO bireport.`rpt_day_bb_guild`
+(dt,backend_account_id,remark,anchor_cnt,live_cnt,revenue,guild_income,anchor_income)
+SELECT  dt,0 as backend_account_id ,'all' AS remark,anchor_cnt,
+live_cnt,
+revenue,
+guild_income,
+anchor_income
+FROM  bireport.rpt_day_all
+WHERE platform='bilibili' AND  dt BETWEEN '{start_date}' AND '{end_date}';
 
