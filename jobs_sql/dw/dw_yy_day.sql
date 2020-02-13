6bd28481da92de628c81a1f0305e2b03047c5240
Rockie_Yuen <rockie_yuen@outlook.com>
2020-02-14 05:21:09 +0800

    up

--- jobs_sql/dw/dw_yy_day.sql
+++ jobs_sql/dw/dw_yy_day.sql
@@ -53,13 +53,13 @@ SELECT al.*,
        ams.min_sign_dt,
        -- 通过判断主播最小注册时间和最小开播时间，取两者之间最小的时间作为判断新老主播条件，两者为NULL则为‘未知’
        warehouse.ANCHOR_NEW_OLD(aml.min_live_dt, ams.min_sign_dt, al.dt, 180)    AS newold_state,
-       mal.duration         AS last_month_duration,
-       mal.live_days,
+       IFNULL(mal.duration, 0)         AS last_month_duration,
+       IFNULL(mal.live_days, 0),
        -- 开播天数大于等于20天且开播时长大于等于20小时（t-1月累计）
        CASE
            WHEN mal.live_days >= 20 AND mal.duration >= 60 * 60 * 60 THEN '活跃主播'
            ELSE '非活跃主播' END AS active_state,
-       mal.revenue          AS last_month_revenue,
+       IFNULL(mal.revenue, 0)          AS last_month_revenue,
        -- 主播流水分级（t-1月）
        CASE
            WHEN mal.revenue * 2 / 1000 / 10000 >= 50 THEN '50+'
@@ -68,7 +68,7 @@ SELECT al.*,
            WHEN mal.revenue * 2 / 1000 / 10000 > 0 THEN '0-3'
            ELSE '0' END     AS revenue_level
 FROM warehouse.ods_yy_day_anchor_live al
-         LEFT JOIN stage.stage_yy_anchor_min_live_dt aml ON al.anchor_uid = aml.anchor_uid
+         LEFT JOIN stage.stage_yy_anchor_min_live_dt aml ON al.anchor_uid = aml.anchor_no
          LEFT JOIN stage.stage_yy_anchor_min_sign_dt ams ON al.anchor_uid = ams.anchor_uid
          LEFT JOIN stage.stage_yy_month_anchor_live mal
                    ON mal.dt = DATE_FORMAT(DATE_SUB(al.dt, INTERVAL 1 MONTH), '%Y-%m-01') AND
