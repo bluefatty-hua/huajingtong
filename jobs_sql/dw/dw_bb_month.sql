71b31d25e9ca7446d437c0a48fff558523bf2536
Rockie_Yuen <rockie_yuen@outlook.com>
2020-02-15 10:48:57 +0800

    up

--- jobs_sql/dw/dw_bb_month.sql
+++ jobs_sql/dw/dw_bb_month.sql
@@ -80,51 +80,67 @@ GROUP BY DATE_FORMAT(CONCAT(YEAR(t.dt), '-', MONTH(t.dt), '-01'), '%Y-%m-%d'),
 
 -- 汇总维度 月-公会
 -- 汇总指标 主播数，开播主播数，虚拟币收入
+# DROP TABLE IF EXISTS warehouse.dw_bb_month_guild_live_true;
+# CREATE TABLE warehouse.dw_bb_month_guild_live_true AS
+DELETE
+FROM warehouse.dw_bb_month_guild_live_true
+WHERE dt BETWEEN DATE_FORMAT('{start_date}', '%Y-%m-01') AND '{end_date}';
+INSERT INTO warehouse.dw_bb_month_guild_live_true
+SELECT dt,
+       backend_account_id,
+       guild_salart_rmb   AS guild_salart_rmb_true,
+       guild_virtual_coin AS guild_virtual_coin_true,
+       type,
+       anchor_income,
+       anchor_base_coin,
+       guild_award_coin,
+       operate_award_punish_coin,
+       special_coin,
+       guild_change_coin,
+       anchor_change_coin,
+       comment
+FROM warehouse.ods_bb_month_guild_live
+WHERE type = '公会总收益'
+  AND dt BETWEEN DATE_FORMAT('{start_date}', '%Y-%m-01') AND '{end_date}'
+;
+
+
+-- 注意backend_account_id = 3  数据中没有主播收入
 -- DROP TABLE IF EXISTS warehouse.dw_bb_month_guild_live;
 -- CREATE TABLE warehouse.dw_bb_month_guild_live AS
 DELETE
 FROM warehouse.dw_bb_month_guild_live
-WHERE DATE_FORMAT(dt, '%Y%m') BETWEEN DATE_FORMAT('{start_date}', '%Y%m') AND DATE_FORMAT('{end_date}', '%Y%m');
+WHERE DATE_FORMAT(dt, '%Y-%m-01') BETWEEN DATE_FORMAT('{start_date}', '%Y-%m-01') AND DATE_FORMAT('{end_date}', '%Y-%m-01');
 INSERT INTO warehouse.dw_bb_month_guild_live
-SELECT t.dt,
-       t.platform_id,
-       t.platform_name,
-       t.backend_account_id,
-       t.anchor_cnt,
-       t.anchor_live_cnt,
-       t1.type,
-       IFNULL(t1.guild_salart_rmb, 0)          AS guild_salart_rmb_true,
-       IFNULL(t1.guild_virtual_coin, 0)        AS guild_virtual_coin_true,
-       IFNULL(t.anchor_virtual_coin, 0)        AS virtual_coin_revenue,
-       IFNULL(t1.anchor_income, 0)             AS anchor_income_true,
-       t.anchor_income,
-       IFNULL(t1.anchor_base_coin, 0)          AS anchor_base_coin_true,
-       t.anchor_base_coin,
-       IFNULL(t1.guild_award_coin, 0)          AS guild_award_coin_true,
-       IFNULL(t1.operate_award_punish_coin, 0) AS operate_award_punish_coin_true,
-       t.operate_award_punish_coin,
-       IFNULL(t1.special_coin, 0)              AS special_coin_true,
-       t.special_coin,
-       IFNULL(t1.anchor_change_coin, 0)        AS anchor_change_coi,
-       IFNULL(t1.guild_change_coin, 0)         AS guild_change_coin
-FROM (SELECT DATE_FORMAT(CONCAT(YEAR(t.dt), '-', MONTH(t.dt), '-01'), '%Y-%m-%d')       AS dt,
-             t.platform_id,
-             t.platform_name,
-             t.backend_account_id,
-             COUNT(DISTINCT t.anchor_no)                                                AS anchor_cnt,
-             COUNT(DISTINCT CASE WHEN t.live_status = 1 THEN t.anchor_no ELSE NULL END) AS anchor_live_cnt,
-             SUM(t.anchor_total_coin)                                                   AS anchor_virtual_coin,
-             SUM(t.anchor_income)                                                       AS anchor_income,
-             SUM(t.special_coin)                                                        AS special_coin,
-             SUM(t.send_coin)                                                           AS operate_award_punish_coin,
-             SUM(t.anchor_base_coin)                                                    AS anchor_base_coin
-      FROM warehouse.ods_bb_day_anchor_live t
+SELECT DATE_FORMAT(al.dt, '%Y-%m-01')                                               AS dt,
+       al.platform_id,
+       al.platform_name,
+       al.backend_account_id,
+       al.month_newold_state,
+       al.active_state,
+       al.revenue_level,
+       COUNT(DISTINCT al.anchor_no)                                                 AS anchor_cnt,
+       COUNT(DISTINCT CASE WHEN al.live_status = 1 THEN al.anchor_no ELSE NULL END) AS anchor_live_cnt,
+       SUM(al.duration)                                                             AS duration,
+       SUM(al.anchor_total_coin)                                                    AS revenue,
+       SUM(al.anchor_income)                                                        AS anchor_income,
+       SUM(al.send_coin)                                                            AS operate_award_punish_coin,
+       SUM(al.anchor_base_coin)                                                     AS anchor_base_coin,
+       SUM(al.special_coin)                                                         AS special_coin
+FROM (SELECT *,
+             warehouse.ANCHOR_NEW_OLD(min_live_dt, min_sign_dt, CASE
+                                                                    WHEN dt < DATE_FORMAT('{end_date}', '%Y-%m-01')
+                                                                        THEN LAST_DAY(dt)
+                                                                    ELSE dt END, 180) AS month_newold_state
+      FROM warehouse.dw_bb_day_anchor_live
       WHERE (contract_status <> 2 OR contract_status IS NULL)
-      GROUP BY DATE_FORMAT(CONCAT(YEAR(t.dt), '-', MONTH(t.dt), '-01'), '%Y-%m-%d'),
-               t.platform_id,
-               t.platform_name,
-               t.backend_account_id) t
-         LEFT JOIN (SELECT * FROM warehouse.ods_bb_month_guild_live WHERE type = '公会总收益') t1
-                   ON t.dt = t1.dt AND t.backend_account_id = t1.backend_account_id
-WHERE DATE_FORMAT(t.dt, '%Y%m') BETWEEN DATE_FORMAT('{start_date}', '%Y%m') AND DATE_FORMAT('{end_date}', '%Y%m')
+        AND DATE_FORMAT(dt, '%Y-%m-01') BETWEEN DATE_FORMAT('{start_date}', '%Y-%m-01') AND DATE_FORMAT('{end_date}', '%Y-%m-01')
+     ) al
+GROUP BY DATE_FORMAT(dt, '%Y-%m-01'),
+         al.platform_id,
+         al.platform_name,
+         al.backend_account_id,
+         al.month_newold_state,
+         al.active_state,
+         al.revenue_level
 ;
