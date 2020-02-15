bff8c4346c592d2897561b12ac9073499d1f1cb7
Page <4061207@qq.com>
2020-02-15 22:25:48 +0800

    补充yy month报表用的表

--- jobs_sql/rpt/rpt_yy_month.sql
+++ jobs_sql/rpt/rpt_yy_month.sql
@@ -282,3 +282,53 @@ FROM bireport.rpt_month_yy_guild_new
 WHERE channel_num = 'all'
   AND dt = '{month}'
 ;
+
+
+-- 报表用，计算上周、上月同期数据---
+REPLACE INTO bireport.rpt_month_yy_guild_new_view
+SELECT 
+	t1.dt,
+	t1.channel_num,
+	t1.revenue_level,
+	t1.newold_state,
+	t1.active_state,
+	t1.anchor_cnt,
+	t3.anchor_cnt AS anchor_cnt_lastmonth,
+	t1.live_cnt,
+	t3.live_cnt AS live_cnt_lastmonth,
+	IF(t1.anchor_cnt>0,ROUND(t1.live_cnt/t1.anchor_cnt,3),0) AS live_ratio,
+	IF(t3.anchor_cnt>0,ROUND(t3.live_cnt/t3.anchor_cnt,3),0) AS live_ratio_lastmonth,
+	ROUND(t1.duration/3600,1) AS duration,
+	ROUND(t3.duration/3600,1) AS duration_lastmonth,
+	t1.revenue,
+	t3.revenue AS revenue_lastmonth,
+	IF(t1.live_cnt>0,ROUND(t1.`revenue`/t1.live_cnt,0),0) AS revenue_per_live,
+	IF(t3.live_cnt>0,ROUND(t3.`revenue`/t3.live_cnt,0),0) AS revenue_per_live_lastmonth,
+	0 AS `guild_income`,
+	0 AS `anchor_income` 
+FROM bireport.rpt_month_yy_guild_new t1
+LEFT JOIN bireport.rpt_month_yy_guild_new t3
+	ON t1.dt - INTERVAL 1 MONTH = t3.dt
+	AND t1.channel_num = t3.channel_num
+	AND t1.revenue_level = t3.revenue_level
+	AND t1.newold_state = t3.newold_state
+	AND t1.active_state = t3.active_state
+  WHERE t1.dt = '{month}';
+
+
+
+
+-- 报表用，计算指标占比---
+replace into `bireport`.`rpt_month_yy_guild_new_view_compare` 
+select * from
+(SELECT dt,channel_num,revenue_level,newold_state,active_state,'主播数' as idx,
+anchor_cnt as val FROM `bireport`.`rpt_month_yy_guild_new` where dt = '{month}' and  revenue_level!='all'
+union 
+SELECT dt,channel_num,revenue_level,newold_state,active_state,'开播数' as idx,
+live_cnt as val FROM `bireport`.`rpt_month_yy_guild_new` where dt = '{month}' and revenue_level!='all'
+union 
+SELECT dt,channel_num,revenue_level,newold_state,active_state,'流水' as idx,
+revenue as val FROM `bireport`.`rpt_month_yy_guild_new` where dt = '{month}' and revenue_level!='all'
+union 
+SELECT dt,channel_num,revenue_level,newold_state,active_state,'开播人均流水' as idx,
+round(revenue/live_cnt,0) as val FROM `bireport`.`rpt_month_yy_guild_new` where dt = '{month}' and  revenue_level!='all' and live_cnt>0) t
