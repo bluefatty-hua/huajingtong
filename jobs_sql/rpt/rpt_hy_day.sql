085684502e8cb3b17b3885337a0c8d69f4d56f63
Page <4061207@qq.com>
2020-02-15 20:44:52 +0800

    huya补充报表数据

--- jobs_sql/rpt/rpt_hy_day.sql
+++ jobs_sql/rpt/rpt_hy_day.sql
@@ -86,9 +86,7 @@ FROM bireport.rpt_day_hy_guild_new
 WHERE dt BETWEEN '{start_date}' AND '{end_date}';
 INSERT INTO bireport.rpt_day_hy_guild_new
 select dt,
-       platform_id,
-       platform_name                                                           AS platform,
-       channel_id,
+       channel_num,
        revenue_level,
        newold_state,
        active_state,
@@ -99,22 +97,18 @@ select dt,
 from warehouse.dw_huya_day_anchor_live
 WHERE dt BETWEEN '{start_date}' AND '{end_date}'
 GROUP BY dt,
-         platform_id,
-         platform_name,
-         channel_id,
+         channel_num,
          revenue_level,
          newold_state,
          active_state
 ;
 
 
-REPLACE INTO bireport.rpt_day_hy_guild_new (dt, platform_id, platform, channel_id, revenue_level,
+REPLACE INTO bireport.rpt_day_hy_guild_new (dt, platform_id, platform, channel_num, revenue_level,
                                             newold_state, active_state, anchor_cnt, live_cnt, duration, revenue)
 SELECT *
 FROM (SELECT dt,
-             MAX(platform_id)             AS platform_id,
-             MAX(platform)                AS platform,
-             IFNULL(channel_id, 'all')    AS channel_id,
+             IFNULL(channel_num, 'all')    AS channel_num,
              IFNULL(revenue_level, 'all') AS revenue_level,
              IFNULL(newold_state, 'all')  AS newold_state,
              IFNULL(active_state, 'all')  AS active_state,
@@ -123,17 +117,15 @@ FROM (SELECT dt,
              SUM(duration)                AS duration,
              SUM(revenue)                 AS revenue
       FROM bireport.rpt_day_hy_guild_new
-      WHERE (channel_id != 'all' OR revenue_level != 'all' OR newold_state != 'all' OR active_state != 'all')
+      WHERE (channel_num != 'all' OR revenue_level != 'all' OR newold_state != 'all' OR active_state != 'all')
         AND dt BETWEEN '{start_date}' AND '{end_date}'
-      GROUP BY dt, channel_id, revenue_level, newold_state, active_state
+      GROUP BY dt, channel_num, revenue_level, newold_state, active_state
       WITH ROLLUP
 
       UNION
 
       SELECT dt,
-             MAX(platform_id)             AS platform_id,
-             MAX(platform)                AS platform,
-             IFNULL(channel_id, 'all')    AS channel_id,
+             IFNULL(channel_num, 'all')    AS channel_num,
              IFNULL(revenue_level, 'all') AS revenue_level,
              IFNULL(newold_state, 'all')  AS newold_state,
              IFNULL(active_state, 'all')  AS active_state,
@@ -142,17 +134,15 @@ FROM (SELECT dt,
              SUM(duration)                AS duration,
              SUM(revenue)                 AS revenue
       FROM bireport.rpt_day_hy_guild_new
-      WHERE (channel_id != 'all' OR revenue_level != 'all' OR newold_state != 'all' OR active_state != 'all')
+      WHERE (channel_num != 'all' OR revenue_level != 'all' OR newold_state != 'all' OR active_state != 'all')
         AND dt BETWEEN '{start_date}' AND '{end_date}'
-      GROUP BY dt, revenue_level, newold_state, active_state, channel_id
+      GROUP BY dt, revenue_level, newold_state, active_state, channel_num
       WITH ROLLUP
 
       UNION
 
       SELECT dt,
-             MAX(platform_id)             AS platform_id,
-             MAX(platform)                AS platform,
-             IFNULL(channel_id, 'all')    AS channel_id,
+             IFNULL(channel_num, 'all')    AS channel_num,
              IFNULL(revenue_level, 'all') AS revenue_level,
              IFNULL(newold_state, 'all')  AS newold_state,
              IFNULL(active_state, 'all')  AS active_state,
@@ -161,17 +151,15 @@ FROM (SELECT dt,
              SUM(duration)                AS duration,
              SUM(revenue)                 AS revenue
       FROM bireport.rpt_day_hy_guild_new
-      WHERE (channel_id != 'all' OR revenue_level != 'all' OR newold_state != 'all' OR active_state != 'all')
+      WHERE (channel_num != 'all' OR revenue_level != 'all' OR newold_state != 'all' OR active_state != 'all')
         AND dt BETWEEN '{start_date}' AND '{end_date}'
-      GROUP BY dt, newold_state, active_state, channel_id, revenue_level
+      GROUP BY dt, newold_state, active_state, channel_num, revenue_level
       WITH ROLLUP
 
       UNION
 
       SELECT dt,
-             MAX(platform_id)             AS platform_id,
-             MAX(platform)                AS platform,
-             IFNULL(channel_id, 'all')    AS channel_id,
+             IFNULL(channel_num, 'all')    AS channel_num,
              IFNULL(revenue_level, 'all') AS revenue_level,
              IFNULL(newold_state, 'all')  AS newold_state,
              IFNULL(active_state, 'all')  AS active_state,
@@ -180,16 +168,14 @@ FROM (SELECT dt,
              SUM(duration)                AS duration,
              SUM(revenue)                 AS revenue
       FROM bireport.rpt_day_hy_guild_new
-      WHERE (channel_id != 'all' OR revenue_level != 'all' OR newold_state != 'all' OR active_state != 'all')
+      WHERE (channel_num != 'all' OR revenue_level != 'all' OR newold_state != 'all' OR active_state != 'all')
         AND dt BETWEEN '{start_date}' AND '{end_date}'
-      GROUP BY dt, active_state, channel_id, revenue_level, newold_state
+      GROUP BY dt, active_state, channel_num, revenue_level, newold_state
       WITH ROLLUP
 
       UNION ALL
       SELECT dt,
-             MAX(platform_id)             AS platform_id,
-             MAX(platform)                AS platform,
-             IFNULL(channel_id, 'all')    AS channel_id,
+             IFNULL(channel_num, 'all')    AS channel_num,
              IFNULL(revenue_level, 'all') AS revenue_level,
              IFNULL(newold_state, 'all')  AS newold_state,
              IFNULL(active_state, 'all')  AS active_state,
@@ -198,7 +184,7 @@ FROM (SELECT dt,
              SUM(duration)                AS duration,
              SUM(revenue)                 AS revenue
       FROM bireport.rpt_day_hy_guild_new
-      WHERE (channel_id != 'all' OR revenue_level != 'all' OR newold_state != 'all' OR active_state != 'all')
+      WHERE (channel_num != 'all' OR revenue_level != 'all' OR newold_state != 'all' OR active_state != 'all')
         AND dt BETWEEN '{start_date}' AND '{end_date}'
       GROUP BY dt, active_state, revenue_level, channel_id, newold_state
       WITH ROLLUP
@@ -222,7 +208,7 @@ REPLACE INTO bireport.rpt_day_all_new
  guild_income,
  anchor_income)
 SELECT dt,
-       platform,
+       'HUYA' as platform,
        revenue_level,
        newold_state,
        active_state,
@@ -233,6 +219,65 @@ SELECT dt,
        0 AS guild_income,
        0 AS anchor_income
 FROM bireport.rpt_day_hy_guild_new
-WHERE channel_id = 'all'
+WHERE channel_num = 'all'
   AND dt BETWEEN '{start_date}' AND '{end_date}'
 ;
+
+-- 报表用，计算上周、上月同期数据---
+REPLACE INTO bireport.rpt_day_huya_guild_new_view
+SELECT 
+	t1.dt,
+	t1.channel_num,
+	t1.revenue_level,
+	t1.newold_state,
+	t1.active_state,
+	t1.anchor_cnt,
+	t2.anchor_cnt AS anchor_cnt_lastweek,
+	t3.anchor_cnt AS anchor_cnt_lastmonth,
+	t1.live_cnt,
+	t2.live_cnt AS live_cnt_lastweek,
+	t3.live_cnt AS live_cnt_lastmonth,
+	IF(t1.anchor_cnt>0,ROUND(t1.live_cnt/t1.anchor_cnt,3),0) AS live_ratio,
+	IF(t2.anchor_cnt>0,ROUND(t2.live_cnt/t2.anchor_cnt,3),0) AS live_ratio_lastweek,
+	IF(t3.anchor_cnt>0,ROUND(t3.live_cnt/t3.anchor_cnt,3),0) AS live_ratio_lastmonth,
+	ROUND(t1.duration/3600,1) AS duration,
+	ROUND(t2.duration/3600,1) AS duration_lastweek,
+	ROUND(t3.duration/3600,1) AS duration_lastmonth,
+	t1.revenue,
+	t2.revenue AS revenue_lastweek,
+	t3.revenue AS revenue_lastmonth,
+	IF(t1.live_cnt>0,ROUND(t1.`revenue`/t1.live_cnt,0),0) AS revenue_per_live,
+	IF(t2.live_cnt>0,ROUND(t2.`revenue`/t2.live_cnt,0),0) AS revenue_per_live_lastweek,
+	IF(t3.live_cnt>0,ROUND(t3.`revenue`/t3.live_cnt,0),0) AS revenue_per_live_lastmonth,
+	t1.`guild_income`,
+	t1.`anchor_income` 
+FROM bireport.rpt_day_huya_guild_new t1
+LEFT JOIN bireport.rpt_day_huya_guild_new t2
+	ON t1.dt - INTERVAL 7 DAY = t2.dt
+	AND t1.channel_num = t2.channel_num
+	AND t1.revenue_level = t2.revenue_level
+	AND t1.newold_state = t2.newold_state
+	AND t1.active_state = t2.active_state
+LEFT JOIN bireport.rpt_day_huya_guild_new t3
+	ON t1.dt - INTERVAL 1 MONTH = t3.dt
+	AND t1.channel_num = t3.channel_num
+	AND t1.revenue_level = t3.revenue_level
+	AND t1.newold_state = t3.newold_state
+	AND t1.active_state = t3.active_state
+  WHERE t1.dt BETWEEN '{start_date}' AND '{end_date}';
+
+
+-- 报表用，计算指标占比---
+replace into `rpt_day_huya_guild_new_view_compare` 
+select * from
+(SELECT dt,channel_num,revenue_level,newold_state,active_state,'主播数' as idx,
+anchor_cnt as val FROM `bireport`.`rpt_day_huya_guild_new` where .dt BETWEEN '{start_date}' AND '{end_date}' and  revenue_level!='all'
+union 
+SELECT dt,channel_num,revenue_level,newold_state,active_state,'开播数' as idx,
+live_cnt as val FROM `bireport`.`rpt_day_huya_guild_new` where dt BETWEEN '{start_date}' AND '{end_date}' and revenue_level!='all'
+union 
+SELECT dt,channel_num,revenue_level,newold_state,active_state,'流水' as idx,
+revenue as val FROM `bireport`.`rpt_day_huya_guild_new` where dt BETWEEN '{start_date}' AND '{end_date}' and revenue_level!='all'
+union 
+SELECT dt,channel_num,revenue_level,newold_state,active_state,'开播数' as idx,
+round(revenue/live_cnt,0) as val FROM `bireport`.`rpt_day_huyaa_guild_new` where dt BETWEEN '{start_date}' AND '{end_date}' and  revenue_level!='all' and live_cnt>0) t
