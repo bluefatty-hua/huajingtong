eccd42697715b224d6d5da05a7d346cf2f554c04
Rockie_Yuen <rockie_yuen@outlook.com>
2020-02-15 11:07:09 +0800

    up

--- jobs_sql/dw/dw_now_anchor_tags.sql
+++ jobs_sql/dw/dw_now_anchor_tags.sql
@@ -2,13 +2,21 @@
 -- DROP TABLE IF EXISTS stage.stage_now_anchor_min_live_dt;
 -- CREATE TABLE stage.stage_now_anchor_min_live_dt
 INSERT IGNORE INTO stage.stage_now_anchor_min_live_dt
-SELECT al.platform_id,
-       al.anchor_no,
-       MIN(dt) AS min_live_dt
-FROM warehouse.ods_now_day_anchor_live al
-WHERE al.live_status = 1
-GROUP BY al.platform_id,
-         al.anchor_no
+SELECT 1003 AS platform_id,
+       t.anchor_no,
+       MIN(t.min_live_dt)
+FROM (SELECT al.anchor_no,
+             MIN(dt) AS min_live_dt
+      FROM warehouse.ods_now_day_anchor_live al
+      WHERE al.live_status = 1
+      GROUP BY al.anchor_no
+      UNION
+      SELECT yj.uid             AS anchor_no,
+             yj.first_live_time AS min_live_time
+      FROM warehouse.ods_yujia_anchor_list yj
+      WHERE platform = 'NOW'
+        AND first_live_time != '0000-00-00') t
+GROUP BY t.anchor_no
 ;
 
 
@@ -16,18 +24,22 @@ GROUP BY al.platform_id,
 -- DROP TABLE IF EXISTS stage.stage_now_anchor_min_sign_dt;
 -- CREATE TABLE stage.stage_now_anchor_min_sign_dt
 INSERT IGNORE INTO stage.stage_now_anchor_min_sign_dt
-SELECT t0.platform_id,
-       t0.anchor_no,
-       CASE WHEN t1.sign_time IS NULL THEN t0.min_sign_dt ELSE t1.sign_time END AS min_sign_dt
-FROM (
-         SELECT al.platform_id,
-                al.anchor_no,
-                MIN(DATE(contract_sign_time)) AS min_sign_dt
-         FROM warehouse.ods_now_day_anchor_live al
-         WHERE al.contract_sign_time IS NOT NULL
-         GROUP BY al.platform_id,
-                  al.anchor_no) t0
-         LEFT JOIN (SELECT * FROM warehouse.ods_yujia_anchor_list WHERE platform = 'NOW') t1 ON t0.anchor_no = t1.uid
+SELECT 1003             AS platform_id,
+       t.anchor_no,
+       MIN(min_sign_dt) AS min_sign_dt
+FROM (SELECT al.anchor_no,
+             MIN(DATE(al.contract_sign_time)) AS min_sign_dt
+      FROM warehouse.ods_now_day_anchor_live al
+      WHERE al.contract_sign_time IS NOT NULL
+      GROUP BY al.anchor_no
+      UNION
+      SELECT yj.uid       AS anchor_no,
+             yj.sign_time AS min_sign_dt
+      FROM warehouse.ods_yujia_anchor_list yj
+      WHERE platform = 'NOW'
+        AND yj.sign_time <> '0000-00-00'
+     ) t
+GROUP BY t.anchor_no
 ;
 
 
