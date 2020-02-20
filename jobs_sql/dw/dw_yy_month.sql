a3817d85186c1033b9fcb9c437c6efa6f7061850
Rockie_Yuen <rockie_yuen@outlook.com>
2020-02-20 18:20:24 +0800

    up

--- jobs_sql/dw/dw_yy_month.sql
+++ jobs_sql/dw/dw_yy_month.sql
@@ -312,7 +312,7 @@ FROM (SELECT *,
              warehouse.ANCHOR_NEW_OLD(min_live_dt, min_sign_dt, CASE
                                                                     WHEN dt < DATE_FORMAT('{end_date}', '%Y-%m-01')
                                                                         THEN LAST_DAY(dt)
-                                                                    ELSE dt END, 180
+                                                                    ELSE '{end_date}' END, 180
                  ) AS month_newold_state
       FROM warehouse.dw_yy_day_anchor_live
       WHERE DATE_FORMAT(dt, '%Y-%m-01') BETWEEN DATE_FORMAT('{start_date}', '%Y-%m-01') AND DATE_FORMAT('{end_date}', '%Y-%m-01')
