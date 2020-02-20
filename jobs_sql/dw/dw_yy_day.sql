0069a2ada0b64a8a27884835fe4dc1c8fa709910
Rockie_Yuen <rockie_yuen@outlook.com>
2020-02-20 18:38:59 +0800

    up

--- jobs_sql/dw/dw_yy_day.sql
+++ jobs_sql/dw/dw_yy_day.sql
@@ -52,21 +52,21 @@ SELECT al.*,
        aml.min_live_dt,
        ams.min_sign_dt,
        -- 通过判断主播最小注册时间和最小开播时间，取两者之间最小的时间作为判断新老主播条件，两者为NULL则为‘未知’
-       warehouse.ANCHOR_NEW_OLD(aml.min_live_dt, ams.min_sign_dt, al.dt, 180)    AS newold_state,
-       IFNULL(mal.duration, 0)         AS last_month_duration,
-       IFNULL(mal.live_days, 0),
+       warehouse.ANCHOR_NEW_OLD(aml.min_live_dt, ams.min_sign_dt, al.dt, 180) AS newold_state,
+       IFNULL(mal.duration, 0)                                                AS last_month_duration,
+       IFNULL(mal.live_days, 0)                                               AS live_days,
        -- 开播天数大于等于20天且开播时长大于等于60小时（t-1月累计）
        CASE
            WHEN mal.live_days >= 20 AND mal.duration >= 60 * 60 * 60 THEN '活跃主播'
-           ELSE '非活跃主播' END AS active_state,
-       IFNULL(mal.revenue, 0)          AS last_month_revenue,
+           ELSE '非活跃主播' END                                                   AS active_state,
+       IFNULL(mal.revenue, 0)                                                 AS last_month_revenue,
        -- 主播流水分级（t-1月）
        CASE
            WHEN mal.revenue * 2 / 1000 / 10000 >= 50 THEN '50+'
            WHEN mal.revenue * 2 / 1000 / 10000 >= 10 THEN '10-50'
            WHEN mal.revenue * 2 / 1000 / 10000 >= 3 THEN '3-10'
            WHEN mal.revenue * 2 / 1000 / 10000 > 0 THEN '0-3'
-           ELSE '0' END     AS revenue_level
+           ELSE '0' END                                                       AS revenue_level
 FROM warehouse.ods_yy_day_anchor_live al
          LEFT JOIN stage.stage_yy_anchor_min_live_dt aml ON al.anchor_uid = aml.anchor_no
          LEFT JOIN stage.stage_yy_anchor_min_sign_dt ams ON al.anchor_uid = ams.anchor_uid
