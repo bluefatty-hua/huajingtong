36368d5fb353b03ff7683f9e6f436977d5064599
Rockie_Yuen <rockie_yuen@outlook.com>
2020-02-21 17:03:52 +0800

    up

--- jobs_sql/rpt/rpt_bb_day.sql
+++ jobs_sql/rpt/rpt_bb_day.sql
@@ -114,129 +114,141 @@ REPLACE INTO bireport.rpt_day_bb_guild_new (dt, platform_id, platform, backend_a
                                             newold_state, active_state, anchor_cnt, live_cnt, duration, revenue,
                                             revenue_orig, guild_income, guild_income_orig, anchor_income,
                                             anchor_income_orig)
-SELECT *
+SELECT t.dt,
+       t.platform_id,
+       t.platform,
+       t.backend_account_id,
+       IFNULL(ai.remark, 'all') AS remark,
+       t.revenue_level,
+       t.newold_state,
+       t.active_state,
+       t.anchor_cnt,
+       t.live_cnt,
+       t.duration,
+       t.revenue,
+       t.revenue_orig,
+       t.guild_income,
+       t.guild_income_orig,
+       t.anchor_income,
+       t.anchor_income_orig
 FROM (SELECT dt,
-             MAX(platform_id)             AS platform_id,
-             MAX(platform)                AS platform,
-             MAX(backend_account_id)      AS backend_account_id,
-             IFNULL(remark, 'all')        AS remark,
-             IFNULL(revenue_level, 'all') AS revenue_level,
-             IFNULL(newold_state, 'all')  AS newold_state,
-             IFNULL(active_state, 'all')  AS active_state,
-             SUM(anchor_cnt)              AS anchor_cnt,
-             SUM(live_cnt)                AS live_cnt,
-             SUM(duration)                AS duration,
-             SUM(revenue)                 AS revenue,
-             SUM(revenue_orig)            AS revenue_orig,
-             SUM(guild_income)            AS guild_income,
-             SUM(guild_income_orig)       AS guild_income_orig,
-             SUM(anchor_income)           AS anchor_income,
-             SUM(anchor_income_orig)      AS anchor_income_orig
+             MAX(platform_id)              AS platform_id,
+             MAX(platform)                 AS platform,
+             IFNULL(backend_account_id, 0) AS backend_account_id,
+             IFNULL(revenue_level, 'all')  AS revenue_level,
+             IFNULL(newold_state, 'all')   AS newold_state,
+             IFNULL(active_state, 'all')   AS active_state,
+             SUM(anchor_cnt)               AS anchor_cnt,
+             SUM(live_cnt)                 AS live_cnt,
+             SUM(duration)                 AS duration,
+             SUM(revenue)                  AS revenue,
+             SUM(revenue_orig)             AS revenue_orig,
+             SUM(guild_income)             AS guild_income,
+             SUM(guild_income_orig)        AS guild_income_orig,
+             SUM(anchor_income)            AS anchor_income,
+             SUM(anchor_income_orig)       AS anchor_income_orig
       FROM bireport.rpt_day_bb_guild_new
-      WHERE (backend_account_id != 'all' OR revenue_level != 'all' OR newold_state != 'all' OR active_state != 'all')
+      WHERE (backend_account_id != 0 OR revenue_level != 'all' OR newold_state != 'all' OR active_state != 'all')
         AND dt BETWEEN '{start_date}' AND '{end_date}'
-      GROUP BY dt, remark, revenue_level, newold_state, active_state
+      GROUP BY dt, backend_account_id, revenue_level, newold_state, active_state
       WITH ROLLUP
 
       UNION
 
       SELECT dt,
-             MAX(platform_id)             AS platform_id,
-             MAX(platform)                AS platform,
-             MAX(backend_account_id)      AS backend_account_id,
-             IFNULL(remark, 'all')        AS remark,
-             IFNULL(revenue_level, 'all') AS revenue_level,
-             IFNULL(newold_state, 'all')  AS newold_state,
-             IFNULL(active_state, 'all')  AS active_state,
-             SUM(anchor_cnt)              AS anchor_cnt,
-             SUM(live_cnt)                AS live_cnt,
-             SUM(duration)                AS duration,
-             SUM(revenue)                 AS revenue,
-             SUM(revenue_orig)            AS revenue_orig,
-             SUM(guild_income)            AS guild_income,
-             SUM(guild_income_orig)       AS guild_income_orig,
-             SUM(anchor_income)           AS anchor_income,
-             SUM(anchor_income_orig)      AS anchor_income_orig
+             MAX(platform_id)              AS platform_id,
+             MAX(platform)                 AS platform,
+             IFNULL(backend_account_id, 0) AS backend_account_id,
+             IFNULL(revenue_level, 'all')  AS revenue_level,
+             IFNULL(newold_state, 'all')   AS newold_state,
+             IFNULL(active_state, 'all')   AS active_state,
+             SUM(anchor_cnt)               AS anchor_cnt,
+             SUM(live_cnt)                 AS live_cnt,
+             SUM(duration)                 AS duration,
+             SUM(revenue)                  AS revenue,
+             SUM(revenue_orig)             AS revenue_orig,
+             SUM(guild_income)             AS guild_income,
+             SUM(guild_income_orig)        AS guild_income_orig,
+             SUM(anchor_income)            AS anchor_income,
+             SUM(anchor_income_orig)       AS anchor_income_orig
       FROM bireport.rpt_day_bb_guild_new
-      WHERE (backend_account_id != 'all' OR revenue_level != 'all' OR newold_state != 'all' OR active_state != 'all')
+      WHERE (backend_account_id != 0 OR revenue_level != 'all' OR newold_state != 'all' OR active_state != 'all')
         AND dt BETWEEN '{start_date}' AND '{end_date}'
-      GROUP BY dt, revenue_level, newold_state, active_state, remark
+      GROUP BY dt, revenue_level, newold_state, active_state, backend_account_id
       WITH ROLLUP
 
       UNION
 
       SELECT dt,
-             MAX(platform_id)             AS platform_id,
-             MAX(platform)                AS platform,
-             MAX(backend_account_id)      AS backend_account_id,
-             IFNULL(remark, 'all')        AS remark,
-             IFNULL(revenue_level, 'all') AS revenue_level,
-             IFNULL(newold_state, 'all')  AS newold_state,
-             IFNULL(active_state, 'all')  AS active_state,
-             SUM(anchor_cnt)              AS anchor_cnt,
-             SUM(live_cnt)                AS live_cnt,
-             SUM(duration)                AS duration,
-             SUM(revenue)                 AS revenue,
-             SUM(revenue_orig)            AS revenue_orig,
-             SUM(guild_income)            AS guild_income,
-             SUM(guild_income_orig)       AS guild_income_orig,
-             SUM(anchor_income)           AS anchor_income,
-             SUM(anchor_income_orig)      AS anchor_income_orig
+             MAX(platform_id)              AS platform_id,
+             MAX(platform)                 AS platform,
+             IFNULL(backend_account_id, 0) AS backend_account_id,
+             IFNULL(revenue_level, 'all')  AS revenue_level,
+             IFNULL(newold_state, 'all')   AS newold_state,
+             IFNULL(active_state, 'all')   AS active_state,
+             SUM(anchor_cnt)               AS anchor_cnt,
+             SUM(live_cnt)                 AS live_cnt,
+             SUM(duration)                 AS duration,
+             SUM(revenue)                  AS revenue,
+             SUM(revenue_orig)             AS revenue_orig,
+             SUM(guild_income)             AS guild_income,
+             SUM(guild_income_orig)        AS guild_income_orig,
+             SUM(anchor_income)            AS anchor_income,
+             SUM(anchor_income_orig)       AS anchor_income_orig
       FROM bireport.rpt_day_bb_guild_new
-      WHERE (backend_account_id != 'all' OR revenue_level != 'all' OR newold_state != 'all' OR active_state != 'all')
+      WHERE (backend_account_id != 0 OR revenue_level != 'all' OR newold_state != 'all' OR active_state != 'all')
         AND dt BETWEEN '{start_date}' AND '{end_date}'
-      GROUP BY dt, newold_state, active_state, remark, revenue_level
+      GROUP BY dt, newold_state, active_state, backend_account_id, revenue_level
       WITH ROLLUP
 
       UNION
 
       SELECT dt,
-             MAX(platform_id)             AS platform_id,
-             MAX(platform)                AS platform,
-             MAX(backend_account_id)      AS backend_account_id,
-             IFNULL(remark, 'all')        AS remark,
-             IFNULL(revenue_level, 'all') AS revenue_level,
-             IFNULL(newold_state, 'all')  AS newold_state,
-             IFNULL(active_state, 'all')  AS active_state,
-             SUM(anchor_cnt)              AS anchor_cnt,
-             SUM(live_cnt)                AS live_cnt,
-             SUM(duration)                AS duration,
-             SUM(revenue)                 AS revenue,
-             SUM(revenue_orig)            AS revenue_orig,
-             SUM(guild_income)            AS guild_income,
-             SUM(guild_income_orig)       AS guild_income_orig,
-             SUM(anchor_income)           AS anchor_income,
-             SUM(anchor_income_orig)      AS anchor_income_orig
+             MAX(platform_id)              AS platform_id,
+             MAX(platform)                 AS platform,
+             IFNULL(backend_account_id, 0) AS backend_account_id,
+             IFNULL(revenue_level, 'all')  AS revenue_level,
+             IFNULL(newold_state, 'all')   AS newold_state,
+             IFNULL(active_state, 'all')   AS active_state,
+             SUM(anchor_cnt)               AS anchor_cnt,
+             SUM(live_cnt)                 AS live_cnt,
+             SUM(duration)                 AS duration,
+             SUM(revenue)                  AS revenue,
+             SUM(revenue_orig)             AS revenue_orig,
+             SUM(guild_income)             AS guild_income,
+             SUM(guild_income_orig)        AS guild_income_orig,
+             SUM(anchor_income)            AS anchor_income,
+             SUM(anchor_income_orig)       AS anchor_income_orig
       FROM bireport.rpt_day_bb_guild_new
-      WHERE (backend_account_id != 'all' OR revenue_level != 'all' OR newold_state != 'all' OR active_state != 'all')
+      WHERE (backend_account_id != 0 OR revenue_level != 'all' OR newold_state != 'all' OR active_state != 'all')
         AND dt BETWEEN '{start_date}' AND '{end_date}'
-      GROUP BY dt, active_state, remark, revenue_level, newold_state
+      GROUP BY dt, active_state, backend_account_id, revenue_level, newold_state
       WITH ROLLUP
 
       UNION ALL
-    SELECT dt,
-             MAX(platform_id)             AS platform_id,
-             MAX(platform)                AS platform,
-             MAX(backend_account_id)      AS backend_account_id,
-             IFNULL(remark, 'all')        AS remark,
-             IFNULL(revenue_level, 'all') AS revenue_level,
-             IFNULL(newold_state, 'all')  AS newold_state,
-             IFNULL(active_state, 'all')  AS active_state,
-             SUM(anchor_cnt)              AS anchor_cnt,
-             SUM(live_cnt)                AS live_cnt,
-             SUM(duration)                AS duration,
-             SUM(revenue)                 AS revenue,
-             SUM(revenue_orig)            AS revenue_orig,
-             SUM(guild_income)            AS guild_income,
-             SUM(guild_income_orig)       AS guild_income_orig,
-             SUM(anchor_income)           AS anchor_income,
-             SUM(anchor_income_orig)      AS anchor_income_orig
+      SELECT dt,
+             MAX(platform_id)              AS platform_id,
+             MAX(platform)                 AS platform,
+             IFNULL(backend_account_id, 0) AS backend_account_id,
+             IFNULL(revenue_level, 'all')  AS revenue_level,
+             IFNULL(newold_state, 'all')   AS newold_state,
+             IFNULL(active_state, 'all')   AS active_state,
+             SUM(anchor_cnt)               AS anchor_cnt,
+             SUM(live_cnt)                 AS live_cnt,
+             SUM(duration)                 AS duration,
+             SUM(revenue)                  AS revenue,
+             SUM(revenue_orig)             AS revenue_orig,
+             SUM(guild_income)             AS guild_income,
+             SUM(guild_income_orig)        AS guild_income_orig,
+             SUM(anchor_income)            AS anchor_income,
+             SUM(anchor_income_orig)       AS anchor_income_orig
       FROM bireport.rpt_day_bb_guild_new
-      WHERE (backend_account_id != 'all' OR revenue_level != 'all' OR newold_state != 'all' OR active_state != 'all')
+      WHERE (backend_account_id != 0 OR revenue_level != 'all' OR newold_state != 'all' OR active_state != 'all')
         AND dt BETWEEN '{start_date}' AND '{end_date}'
-      GROUP BY dt, active_state, revenue_level, remark, newold_state
+      GROUP BY dt, backend_account_id, newold_state, revenue_level, active_state
       WITH ROLLUP
      ) t
+         LEFT JOIN warehouse.ods_bb_account_info ai ON t.backend_account_id = ai.backend_account_id
 WHERE dt IS NOT NULL
 ;
 
@@ -274,30 +286,29 @@ WHERE remark = 'all'
 
 -- 报表用，计算上周、上月同期数据---
 REPLACE INTO bireport.rpt_day_bb_guild_new_view
-SELECT 
-	t1.dt,
-	t1.remark,
-	t1.revenue_level,
-	t1.newold_state,
-	t1.active_state,
-	t1.anchor_cnt,
-	t2.anchor_cnt AS anchor_cnt_lastweek,
-	t3.anchor_cnt AS anchor_cnt_lastmonth,
-	t1.live_cnt,
-	t2.live_cnt AS live_cnt_lastweek,
-	t3.live_cnt AS live_cnt_lastmonth,
-	IF(t1.anchor_cnt>0,ROUND(t1.live_cnt/t1.anchor_cnt,3),0) AS live_ratio,
-	IF(t2.anchor_cnt>0,ROUND(t2.live_cnt/t2.anchor_cnt,3),0) AS live_ratio_lastweek,
-	IF(t3.anchor_cnt>0,ROUND(t3.live_cnt/t3.anchor_cnt,3),0) AS live_ratio_lastmonth,
-	ROUND(t1.duration/3600,1) AS duration,
-	ROUND(t2.duration/3600,1) AS duration_lastweek,
-	ROUND(t3.duration/3600,1) AS duration_lastmonth,
-	t1.revenue,
-	t2.revenue AS revenue_lastweek,
-	t3.revenue AS revenue_lastmonth,
-	IF(t1.live_cnt>0,ROUND(t1.`revenue`/t1.live_cnt,0),0) AS revenue_per_live,
-	IF(t2.live_cnt>0,ROUND(t2.`revenue`/t2.live_cnt,0),0) AS revenue_per_live_lastweek,
-	IF(t3.live_cnt>0,ROUND(t3.`revenue`/t3.live_cnt,0),0) AS revenue_per_live_lastmonth
+SELECT t1.dt,
+       t1.remark,
+       t1.revenue_level,
+       t1.newold_state,
+       t1.active_state,
+       t1.anchor_cnt,
+       t2.anchor_cnt                                                   AS anchor_cnt_lastweek,
+       t3.anchor_cnt                                                   AS anchor_cnt_lastmonth,
+       t1.live_cnt,
+       t2.live_cnt                                                     AS live_cnt_lastweek,
+       t3.live_cnt                                                     AS live_cnt_lastmonth,
+       IF(t1.anchor_cnt > 0, ROUND(t1.live_cnt / t1.anchor_cnt, 3), 0) AS live_ratio,
+       IF(t2.anchor_cnt > 0, ROUND(t2.live_cnt / t2.anchor_cnt, 3), 0) AS live_ratio_lastweek,
+       IF(t3.anchor_cnt > 0, ROUND(t3.live_cnt / t3.anchor_cnt, 3), 0) AS live_ratio_lastmonth,
+       ROUND(t1.duration / 3600, 1)                                    AS duration,
+       ROUND(t2.duration / 3600, 1)                                    AS duration_lastweek,
+       ROUND(t3.duration / 3600, 1)                                    AS duration_lastmonth,
+       t1.revenue,
+       t2.revenue                                                      AS revenue_lastweek,
+       t3.revenue                                                      AS revenue_lastmonth,
+       IF(t1.live_cnt > 0, ROUND(t1.revenue / t1.live_cnt, 0), 0)      AS revenue_per_live,
+       IF(t2.live_cnt > 0, ROUND(t2.revenue / t2.live_cnt, 0), 0)      AS revenue_per_live_lastweek,
+       IF(t3.live_cnt > 0, ROUND(t3.revenue / t3.live_cnt, 0), 0)      AS revenue_per_live_lastmonth
 FROM bireport.rpt_day_bb_guild_new t1
 LEFT JOIN bireport.rpt_day_bb_guild_new t2
 	ON t1.dt - INTERVAL 7 DAY = t2.dt
@@ -311,20 +322,56 @@ LEFT JOIN bireport.rpt_day_bb_guild_new t3
 	AND t1.revenue_level = t3.revenue_level
 	AND t1.newold_state = t3.newold_state
 	AND t1.active_state = t3.active_state
-  WHERE t1.dt BETWEEN '{start_date}' AND '{end_date}';
+  WHERE t1.dt BETWEEN '{start_date}' AND '{end_date}'
+;
 
 
 -- 报表用，计算指标占比---
-replace into `bireport`.`rpt_day_bb_guild_new_view_compare` 
-select * from
-(SELECT dt,remark,revenue_level,newold_state,active_state,'主播数' as idx,
-anchor_cnt as val FROM `bireport`.`rpt_day_bb_guild_new` where dt BETWEEN '{start_date}' AND '{end_date}' and  revenue_level!='all'
-union 
-SELECT dt,remark,revenue_level,newold_state,active_state,'开播数' as idx,
-live_cnt as val FROM `bireport`.`rpt_day_bb_guild_new` where dt BETWEEN '{start_date}' AND '{end_date}' and revenue_level!='all'
-union 
-SELECT dt,remark,revenue_level,newold_state,active_state,'流水' as idx,
-revenue as val FROM `bireport`.`rpt_day_bb_guild_new` where dt BETWEEN '{start_date}' AND '{end_date}' and revenue_level!='all'
-union 
-SELECT dt,remark,revenue_level,newold_state,active_state,'开播数' as idx,
-round(revenue/live_cnt,0) as val FROM `bireport`.`rpt_day_bb_guild_new` where dt BETWEEN '{start_date}' AND '{end_date}' and  revenue_level!='all' and live_cnt>0) t
+replace into bireport.rpt_day_bb_guild_new_view_compare
+select *
+from (SELECT dt,
+             remark,
+             revenue_level,
+             newold_state,
+             active_state,
+             '主播数'      as idx,
+             anchor_cnt as val
+      FROM bireport.rpt_day_bb_guild_new
+      where revenue_level != 'all'
+        and dt BETWEEN '{start_date}' AND '{end_date}'
+      union
+      SELECT dt,
+             remark,
+             revenue_level,
+             newold_state,
+             active_state,
+             '开播数'    as idx,
+             live_cnt as val
+      FROM bireport.rpt_day_bb_guild_new
+      where revenue_level != 'all'
+        and dt BETWEEN '{start_date}' AND '{end_date}'
+      union
+      SELECT dt,
+             remark,
+             revenue_level,
+             newold_state,
+             active_state,
+             '流水'    as idx,
+             revenue as val
+      FROM bireport.rpt_day_bb_guild_new
+      where revenue_level != 'all'
+        and dt BETWEEN '{start_date}' AND '{end_date}'
+      union
+      SELECT dt,
+             remark,
+             revenue_level,
+             newold_state,
+             active_state,
+             '开播数'                        as idx,
+             round(revenue / live_cnt, 0) as val
+      FROM bireport.rpt_day_bb_guild_new
+      where revenue_level != 'all'
+        and dt BETWEEN '{start_date}' AND '{end_date}'
+        and live_cnt > 0) t
+;
+
