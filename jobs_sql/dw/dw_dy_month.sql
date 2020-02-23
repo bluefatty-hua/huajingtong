175212acfe5529c43577d17a96b62a0ee6487519
Rockie_Yuen <rockie_yuen@outlook.com>
2020-02-23 18:47:10 +0800

    up

--- jobs_sql/dw/dw_dy_month.sql
+++ jobs_sql/dw/dw_dy_month.sql
@@ -4,20 +4,20 @@ DELETE
 FROM warehouse.dw_dy_month_guild_live
 WHERE dt BETWEEN DATE_FORMAT('{start_date}', '%Y-%m-01') AND '{end_date}';
 INSERT INTO warehouse.dw_dy_month_guild_live
-SELECT DATE_FORMAT(al.dt, '%Y-%m-01')                                      AS dt,
+SELECT DATE_FORMAT(al.dt, '%Y-%m-01')                                       AS dt,
        al.platform_id,
        al.platform_name,
        al.backend_account_id,
        al.revenue_level,
-       al.month_newold_state                                               AS newold_state,
+       al.month_newold_state                                                AS newold_state,
        al.active_state,
        COUNT(DISTINCT al.anchor_uid)                                        AS anchor_cnt,
        COUNT(DISTINCT
              CASE WHEN al.live_status = 1 THEN al.anchor_uid ELSE NULL END) AS anchor_live_cnt,
-       SUM(al.duration)                                                    AS duration,
-       SUM(IF(al.revenue >= 0, al.revenue, 0))                             AS revenue,
-       SUM(IF(al.anchor_income >= 0, al.anchor_income, 0))                 AS anchor_income,
-       SUM(IF(al.guild_income >= 0, al.guild_income, 0))                   AS guild_income
+       SUM(IF(al.duration >= 0, al.duration, 0))                            AS duration,
+       SUM(IF(al.revenue >= 0, al.revenue, 0))                              AS revenue,
+       SUM(IF(al.anchor_income >= 0, al.anchor_income, 0))                  AS anchor_income,
+       SUM(IF(al.guild_income >= 0, al.guild_income, 0))                    AS guild_income
 FROM (SELECT *,
              warehouse.ANCHOR_NEW_OLD(min_live_dt, min_sign_dt, CASE
                                                                     WHEN dt < DATE_FORMAT('{start_date}', '%Y-%m-01')
