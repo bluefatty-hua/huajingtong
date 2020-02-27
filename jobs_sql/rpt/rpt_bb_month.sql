f14b84e4248045086eaa000c6100fadb54b66938
Rockie_Yuen <rockie_yuen@outlook.com>
2020-02-27 15:18:27 +0800

    up

--- jobs_sql/rpt/rpt_bb_month.sql
+++ jobs_sql/rpt/rpt_bb_month.sql
@@ -1,12 +1,11 @@
-DROP TABLE IF EXISTS stage.bb_guild_income_rate;
-CREATE TABLE stage.bb_guild_income_rate AS
+REPLACE stage.bb_guild_income_rate
 SELECT backend_account_id,
        AVG(guild_income / revenue)  AS guild_income_rate,
        AVG(anchor_income / revenue) AS anchor_income_rate
 FROM bireport.rpt_month_bb_guild
 WHERE guild_income > 0
   AND revenue > 0
-  AND DATE_FORMAT(dt, '%Y-%m') <> DATE_FORMAT('{end_date}', '%Y-%m')
+  AND dt <> DATE_FORMAT('{cur_date}', '%Y-%m-01')
 GROUP BY backend_account_id
 ;
 
@@ -14,10 +13,34 @@ GROUP BY backend_account_id
 -- 公会每月流水、公会收入、主播收入
 -- DROP TABLE IF EXISTS bireport.rpt_month_bb_guild;
 -- CREATE TABLE bireport.rpt_month_bb_guild AS
-DELETE
-FROM bireport.rpt_month_bb_guild
-WHERE dt BETWEEN DATE_FORMAT('{start_date}', '%Y-%m-01') AND DATE_FORMAT('{end_date}', '%Y-%m-01');
-INSERT INTO bireport.rpt_month_bb_guild
+REPLACE INTO bireport.rpt_month_bb_guild
+SELECT t.dt,
+       t.platform_id,
+       pf.platform_name                              AS platform,
+       t.backend_account_id,
+       t1.remark,
+       SUM(t.anchor_cnt)                             AS anchor_cnt,
+       SUM(t.anchor_live_cnt)                        AS live_cnt,
+       SUM(t.revenue) / 1000                         AS revenue,
+       SUM(t.revenue)                                AS revenune_orig,
+       SUM(t.revenue * ig.guild_income_rate) / 1000  AS guild_income,
+       SUM(t.revenue * ig.guild_income_rate)         AS guild_income_orig,
+       SUM(t.revenue * ig.anchor_income_rate) / 1000 AS anchor_income,
+       SUM(t.revenue * ig.anchor_income_rate)        AS anchor_income_orig
+FROM warehouse.dw_bb_month_guild_live t
+         LEFT JOIN spider_bb_backend.account_info t1 ON t.backend_account_id = t1.backend_account_id
+         LEFT JOIN stage.bb_guild_income_rate ig ON t.backend_account_id = ig.backend_account_id
+         lEFT JOIN warehouse.platform pf ON pf.id = t.platform_id
+WHERE dt = '{month}'
+GROUP BY t.dt,
+         t.platform_id,
+         pf.platform_name,
+         t.backend_account_id,
+         t1.remark
+;
+
+
+REPLACE INTO bireport.rpt_month_bb_guild
 SELECT gl.dt,
        gl.platform_id,
        gl.platform_name                                                              AS platform,
@@ -39,87 +62,26 @@ FROM (SELECT dt,
              platform_id,
              platform_name,
              backend_account_id,
-             SUM(anchor_cnt)      AS anchor_cnt,
+             SUM(anchor_cnt)
+                                  AS anchor_cnt,
              SUM(anchor_live_cnt) AS anchor_live_cnt,
              SUM(revenue)         AS revenue
       FROM warehouse.dw_bb_month_guild_live
-      WHERE dt BETWEEN DATE_FORMAT('{start_date}', '%Y-%m-01') AND DATE_FORMAT('{end_date}', '%Y-%m-01')
-        AND dt <> DATE_FORMAT('{end_date}', '%Y-%m-01')
-        -- AND dt > DATE_FORMAT('2019-01-01', '%Y-%m-01')
+      WHERE dt >= '2019-02-01'
+        -- 结算数据只有2019年2月之后的
+        AND dt = '{month}'
       GROUP BY dt,
                platform_id,
                platform_name,
                backend_account_id) gl
-         LEFT JOIN warehouse.dw_bb_month_guild_live_true gr
-                   ON gl.dt = gr.dt AND gl.backend_account_id = gr.backend_account_id
+         INNER JOIN warehouse.dw_bb_month_guild_live_true gr
+                    ON gl.dt = gr.dt AND gl.backend_account_id = gr.backend_account_id
          LEFT JOIN spider_bb_backend.account_info ai ON gl.backend_account_id = ai.backend_account_id
-UNION ALL
-SELECT t.dt,
-       t.platform_id,
-       pf.platform_name                              AS platform,
-       t.backend_account_id,
-       t1.remark,
-       SUM(t.anchor_cnt)                             AS anchor_cnt,
-       SUM(t.anchor_live_cnt)                        AS live_cnt,
-       SUM(t.revenue) / 1000                         AS revenue,
-       SUM(t.revenue)                                AS revenune_orig,
-       SUM(t.revenue * ig.guild_income_rate) / 1000  AS guild_income,
-       SUM(t.revenue * ig.guild_income_rate)         AS guild_income_orig,
-       SUM(t.revenue * ig.anchor_income_rate) / 1000 AS anchor_income,
-       SUM(t.revenue * ig.anchor_income_rate)        AS anchor_income_orig
-FROM warehouse.dw_bb_month_guild_live t
-         LEFT JOIN spider_bb_backend.account_info t1 ON t.backend_account_id = t1.backend_account_id
-         LEFT JOIN stage.bb_guild_income_rate ig ON t.backend_account_id = ig.backend_account_id
-         lEFT JOIN warehouse.platform pf ON pf.id = t.platform_id
-WHERE dt = DATE_FORMAT('{end_date}', '%Y-%m-01')
---   OR dt <= DATE_FORMAT('2019-01-01', '%Y-%m-01')
-GROUP BY t.dt,
-         t.platform_id,
-         pf.platform_name,
-         t.backend_account_id,
-         t1.remark
-;
-
-
-DELETE
-FROM bireport.rpt_month_all_guild
-WHERE platform_id = 1001
-  AND DATE_FORMAT(dt, '%Y-%m') BETWEEN DATE_FORMAT('{start_date}', '%Y-%m') AND DATE_FORMAT('{end_date}', '%Y-%m');
-INSERT INTO bireport.rpt_month_all_guild
-SELECT dt,
-       platform_id,
-       platform,
-       channel_num,
-       anchor_cnt         AS anchor_cnt,
-       live_cnt           AS live_cnt,
-       revenue            AS revenue,
-       revenue_orig       AS revenue_orig,
-       guild_income       AS guild_income,
-       guild_income_orig  AS guild_income_orig,
-       anchor_income      AS anchor_income,
-       anchor_income_orig AS anchor_income_orig
-FROM (SELECT dt,
-             platform_id,
-             platform,
-             backend_account_id AS channel_num,
-             anchor_cnt,
-             live_cnt,
-             revenue,
-             revenue_orig,
-             guild_income,
-             guild_income_orig,
-             anchor_income,
-             anchor_income_orig
-      FROM bireport.rpt_month_bb_guild) t
-WHERE dt BETWEEN DATE_FORMAT('{start_date}', '%Y-%m-01') AND DATE_FORMAT('{end_date}', '%Y-%m-01')
 ;
 
 
 -- rpt_month_bb_guild_new
-DELETE
-FROM bireport.rpt_month_bb_guild_new
-WHERE dt BETWEEN DATE_FORMAT('{start_date}', '%Y-%m-01') AND DATE_FORMAT('{end_date}', '%Y-%m-01');
-INSERT INTO bireport.rpt_month_bb_guild_new
+REPLACE INTO bireport.rpt_month_bb_guild_new
 SELECT gl.dt,
        gl.platform_id,
        pf.platform_name                            AS platform,
@@ -141,14 +103,11 @@ FROM warehouse.dw_bb_month_guild_live gl
          LEFT JOIN spider_bb_backend.account_info ai ON gl.backend_account_id = ai.backend_account_id
          LEFT JOIN stage.bb_guild_income_rate ig ON gl.backend_account_id = ig.backend_account_id
          lEFT JOIN warehouse.platform pf ON pf.id = gl.platform_id
-WHERE dt BETWEEN DATE_FORMAT('{start_date}', '%Y-%m-01') AND DATE_FORMAT('{end_date}', '%Y-%m-01')
+WHERE dt = '{month}'
 ;
 
 
-REPLACE into bireport.rpt_month_bb_guild_new (dt, platform_id, platform, backend_account_id, remark, revenue_level,
-                                              newold_state, active_state, anchor_cnt, live_cnt, duration, revenue,
-                                              revenue_orig, guild_income, guild_income_orig, anchor_income,
-                                              anchor_income_orig)
+REPLACE INTO bireport.rpt_month_bb_guild_new
 SELECT t.dt,
        t.platform_id,
        t.platform,
@@ -186,7 +145,7 @@ FROM (
          FROM bireport.rpt_month_bb_guild_new
          WHERE (backend_account_id != 'all' OR revenue_level != 'all' OR newold_state != 'all' OR
                 active_state != 'all')
-           AND dt BETWEEN DATE_FORMAT('{start_date}', '%Y-%m-01') AND DATE_FORMAT('{end_date}', '%Y-%m-01')
+           AND dt = '{month}'
          GROUP BY dt, backend_account_id, revenue_level, newold_state, active_state
          WITH ROLLUP
 
@@ -211,7 +170,7 @@ FROM (
          FROM bireport.rpt_month_bb_guild_new
          WHERE (backend_account_id != 'all' OR revenue_level != 'all' OR newold_state != 'all' OR
                 active_state != 'all')
-           AND dt BETWEEN DATE_FORMAT('{start_date}', '%Y-%m-01') AND DATE_FORMAT('{end_date}', '%Y-%m-01')
+           AND dt = '{month}'
          GROUP BY dt, revenue_level, newold_state, active_state, backend_account_id
          WITH ROLLUP
 
@@ -236,7 +195,7 @@ FROM (
          FROM bireport.rpt_month_bb_guild_new
          WHERE (backend_account_id != 'all' OR revenue_level != 'all' OR newold_state != 'all' OR
                 active_state != 'all')
-           AND dt BETWEEN DATE_FORMAT('{start_date}', '%Y-%m-01') AND DATE_FORMAT('{end_date}', '%Y-%m-01')
+           AND dt = '{month}'
          GROUP BY dt, newold_state, active_state, backend_account_id, revenue_level
          WITH ROLLUP
 
@@ -261,7 +220,7 @@ FROM (
          FROM bireport.rpt_month_bb_guild_new
          WHERE (backend_account_id != 'all' OR revenue_level != 'all' OR newold_state != 'all' OR
                 active_state != 'all')
-           AND dt BETWEEN DATE_FORMAT('{start_date}', '%Y-%m-01') AND DATE_FORMAT('{end_date}', '%Y-%m-01')
+           AND dt = '{month}'
          GROUP BY dt, active_state, backend_account_id, revenue_level, newold_state
          WITH ROLLUP
          UNION
@@ -285,7 +244,7 @@ FROM (
          FROM bireport.rpt_month_bb_guild_new
          WHERE (backend_account_id != 'all' OR revenue_level != 'all' OR newold_state != 'all' OR
                 active_state != 'all')
-           AND dt BETWEEN DATE_FORMAT('{start_date}', '%Y-%m-01') AND DATE_FORMAT('{end_date}', '%Y-%m-01')
+           AND dt = '{month}'
          GROUP BY dt, newold_state, revenue_level, backend_account_id, active_state
          WITH ROLLUP) t
          LEFT JOIN spider_bb_backend.account_info ai
@@ -294,61 +253,85 @@ WHERE dt IS NOT NULL
 ;
 
 
-REPLACE INTO bireport.rpt_month_all_new
-(dt,
- platform,
- revenue_level,
- newold_state,
- active_state,
- anchor_cnt,
- live_cnt,
- duration,
- revenue,
- guild_income,
- anchor_income)
-SELECT dt,
-       platform,
-       revenue_level,
-       newold_state,
-       active_state,
-       anchor_cnt,
-       live_cnt,
-       duration,
-       revenue,
-       guild_income,
-       anchor_income
-FROM bireport.rpt_month_bb_guild_new
-WHERE backend_account_id = 0
-  AND dt BETWEEN DATE_FORMAT('{start_date}', '%Y-%m-01') AND DATE_FORMAT('{end_date}', '%Y-%m-01')
-;
-
 -- 报表用，计算上周、上月同期数据---
 REPLACE INTO bireport.rpt_month_bb_guild_new_view
-SELECT 
-	t1.dt,
-	t1.remark,
-	t1.revenue_level,
-	t1.newold_state,
-	t1.active_state,
-	t1.anchor_cnt,
-	t3.anchor_cnt AS anchor_cnt_lastmonth,
-	t1.live_cnt,
-	t3.live_cnt AS live_cnt_lastmonth,
-	IF(t1.anchor_cnt>0,ROUND(t1.live_cnt/t1.anchor_cnt,3),0) AS live_ratio,
-	IF(t3.anchor_cnt>0,ROUND(t3.live_cnt/t3.anchor_cnt,3),0) AS live_ratio_lastmonth,
-	ROUND(t1.duration/3600,1) AS duration,
-	ROUND(t3.duration/3600,1) AS duration_lastmonth,
-	t1.revenue,
-	t3.revenue AS revenue_lastmonth,
-	IF(t1.live_cnt>0,ROUND(t1.`revenue`/t1.live_cnt,0),0) AS revenue_per_live,
-	IF(t3.live_cnt>0,ROUND(t3.`revenue`/t3.live_cnt,0),0) AS revenue_per_live_lastmonth,
-	0 AS `guild_income`,
-	0 AS `anchor_income` 
+SELECT t1.dt,
+       t1.remark,
+       t1.revenue_level,
+       t1.newold_state,
+       t1.active_state,
+       t1.anchor_cnt,
+       t3.anchor_cnt                                                   AS anchor_cnt_lastmonth,
+       t1.live_cnt,
+       t3.live_cnt                                                     AS live_cnt_lastmonth,
+       IF(t1.anchor_cnt > 0, ROUND(t1.live_cnt / t1.anchor_cnt, 3), 0) AS live_ratio,
+       IF(t3.anchor_cnt > 0, ROUND(t3.live_cnt / t3.anchor_cnt, 3), 0) AS live_ratio_lastmonth,
+       ROUND(t1.duration / 3600, 1)                                    AS duration,
+       ROUND(t3.duration / 3600, 1)                                    AS duration_lastmonth,
+       t1.revenue,
+       t3.revenue                                                      AS revenue_lastmonth,
+       IF(t1.live_cnt > 0, ROUND(t1.`revenue` / t1.live_cnt, 0), 0)    AS revenue_per_live,
+       IF(t3.live_cnt > 0, ROUND(t3.`revenue` / t3.live_cnt, 0), 0)    AS revenue_per_live_lastmonth,
+       0                                                               AS `guild_income`,
+       0                                                               AS `anchor_income`
 FROM bireport.rpt_month_bb_guild_new t1
-LEFT JOIN bireport.rpt_month_bb_guild_new t3
-	ON t1.dt - INTERVAL 1 MONTH = t3.dt
-	AND t1.remark = t3.remark
-	AND t1.revenue_level = t3.revenue_level
-	AND t1.newold_state = t3.newold_state
-	AND t1.active_state = t3.active_state
-  -- WHERE t1.dt = '{month}';
+         LEFT JOIN bireport.rpt_month_bb_guild_new t3
+                   ON t1.dt - INTERVAL 1 MONTH = t3.dt
+                       AND t1.remark = t3.remark
+                       AND t1.revenue_level = t3.revenue_level
+                       AND t1.newold_state = t3.newold_state
+                       AND t1.active_state = t3.active_state
+WHERE t1.dt = '{month}'
+;
+
+
+-- 报表用，计算指标占比---
+REPLACE INTO bireport.rpt_month_bb_guild_new_view_compare
+SELECT *
+FROM (SELECT dt,
+             backend_account_id,
+             revenue_level,
+             newold_state,
+             active_state,
+             '主播数'      AS idx,
+             anchor_cnt AS val
+      FROM bireport.rpt_month_bb_guild_new
+      WHERE revenue_level != 'all'
+        AND dt = '{month}'
+      UNION
+      SELECT dt,
+             backend_account_id,
+             revenue_level,
+             newold_state,
+             active_state,
+             '开播数'    AS idx,
+             live_cnt AS val
+      FROM bireport.rpt_month_bb_guild_new
+      WHERE revenue_level != 'all'
+        AND dt = '{month}'
+      UNION
+      SELECT dt,
+             backend_account_id,
+             revenue_level,
+             newold_state,
+             active_state,
+             '流水'    AS idx,
+             revenue AS val
+      FROM bireport.rpt_month_bb_guild_new
+      WHERE revenue_level != 'all'
+        AND dt = '{month}'
+      UNION
+      SELECT dt,
+             backend_account_id,
+             revenue_level,
+             newold_state,
+             active_state,
+             '开播人均流水'                     AS idx,
+             round(revenue / live_cnt, 0) AS val
+      FROM bireport.rpt_month_bb_guild_new
+      WHERE revenue_level != 'all'
+        AND dt = '{month}'
+        AND live_cnt > 0
+     ) t
+;
+
