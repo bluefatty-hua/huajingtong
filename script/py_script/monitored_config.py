63727dd9c6ef64ab3876ba4a05093801461dc39b
Rockie_Yuen <rockie_yuen@outlook.com>
2020-03-04 00:41:01 +0800

    up

---
+++ script/py_script/monitored_config.py
@@ -0,0 +1,41 @@
+# -*- coding: utf8 -*-
+
+MONITOR_TABLE = [
+    'rpt_day_all_new',
+]
+
+
+MONITOR_DIC = {
+    'rpt_day_all_new': {
+        # 用于判断t-2的数据是否变化(数据刷新的最近7天的数据)
+        'judge_sql': '''
+                     SELECT n1.dt, 
+                     m.platform, 
+                     (n1.revenue = m.revenue AND n1.anchor_cnt = m.anchor_cnt AND n1.live_cnt = m.live_cnt) AS jud1,
+                     (n2.dt IS NOT NULL AND n2.anchor_cnt > 0 AND n2.live_cnt > 0 AND n2.revenue) AS jud2
+                     FROM bireport.rpt_day_all_new n1
+                     INNER JOIN (SELECT * FROM stage.monitored WHERE type = 'all') m ON n1.dt = m.dt AND n1.platform = m.platform
+                     LEFT JOIN (SELECT * 
+                                FROM bireport.rpt_day_all_new
+                                WHERE newold_state = 'all'
+                                  AND active_state = 'all'
+                                  AND revenue_level = 'all'
+                                  AND dt = '2020-03-03') n2 ON n1.platform = n2.platform
+                     WHERE n1.newold_state = 'all'
+                       AND n1.active_state = 'all'
+                       AND n1.revenue_level = 'all'
+                        -- t-2
+                       AND n1.dt = '2020-03-03' - INTERVAL 1 DAY;
+    ''',
+        'insert_sql': '''
+    REPLACE INTO stage.monitored
+            SELECT dt, platform, anchor_cnt, live_cnt, revenue, 'all' AS type
+            FROM bireport.rpt_day_all_new
+            WHERE newold_state = 'all'
+              AND active_state = 'all'
+              AND revenue_level = 'all'
+              -- t-1
+              AND dt = '{cur_date}';
+    '''
+    }
+}
---
+++ script/py_script/monitored_test.py
@@ -0,0 +1,78 @@
+# -*- coding: utf8 -*-
+
+import argparse
+import pymysql
+from datetime import datetime
+from datetime import date
+from datetime import timedelta
+import io
+import os
+import logging
+from common.log import init_logging
+from common.sent_email import send_email
+from common.config import LOG_DIR
+from common.config import XJL_ETL_DB
+from common.config import TO_AGENT
+from monitored_config import MONITOR_TABLE
+from monitored_config import MONITOR_DIC
+import sys
+from warnings import filterwarnings
+
+log_path = LOG_DIR + datetime.now().strftime('%Y%m%d')
+if not os.path.exists(log_path):
+    os.makedirs(log_path)
+log_file = log_path + '/etl_' + 'monitored.log'
+init_logging({'console_log_level': logging.INFO, 'file_log_level': logging.INFO, 'log_file': log_file})
+cur_date = (date.today() + timedelta(days=-1)).strftime('%Y-%m-%d')
+
+# 连接数据库
+conn = pymysql.Connect(host=XJL_ETL_DB['host'], port=XJL_ETL_DB['port'], user=XJL_ETL_DB['user'],
+                       password=XJL_ETL_DB['password'])
+cursor = conn.cursor()
+
+
+def run_sql(sql_dic, sql_param):
+    judge_sql = sql_dic['judge_sql'].format(cur_date=sql_param['cur_date'])
+    insert_sql = sql_dic['insert_sql'].format(cur_date=sql_param['cur_date'])
+    i = 0
+    sql = ''
+    try:
+        sql = judge_sql
+        cursor.execute(judge_sql)
+        result = cursor.fetchall()
+        logging.info('\n' + str(result).replace('), (', '),\n ('))
+        # ((datetime.date(2020, 3, 1), 'all', 1),
+        #  (datetime.date(2020, 3, 1), 'bilibili', 1),
+        #  (datetime.date(2020, 3, 1), 'DouYin', 1),
+        #  (datetime.date(2020, 3, 1), 'FanXing', 1),
+        #  (datetime.date(2020, 3, 1), 'HUYA', 1),
+        #  (datetime.date(2020, 3, 1), 'NOW', 1),
+        #  (datetime.date(2020, 3, 1), 'YY', 1))
+        for t in result:
+            print(t)
+            if t[2] == 1 and t[3] == 1:
+                pass
+            else:
+                i += 1
+                text = 'ERROR:' + t[1] + sql_param['cur_date'] + '数据有误&数据缺失' if t[2] != 1 and t[3] != 1 else (
+                    '数据缺失' if t[2] == 1 and t[3] != 1 else '数据有误')
+                send_email(TO_AGENT['email'], 'monitored.sql', '', text)
+        if i == 0:
+            sql = insert_sql
+            cursor.execute(insert_sql)
+            conn.commit()
+    except Exception as err:
+        logging.exception(err)
+        text = '{err}\n{sql}'.format(err=err, sql=sql)
+        send_email(TO_AGENT['email'], 'monitored.sql', '', text)
+        conn.rollback()
+        return
+
+
+if __name__ == '__main__':
+    for table in MONITOR_TABLE:
+        print(MONITOR_DIC[table])
+        param = {'cur_date': cur_date}
+        run_sql(MONITOR_DIC[table], param)
+    cursor.close()
+    conn.close()
