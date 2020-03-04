244d6a3aa248e50e8b0fcf8c38676231171929dc
Page <4061207@qq.com>
2020-03-04 18:32:36 +0800

    招募样式修改

--- jobs_sql/rpt/rpt_dy_xjl_day_recruit.sql
+++ jobs_sql/rpt/rpt_dy_xjl_day_recruit.sql
@@ -2,28 +2,28 @@ USE bireport;
 DELETE FROM rpt_day_dy_xjl_recruit;
 INSERT INTO `rpt_day_dy_xjl_recruit`
 SELECT MIN(dt) AS dt,xjl_week,recruit_team,
-CONCAT('<br><img title="累计主播数" style="height:22px" src="/images/181548.svg">:',MAX(anchor_cnt),'<br><img title="总新开播" style="height:22px" src="/images/181537.svg">:',SUM(new_live_cnt),'<br><img title="总短视频" style="height:22px" src="/images/181538.svg">:',SUM(aweme_cnt),'<br><img title="总流水" style="height:22px" src="/images/181569.svg">:',ROUND(SUM(revenue)/10,0))
+CONCAT('<br><img title="累计主播数" style="height:22px" src="/images/181548.svg">:',MAX(anchor_cnt),'<br><img title="总新开播" style="height:22px" src="/images/new_black.svg">:',SUM(new_live_cnt),'<br><img title="日均开播" style="height:22px" src="/images/181537.svg">:',ROUND(AVG(live_cnt),0),'<br><img title="总短视频" style="height:22px" src="/images/181538.svg">:',SUM(aweme_cnt),'<br><img title="总流水" style="height:22px" src="/images/181569.svg">:',ROUND(SUM(revenue)/10,0))
 AS total,
 MAX(IF(xjl_day_of_week=0,
-CONCAT(RIGHT(dt,5),'<br>','<img title="" style="height:22px" src="/images/181548.svg">:',anchor_cnt,'<br><img title="" style="height:22px" src="/images/181537.svg">:',new_live_cnt,'<br><img title="" style="height:22px" src="/images/181538.svg">:',aweme_cnt,'<br><img title="" style="height:22px" src="/images/181569.svg">:',ROUND(revenue/10,0)),
+CONCAT(RIGHT(dt,5),'<br>','<img title="累计主播数" style="height:22px" src="/images/181548.svg">:',anchor_cnt,'<br><img title="新开播" style="height:22px" src="/images/new_black.svg">:',new_live_cnt,'<br><img title="开播数" style="height:22px" src="/images/new_black.svg">:',live_cnt,'<br><img title="短视频" style="height:22px" src="/images/181538.svg">:',aweme_cnt,'<br><img title="流水" style="height:22px" src="/images/181569.svg">:',ROUND(revenue/10,0)),
 '')) AS day1,
 MAX(IF(xjl_day_of_week=1,
-CONCAT(RIGHT(dt,5),'<br>','<img title="" style="height:22px" src="/images/181548.svg">:',anchor_cnt,'<br><img title="" style="height:22px" src="/images/181537.svg">:',new_live_cnt,'<br><img title="" style="height:22px" src="/images/181538.svg">:',aweme_cnt,'<br><img title="" style="height:22px" src="/images/181569.svg">:',ROUND(revenue/10,0)),
+CONCAT(RIGHT(dt,5),'<br>','<img title="累计主播数" style="height:22px" src="/images/181548.svg">:',anchor_cnt,'<br><img title="新开播" style="height:22px" src="/images/new_black.svg">:',new_live_cnt,'<br><img title="开播数" style="height:22px" src="/images/new_black.svg">:',live_cnt,'<br><img title="短视频" style="height:22px" src="/images/181538.svg">:',aweme_cnt,'<br><img title="流水" style="height:22px" src="/images/181569.svg">:',ROUND(revenue/10,0)),
 '')) AS day2,
 MAX(IF(xjl_day_of_week=2,
-CONCAT(RIGHT(dt,5),'<br>','<img title="" style="height:22px" src="/images/181548.svg">:',anchor_cnt,'<br><img title="" style="height:22px" src="/images/181537.svg">:',new_live_cnt,'<br><img title="" style="height:22px" src="/images/181538.svg">:',aweme_cnt,'<br><img title="" style="height:22px" src="/images/181569.svg">:',ROUND(revenue/10,0)),
+CONCAT(RIGHT(dt,5),'<br>','<img title="累计主播数" style="height:22px" src="/images/181548.svg">:',anchor_cnt,'<br><img title="新开播" style="height:22px" src="/images/new_black.svg">:',new_live_cnt,'<br><img title="开播数" style="height:22px" src="/images/new_black.svg">:',live_cnt,'<br><img title="短视频" style="height:22px" src="/images/181538.svg">:',aweme_cnt,'<br><img title="流水" style="height:22px" src="/images/181569.svg">:',ROUND(revenue/10,0)),
 '')) AS day3,
 MAX(IF(xjl_day_of_week=3,
-CONCAT(RIGHT(dt,5),'<br>','<img title="" style="height:22px" src="/images/181548.svg">:',anchor_cnt,'<br><img title="" style="height:22px" src="/images/181537.svg">:',new_live_cnt,'<br><img title="" style="height:22px" src="/images/181538.svg">:',aweme_cnt,'<br><img title="" style="height:22px" src="/images/181569.svg">:',ROUND(revenue/10,0)),
+CONCAT(RIGHT(dt,5),'<br>','<img title="累计主播数" style="height:22px" src="/images/181548.svg">:',anchor_cnt,'<br><img title="新开播" style="height:22px" src="/images/new_black.svg">:',new_live_cnt,'<br><img title="开播数" style="height:22px" src="/images/new_black.svg">:',live_cnt,'<br><img title="短视频" style="height:22px" src="/images/181538.svg">:',aweme_cnt,'<br><img title="流水" style="height:22px" src="/images/181569.svg">:',ROUND(revenue/10,0)),
 '')) AS day4,
 MAX(IF(xjl_day_of_week=4,
-CONCAT(RIGHT(dt,5),'<br>','<img title="" style="height:22px" src="/images/181548.svg">:',anchor_cnt,'<br><img title="" style="height:22px" src="/images/181537.svg">:',new_live_cnt,'<br><img title="" style="height:22px" src="/images/181538.svg">:',aweme_cnt,'<br><img title="" style="height:22px" src="/images/181569.svg">:',ROUND(revenue/10,0)),
+CONCAT(RIGHT(dt,5),'<br>','<img title="累计主播数" style="height:22px" src="/images/181548.svg">:',anchor_cnt,'<br><img title="新开播" style="height:22px" src="/images/new_black.svg">:',new_live_cnt,'<br><img title="开播数" style="height:22px" src="/images/new_black.svg">:',live_cnt,'<br><img title="短视频" style="height:22px" src="/images/181538.svg">:',aweme_cnt,'<br><img title="流水" style="height:22px" src="/images/181569.svg">:',ROUND(revenue/10,0)),
 '')) AS day5,
 MAX(IF(xjl_day_of_week=5,
-CONCAT(RIGHT(dt,5),'<br>','<img title="" style="height:22px" src="/images/181548.svg">:',anchor_cnt,'<br><img title="" style="height:22px" src="/images/181537.svg">:',new_live_cnt,'<br><img title="" style="height:22px" src="/images/181538.svg">:',aweme_cnt,'<br><img title="" style="height:22px" src="/images/181569.svg">:',ROUND(revenue/10,0)),
+CONCAT(RIGHT(dt,5),'<br>','<img title="累计主播数" style="height:22px" src="/images/181548.svg">:',anchor_cnt,'<br><img title="新开播" style="height:22px" src="/images/new_black.svg">:',new_live_cnt,'<br><img title="开播数" style="height:22px" src="/images/new_black.svg">:',live_cnt,'<br><img title="短视频" style="height:22px" src="/images/181538.svg">:',aweme_cnt,'<br><img title="流水" style="height:22px" src="/images/181569.svg">:',ROUND(revenue/10,0)),
 '')) AS day6,
 MAX(IF(xjl_day_of_week=6,
-CONCAT(RIGHT(dt,5),'<br>','<img title="" style="height:22px" src="/images/181548.svg">:',anchor_cnt,'<br><img title="" style="height:22px" src="/images/181537.svg">:',new_live_cnt,'<br><img title="" style="height:22px" src="/images/181538.svg">:',aweme_cnt,'<br><img title="流水" style="height:22px" src="/images/181569.svg">:',ROUND(revenue/10,0)),
+CONCAT(RIGHT(dt,5),'<br>','<img title="累计主播数" style="height:22px" src="/images/181548.svg">:',anchor_cnt,'<br><img title="新开播" style="height:22px" src="/images/new_black.svg">:',new_live_cnt,'<br><img title="开播数" style="height:22px" src="/images/new_black.svg">:',live_cnt,'<br><img title="" style="height:22px" src="/images/181538.svg">:',aweme_cnt,'<br><img title="流水" style="height:22px" src="/images/181569.svg">:',ROUND(revenue/10,0)),
 '')) AS day7
 FROM 
 (SELECT 
@@ -47,13 +47,13 @@ DELETE FROM rpt_day_dy_xjl_recruit_compare;
 INSERT INTO rpt_day_dy_xjl_recruit_compare
 SELECT dt,
 '新开播' AS idx,
-SUM(IF(recruit_team='招募一组',new_live_cnt,0)) AS team1,
-SUM(IF(recruit_team='招募二组',new_live_cnt,0)) AS team2,
-SUM(IF(recruit_team='招募三组',new_live_cnt,0)) AS team3,
-SUM(IF(recruit_team='招募四组',new_live_cnt,0)) AS team4,
-SUM(IF(recruit_team='招募五组',new_live_cnt,0)) AS team5,
-SUM(IF(recruit_team='招募六组',new_live_cnt,0)) AS team6,
-SUM(IF(recruit_team='招募七组',new_live_cnt,0)) AS team7,
+SUM(IF(recruit_team='招募1组',new_live_cnt,0)) AS team1,
+SUM(IF(recruit_team='招募2组',new_live_cnt,0)) AS team2,
+SUM(IF(recruit_team='招募3组',new_live_cnt,0)) AS team3,
+SUM(IF(recruit_team='招募4组',new_live_cnt,0)) AS team4,
+SUM(IF(recruit_team='招募5组',new_live_cnt,0)) AS team5,
+SUM(IF(recruit_team='招募6组',new_live_cnt,0)) AS team6,
+SUM(IF(recruit_team='招募7组',new_live_cnt,0)) AS team7,
 SUM(IF(recruit_team='未知',new_live_cnt,0)) AS unknow
 FROM 
 (SELECT 
@@ -72,13 +72,13 @@ GROUP BY dt;
 INSERT INTO rpt_day_dy_xjl_recruit_compare
 SELECT dt,
 '流水' AS idx,
-SUM(IF(recruit_team='招募一组',ROUND(revenue/10,0),0)) AS team1,
-SUM(IF(recruit_team='招募二组',ROUND(revenue/10,0),0)) AS team2,
-SUM(IF(recruit_team='招募三组',ROUND(revenue/10,0),0)) AS team3,
-SUM(IF(recruit_team='招募四组',ROUND(revenue/10,0),0)) AS team4,
-SUM(IF(recruit_team='招募五组',ROUND(revenue/10,0),0)) AS team5,
-SUM(IF(recruit_team='招募六组',ROUND(revenue/10,0),0)) AS team6,
-SUM(IF(recruit_team='招募七组',ROUND(revenue/10,0),0)) AS team7,
+SUM(IF(recruit_team='招募1组',ROUND(revenue/10,0),0)) AS team1,
+SUM(IF(recruit_team='招募2组',ROUND(revenue/10,0),0)) AS team2,
+SUM(IF(recruit_team='招募3组',ROUND(revenue/10,0),0)) AS team3,
+SUM(IF(recruit_team='招募4组',ROUND(revenue/10,0),0)) AS team4,
+SUM(IF(recruit_team='招募5组',ROUND(revenue/10,0),0)) AS team5,
+SUM(IF(recruit_team='招募6组',ROUND(revenue/10,0),0)) AS team6,
+SUM(IF(recruit_team='招募7组',ROUND(revenue/10,0),0)) AS team7,
 SUM(IF(recruit_team='未知',ROUND(revenue/10,0),0)) AS unknow
 FROM 
 (SELECT 
@@ -97,13 +97,13 @@ GROUP BY dt;
 INSERT INTO rpt_day_dy_xjl_recruit_compare
 SELECT dt,
 '短视频数' AS idx,
-SUM(IF(recruit_team='招募一组',aweme_cnt,0)) AS team1,
-SUM(IF(recruit_team='招募二组',aweme_cnt,0)) AS team2,
-SUM(IF(recruit_team='招募三组',aweme_cnt,0)) AS team3,
-SUM(IF(recruit_team='招募四组',aweme_cnt,0)) AS team4,
-SUM(IF(recruit_team='招募五组',aweme_cnt,0)) AS team5,
-SUM(IF(recruit_team='招募六组',aweme_cnt,0)) AS team6,
-SUM(IF(recruit_team='招募七组',aweme_cnt,0)) AS team7,
+SUM(IF(recruit_team='招募1组',aweme_cnt,0)) AS team1,
+SUM(IF(recruit_team='招募2组',aweme_cnt,0)) AS team2,
+SUM(IF(recruit_team='招募3组',aweme_cnt,0)) AS team3,
+SUM(IF(recruit_team='招募4组',aweme_cnt,0)) AS team4,
+SUM(IF(recruit_team='招募5组',aweme_cnt,0)) AS team5,
+SUM(IF(recruit_team='招募6组',aweme_cnt,0)) AS team6,
+SUM(IF(recruit_team='招募7组',aweme_cnt,0)) AS team7,
 SUM(IF(recruit_team='未知',aweme_cnt,0)) AS unknow
 FROM 
 (SELECT 
