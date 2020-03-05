c17bffa942f42658756544448733242b57b688c3
Rockie_Yuen <rockie_yuen@outlook.com>
2020-03-05 10:33:29 +0800

    up

--- script/py_script/monitored.py
+++ script/py_script/monitored.py
@@ -13,16 +13,19 @@ from common.sent_email import send_email
 from common.config import LOG_DIR
 from common.config import XJL_ETL_DB
 from common.config import TO_AGENT
+from monitored_config import MONITOR_TABLE
+from monitored_config import MONITOR_DIC
 import sys
 from warnings import filterwarnings
-
 
 log_path = LOG_DIR + datetime.now().strftime('%Y%m%d')
 if not os.path.exists(log_path):
     os.makedirs(log_path)
 log_file = log_path + '/etl_' + 'monitored.log'
 init_logging({'console_log_level': logging.INFO, 'file_log_level': logging.INFO, 'log_file': log_file})
+
 cur_date = (date.today() + timedelta(days=-1)).strftime('%Y-%m-%d')
+judge_date = (date.today() + timedelta(days=-1)).strftime('%Y-%m-%d')
 
 # 连接数据库
 conn = pymysql.Connect(host=XJL_ETL_DB['host'], port=XJL_ETL_DB['port'], user=XJL_ETL_DB['user'],
@@ -30,49 +33,42 @@ conn = pymysql.Connect(host=XJL_ETL_DB['host'], port=XJL_ETL_DB['port'], user=XJ
 cursor = conn.cursor()
 
 
-def run_sql(sql_param):
-    judge_sql = '''SELECT n.dt, m.platform, (n.revenue = m.revenue AND n.anchor_cnt = m.anchor_cnt AND n.live_cnt = m.live_cnt)
-             FROM bireport.rpt_day_all_new n
-             INNER JOIN stage.monitored m ON n.dt = m.dt AND n.platform = m.platform
-             WHERE newold_state = 'all'
-               AND active_state = 'all'
-               AND revenue_level = 'all'
-               -- t-2
-               AND n.dt = '{cur_date}' - INTERVAL 1 DAY;'''.format(cur_date=cur_date)
-    insert_sql = '''
-            REPLACE INTO stage.monitored
-            SELECT dt, platform, anchor_cnt, live_cnt, revenue
-            FROM bireport.rpt_day_all_new
-            WHERE newold_state = 'all'
-              AND active_state = 'all'
-              AND revenue_level = 'all'
-              -- t-1
-              AND dt = '{cur_date}';'''.format(cur_date=cur_date)
+def run_sql(sql_dic, sql_param):
+    judge_sql = sql_dic['judge_sql'].format(cur_date=sql_param['cur_date'])
+    result_insert_sql = sql_dic['result_insert_sql'].format(cur_date=sql_param['cur_date'])
+    insert_sql = sql_dic['insert_sql'].format(cur_date=sql_param['cur_date'])
     i = 0
+    text = ''
     sql = ''
     try:
         sql = judge_sql
         cursor.execute(judge_sql)
         result = cursor.fetchall()
+        cursor.execute(result_insert_sql)
         logging.info('\n' + str(result).replace('), (', '),\n ('))
-        # ((datetime.date(2020, 3, 1), 'all', 1),
-        #  (datetime.date(2020, 3, 1), 'bilibili', 1),
-        #  (datetime.date(2020, 3, 1), 'DouYin', 1),
-        #  (datetime.date(2020, 3, 1), 'FanXing', 1),
-        #  (datetime.date(2020, 3, 1), 'HUYA', 1),
-        #  (datetime.date(2020, 3, 1), 'NOW', 1),
-        #  (datetime.date(2020, 3, 1), 'YY', 1))
+        # (datetime.date(2020, 3, 1), 'all', 1, 1)
+        # (datetime.date(2020, 3, 1), 'bilibili', 1, 1)
+        # (datetime.date(2020, 3, 1), 'DouYin', 1, 1)
+        # (datetime.date(2020, 3, 1), 'FanXing', 1, 1)
+        # (datetime.date(2020, 3, 1), 'HUYA', 1, 1)
+        # (datetime.date(2020, 3, 1), 'NOW', 1, 1)
+        # (datetime.date(2020, 3, 1), 'YY', 1, 1)
         for t in result:
             print(t)
-            if t[2] == 1:
+            if t[2] == 1 and t[3] == 1:
                 pass
             else:
                 i += 1
-                send_email(TO_AGENT['email'], 'monitored.sql', '', 'ERROR:' + t[1] + '数据有误')
+                text += 'ERROR: ' + t[1] + ('-{judge_date}-数据有误&{cur_date}-数据缺失' if t[2] != 1 and t[3] != 1 else (
+                    '-{cur_date}-数据缺失' if t[2] == 1 and t[3] != 1 else '-{judge_date}-数据有误')).format(
+                    judge_date=judge_date,
+                    cur_date=cur_date) + '\n'
         if i == 0:
             sql = insert_sql
             cursor.execute(insert_sql)
             conn.commit()
+        else:
+            send_email(TO_AGENT['email'], 'monitored.sql', '', text)
     except Exception as err:
         logging.exception(err)
         text = '{err}\n{sql}'.format(err=err, sql=sql)
@@ -82,5 +78,10 @@ def run_sql(sql_param):
 
 
 if __name__ == '__main__':
-    param = {'cur_date': cur_date}
-    run_sql(param)
+    logging.info('cur_date: ' + cur_date + ' judge_date: ' + judge_date)
+    for table in MONITOR_TABLE:
+        # print(MONITOR_DIC[table])
+        param = {'cur_date': cur_date}
+        run_sql(MONITOR_DIC[table], param)
+    cursor.close()
+    conn.close()
