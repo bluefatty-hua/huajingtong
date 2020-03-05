7e33153682a78c770abe8e6e7043158095e5308b
Rockie_Yuen <rockie_yuen@outlook.com>
2020-03-05 17:40:29 +0800

    up

--- script/sh_script/all_sql.sh
+++ script/sh_script/all_sql.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-/usr/bin/python /services/xjl_etl/script/py_script/run_sql.py -f rpt/rpt_yy_month.sql
+# /usr/bin/python /services/xjl_etl/script/py_script/run_sql.py -f rpt/rpt_yy_month.sql
 /usr/bin/python /services/xjl_etl/script/py_script/run_sql.py -f rpt/rpt_all_month.sql
 /usr/bin/python /services/xjl_etl/script/py_script/run_sql.py -f rpt/rpt_all_month_compare.sql
 
-/usr/bin/python /services/xjl_etl/script/py_script/run_sql.py -f rpt/rpt_yy_day.sql
+# /usr/bin/python /services/xjl_etl/script/py_script/run_sql.py -f rpt/rpt_yy_day.sql
 /usr/bin/python /services/xjl_etl/script/py_script/run_sql.py -f rpt/rpt_all_day.sql
 /usr/bin/python /services/xjl_etl/script/py_script/run_sql.py -f rpt/rpt_all_day_compare.sql
 
