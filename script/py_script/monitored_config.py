25fb0c6cf0e237fbd92a2e4a64435cac1f6ab115
Rockie_Yuen <rockie_yuen@outlook.com>
2020-03-05 10:01:30 +0800

    up

--- script/py_script/monitored_config.py
+++ script/py_script/monitored_config.py
@@ -1,11 +1,11 @@
 # -*- coding: utf8 -*-
 
 MONITOR_TABLE = [
-    'rpt_day_all_new',
+    'rpt_day_all',
 ]
 
 MONITOR_DIC = {
-    'rpt_day_all_new': {
+    'rpt_day_all': {
         # 用于判断t-2的数据是否变化(数据刷新的最近7天的数据)
         # jud1 jud2用于判断数据是否有误
         'judge_sql': '''
@@ -13,19 +13,19 @@ MONITOR_DIC = {
                             m.platform, 
                             (n1.revenue = m.revenue AND n1.anchor_cnt = m.anchor_cnt AND n1.live_cnt = m.live_cnt) AS jud1,
                             (n2.dt IS NOT NULL AND n2.anchor_cnt > 0 AND n2.live_cnt > 0 AND n2.revenue) AS jud2
-                     FROM bireport.rpt_day_all_new n1
-                     INNER JOIN (SELECT * FROM stage.monitored WHERE table_name = 'rpt_day_all_new') m ON n1.dt = m.dt AND n1.platform = m.platform
+                     FROM bireport.rpt_day_all n1
+                     INNER JOIN (SELECT * FROM stage.monitored WHERE table_name = 'rpt_day_all') m ON n1.dt = m.dt AND n1.platform = m.platform
                      LEFT JOIN (SELECT * 
-                                FROM bireport.rpt_day_all_new
+                                FROM bireport.rpt_day_all
                                 WHERE newold_state = 'all'
                                   AND active_state = 'all'
                                   AND revenue_level = 'all'
-                                  AND dt = '2020-03-03') n2 ON n1.platform = n2.platform
+                                  AND dt = ''{cur_date}'') n2 ON n1.platform = n2.platform
                      WHERE n1.newold_state = 'all'
                        AND n1.active_state = 'all'
                        AND n1.revenue_level = 'all'
                         -- t-2
-                       AND n1.dt = '2020-03-03' - INTERVAL 1 DAY;
+                       AND n1.dt = ''{cur_date}'' - INTERVAL 1 DAY;
     ''',
         'result_insert_sql': '''
                      REPLACE INTO stage.monitored_result
@@ -48,10 +48,10 @@ MONITOR_DIC = {
                              n2.live_cnt,
                              n2.revenue,
                              m.table_name
-                     FROM bireport.rpt_day_all_new n1
-                     INNER JOIN (SELECT * FROM stage.monitored WHERE table_name = 'rpt_day_all_new') m ON n1.dt = m.dt AND n1.platform = m.platform
+                     FROM bireport.rpt_day_all n1
+                     INNER JOIN (SELECT * FROM stage.monitored WHERE table_name = 'rpt_day_all') m ON n1.dt = m.dt AND n1.platform = m.platform
                      LEFT JOIN (SELECT *
-                                FROM bireport.rpt_day_all_new
+                                FROM bireport.rpt_day_all
                                 WHERE newold_state = 'all'
                                   AND active_state = 'all'
                                   AND revenue_level = 'all'
@@ -64,8 +64,8 @@ MONITOR_DIC = {
     ''',
         'insert_sql': '''
             REPLACE INTO stage.monitored
-            SELECT dt, platform, anchor_cnt, live_cnt, revenue, 'rpt_day_all_new' AS table_name
-            FROM bireport.rpt_day_all_new
+            SELECT dt, platform, anchor_cnt, live_cnt, revenue, 'rpt_day_all' AS table_name
+            FROM bireport.rpt_day_all
             WHERE newold_state = 'all'
               AND active_state = 'all'
               AND revenue_level = 'all'
