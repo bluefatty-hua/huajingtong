1df770b52599e614b4e226049b42f1c869fd1383
Rockie_Yuen <rockie_yuen@outlook.com>
2020-03-09 14:01:11 +0800

    up

--- jobs_sql/dw/dw_hy_day.sql
+++ jobs_sql/dw/dw_hy_day.sql
@@ -51,7 +51,8 @@ WHERE dt BETWEEN '{start_date}' AND '{end_date}'
 INSERT IGNORE INTO stage.stage_huya_day_anchor_info (anchor_uid, anchor_no, channel_id, comment, dt)
 SELECT al.anchor_uid, ai.anchor_no, al.channel_id, 'from anchor_live_detail_day' AS comment, al.dt
 FROM warehouse.ods_huya_day_anchor_live al
-LEFT JOIN (SELECT DISTINCT anchor_uid, anchor_no FROM stage.stage_huya_day_anchor_info) ai ON al.anchor_uid = ai.anchor_uid
+         LEFT JOIN (SELECT DISTINCT anchor_uid, anchor_no FROM stage.stage_huya_day_anchor_info) ai
+                   ON al.anchor_uid = ai.anchor_uid
 WHERE al.dt BETWEEN '{start_date}' AND '{end_date}'
 ;
 
@@ -186,7 +187,7 @@ FROM warehouse.dw_huya_day_guild_info gi
                    ON gi.dt = cgu.dt AND gi.channel_id = cgu.channel_id
          LEFT JOIN warehouse.ods_huya_day_guild_live_income_noble cn ON gi.dt = cn.dt AND gi.channel_id = cn.channel_id
          LEFT JOIN warehouse.ods_hy_account_info ai ON gi.channel_id = ai.channel_id
-    WHERE gi.dt BETWEEN '{start_date}' AND '{end_date}'
+WHERE gi.dt BETWEEN '{start_date}' AND '{end_date}'
 ;
 
 
@@ -199,24 +200,24 @@ SELECT al.dt,
        al.platform_name,
        al.channel_type,
        al.channel_id,
-       ai.channel_no AS channel_num,
+       ai.channel_no                                                                AS channel_num,
        al.newold_state,
        al.active_state,
        al.revenue_level,
-       COUNT(DISTINCT al.anchor_no) AS anchor_cnt,
+       COUNT(DISTINCT al.anchor_no)                                                 AS anchor_cnt,
        COUNT(DISTINCT CASE WHEN al.live_status = 1 THEN al.anchor_no ELSE NULL END) AS live_cnt,
-       SUM(IFNULL(al.revenue, 0))       AS revenue
+       SUM(IFNULL(al.revenue, 0))                                                   AS revenue
 FROM warehouse.dw_huya_day_anchor_live al
-LEFT JOIN warehouse.ods_hy_account_info ai ON al.channel_id = ai.channel_id
-    WHERE al.dt BETWEEN '{start_date}' AND '{end_date}'
+         LEFT JOIN warehouse.ods_hy_account_info ai ON al.channel_id = ai.channel_id
+WHERE al.dt BETWEEN '{start_date}' AND '{end_date}'
 GROUP BY al.dt,
-       al.platform_id,
-       al.platform_name,
-       al.channel_type,
-       al.channel_id,
-       al.channel_num,
-       al.newold_state,
-       al.active_state,
-       al.revenue_level
+         al.platform_id,
+         al.platform_name,
+         al.channel_type,
+         al.channel_id,
+         al.channel_num,
+         al.newold_state,
+         al.active_state,
+         al.revenue_level
 ;
 
