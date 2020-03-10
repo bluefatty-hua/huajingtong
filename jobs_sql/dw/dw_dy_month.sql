c753f34982df032694bf20c380adc77fbbb84fdd
Rockie_Yuen <rockie_yuen@outlook.com>
2020-03-10 11:46:31 +0800

    BUG 月主播流水分档存在多个，取最终分档（t-1天）

--- jobs_sql/dw/dw_dy_month.sql
+++ jobs_sql/dw/dw_dy_month.sql
@@ -1,6 +1,9 @@
 -- DROP TABLE IF EXISTS warehouse.dw_dy_month_guild_live;
 -- CREATE TABLE warehouse.dw_dy_month_guild_live AS
-REPLACE INTO warehouse.dw_dy_month_guild_live
+DELETE
+FROM warehouse.dw_dy_month_guild_live
+WHERE dt = '2020-03-01';
+INSERT INTO warehouse.dw_dy_month_guild_live
 SELECT DATE_FORMAT(al.dt, '%Y-%m-01')                                       AS dt,
        al.platform_id,
        al.platform_name,
@@ -18,13 +21,13 @@ SELECT DATE_FORMAT(al.dt, '%Y-%m-01')                                       AS d
 FROM (SELECT *,
              -- cur_date: t-1
              warehouse.ANCHOR_NEW_OLD(min_live_dt, min_sign_dt, CASE
-                                                                    WHEN dt < DATE_FORMAT('{cur_date}', '%Y-%m-01')
+                                                                    WHEN dt < DATE_FORMAT('2020-03-09', '%Y-%m-01')
                                                                         THEN LAST_DAY(dt)
-                                                                    ELSE '{cur_date}' END, 180
+                                                                    ELSE '2020-03-09' END, 180
                  ) AS month_newold_state
       FROM warehouse.dw_dy_day_anchor_live
-      WHERE dt >= '{month}'
-        AND dt < '{month}' + INTERVAL 1 MONTH
+      WHERE dt >= '2020-03-01'
+        AND dt < '2020-03-01' + INTERVAL 1 MONTH
      ) al
 GROUP BY DATE_FORMAT(al.dt, '%Y-%m-01'),
          al.platform_id,
@@ -38,13 +41,21 @@ GROUP BY DATE_FORMAT(al.dt, '%Y-%m-01'),
 
 -- DROP TABLE IF EXISTS warehouse.dw_dy_month_anchor_live;
 -- CREATE TABLE warehouse.dw_dy_month_anchor_live AS
-REPLACE INTO warehouse.dw_dy_month_anchor_live
+DELETE
+FROM warehouse.dw_dy_month_anchor_live
+WHERE dt = '{month}';
+INSERT INTO warehouse.dw_dy_month_anchor_live
 SELECT DATE_FORMAT(al.dt, '%Y-%m-01')                      AS dt,
        al.platform_id,
        al.platform_name,
        al.backend_account_id,
        al.anchor_uid,
-       al.revenue_level,
+       CASE
+           WHEN mal.revenue / 10000 >= 50 THEN '50+'
+           WHEN mal.revenue / 10000 >= 10 THEN '10-50'
+           WHEN mal.revenue / 10000 >= 3 THEN '3-10'
+           WHEN mal.revenue / 10000 > 0 THEN '0-3'
+           ELSE '0' END                                    AS revenue_level,
        al.month_newold_state                               AS newold_state,
        al.active_state,
        SUM(IF(al.duration >= 0, al.duration, 0))           AS duration,
@@ -53,20 +64,27 @@ SELECT DATE_FORMAT(al.dt, '%Y-%m-01')                      AS dt,
        SUM(IF(al.guild_income >= 0, al.guild_income, 0))   AS guild_income
 FROM (SELECT *,
              warehouse.ANCHOR_NEW_OLD(min_live_dt, min_sign_dt, CASE
-                                                                    WHEN dt < DATE_FORMAT('{cur_date}', '%Y-%m-01')
+                                                                    WHEN dt < DATE_FORMAT('2020-03-09', '%Y-%m-01')
                                                                         THEN LAST_DAY(dt)
-                                                                    ELSE '{cur_date}' END, 180
+                                                                    ELSE '2020-03-09' END, 180
                  ) AS month_newold_state
       FROM warehouse.dw_dy_day_anchor_live
-      WHERE dt >= '{month}'
-        AND dt < '{month}' + INTERVAL 1 MONTH
+      WHERE dt >= '2020-03-01'
+        AND dt < '2020-03-01' + INTERVAL 1 MONTH
      ) al
+         LEFT JOIN stage.stage_dy_month_anchor_live mal
+                   ON mal.dt = al.dt AND al.anchor_uid = mal.anchor_uid
 GROUP BY DATE_FORMAT(al.dt, '%Y-%m-01'),
          al.platform_id,
          al.platform_name,
          al.backend_account_id,
          al.anchor_uid,
-         al.revenue_level,
+         CASE
+             WHEN mal.revenue / 10000 >= 50 THEN '50+'
+             WHEN mal.revenue / 10000 >= 10 THEN '10-50'
+             WHEN mal.revenue / 10000 >= 3 THEN '3-10'
+             WHEN mal.revenue / 10000 > 0 THEN '0-3'
+             ELSE '0' END,
          al.month_newold_state,
          al.active_state
 ;
