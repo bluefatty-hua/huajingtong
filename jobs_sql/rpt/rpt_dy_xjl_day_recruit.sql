b8e33635452e5e1539b4e05138a9956aad2d74cf
Page <4061207@qq.com>
2020-03-11 10:55:51 +0800

    中文bug

--- jobs_sql/rpt/rpt_dy_xjl_day_recruit.sql
+++ jobs_sql/rpt/rpt_dy_xjl_day_recruit.sql
@@ -1,154 +1,8 @@
 SET NAMES utf8;
 USE bireport;
-DELETE
-FROM rpt_day_dy_xjl_recruit;
-INSERT INTO rpt_day_dy_xjl_recruit
-SELECT MIN(dt)     AS dt,
-       xjl_week,
-       recruit_team,
-       CONCAT('<br><img title="&#32047;&#35745;&#20027;&#25773;&#25968;" style="height:22px" src="/images/181548.svg" />:', MAX(anchor_cnt),
-              '<br><img title="&#24635;&#26032;&#24320;&#25773;" style="height:22px" src="/images/new_black.svg" />:', SUM(new_live_cnt),
-              '<br><img title="&#26085;&#22343;&#24320;&#25773;" style="height:22px" src="/images/181537.svg"  />:', ROUND(AVG(live_cnt), 0),
-              '<br><img title="&#24635;&#30701;&#35270;&#39057;" style="height:22px" src="/images/181538.svg"  />:', SUM(aweme_cnt),
-              '<br><img title="&#24635;&#27969;&#27700;" style="height:22px" src="/images/181569.svg"  />:', ROUND(SUM(revenue) / 10, 0))
-                   AS total,
-       MAX(IF(xjl_day_of_week = 0,
-              CONCAT(RIGHT(dt, 5), '<br>', '<img title="&#32047;&#35745;&#20027;&#25773;&#25968;" style="height:22px" src="/images/181548.svg"  />:',
-                     anchor_cnt, '<br><img title="&#26032;&#24320;&#25773;" style="height:22px" src="/images/new_black.svg" />:', new_live_cnt,
-                     '<br><img title="&#24320;&#25773;&#25968;" style="height:22px" src="/images/181537.svg"  />:', live_cnt,
-                     '<br><img title="&#30701;&#35270;&#39057;" style="height:22px" src="/images/181538.svg"  />:', aweme_cnt,
-                     '<br><img title="&#27969;&#27700;" style="height:22px" src="/images/181569.svg"  />:', ROUND(revenue / 10, 0)),
-              '')) AS day1,
-       MAX(IF(xjl_day_of_week = 1,
-              CONCAT(RIGHT(dt, 5), '<br>', '<img title="&#32047;&#35745;&#20027;&#25773;&#25968;" style="height:22px" src="/images/181548.svg"  />:',
-                     anchor_cnt, '<br><img title="&#26032;&#24320;&#25773;" style="height:22px" src="/images/new_black.svg" />:', new_live_cnt,
-                     '<br><img title="&#24320;&#25773;&#25968;" style="height:22px" src="/images/181537.svg"  />:', live_cnt,
-                     '<br><img title="&#30701;&#35270;&#39057;" style="height:22px" src="/images/181538.svg"  />:', aweme_cnt,
-                     '<br><img title="&#27969;&#27700;" style="height:22px" src="/images/181569.svg"  />:', ROUND(revenue / 10, 0)),
-              '')) AS day2,
-       MAX(IF(xjl_day_of_week = 2,
-              CONCAT(RIGHT(dt, 5), '<br>', '<img title="&#32047;&#35745;&#20027;&#25773;&#25968;" style="height:22px" src="/images/181548.svg"  />:',
-                     anchor_cnt, '<br><img title="&#26032;&#24320;&#25773;" style="height:22px" src="/images/new_black.svg" />:', new_live_cnt,
-                     '<br><img title="&#24320;&#25773;&#25968;" style="height:22px" src="/images/181537.svg"  />:', live_cnt,
-                     '<br><img title="&#30701;&#35270;&#39057;" style="height:22px" src="/images/181538.svg"  />:', aweme_cnt,
-                     '<br><img title="&#27969;&#27700;" style="height:22px" src="/images/181569.svg"  />:', ROUND(revenue / 10, 0)),
-              '')) AS day3,
-       MAX(IF(xjl_day_of_week = 3,
-              CONCAT(RIGHT(dt, 5), '<br>', '<img title="&#32047;&#35745;&#20027;&#25773;&#25968;" style="height:22px" src="/images/181548.svg"  />:',
-                     anchor_cnt, '<br><img title="&#26032;&#24320;&#25773;" style="height:22px" src="/images/new_black.svg" />:', new_live_cnt,
-                     '<br><img title="&#24320;&#25773;&#25968;" style="height:22px" src="/images/181537.svg"  />:', live_cnt,
-                     '<br><img title="&#30701;&#35270;&#39057;" style="height:22px" src="/images/181538.svg"  />:', aweme_cnt,
-                     '<br><img title="&#27969;&#27700;" style="height:22px" src="/images/181569.svg"  />:', ROUND(revenue / 10, 0)),
-              '')) AS day4,
-       MAX(IF(xjl_day_of_week = 4,
-              CONCAT(RIGHT(dt, 5), '<br>', '<img title="&#32047;&#35745;&#20027;&#25773;&#25968;" style="height:22px" src="/images/181548.svg"  />:',
-                     anchor_cnt, '<br><img title="&#26032;&#24320;&#25773;" style="height:22px" src="/images/new_black.svg" />:', new_live_cnt,
-                     '<br><img title="&#24320;&#25773;&#25968;" style="height:22px" src="/images/181537.svg"  />:', live_cnt,
-                     '<br><img title="&#30701;&#35270;&#39057;" style="height:22px" src="/images/181538.svg"  />:', aweme_cnt,
-                     '<br><img title="&#27969;&#27700;" style="height:22px" src="/images/181569.svg"  />:', ROUND(revenue / 10, 0)),
-              '')) AS day5,
-       MAX(IF(xjl_day_of_week = 5,
-              CONCAT(RIGHT(dt, 5), '<br>', '<img title="&#32047;&#35745;&#20027;&#25773;&#25968;" style="height:22px" src="/images/181548.svg"  />:',
-                     anchor_cnt, '<br><img title="&#26032;&#24320;&#25773;" style="height:22px" src="/images/new_black.svg" />:', new_live_cnt,
-                     '<br><img title="&#24320;&#25773;&#25968;" style="height:22px" src="/images/181537.svg"  />:', live_cnt,
-                     '<br><img title="&#30701;&#35270;&#39057;" style="height:22px" src="/images/181538.svg"  />:', aweme_cnt,
-                     '<br><img title="&#27969;&#27700;" style="height:22px" src="/images/181569.svg"  />:', ROUND(revenue / 10, 0)),
-              '')) AS day6,
-       MAX(IF(xjl_day_of_week = 6,
-              CONCAT(RIGHT(dt, 5), '<br>', '<img title="&#32047;&#35745;&#20027;&#25773;&#25968;" style="height:22px" src="/images/181548.svg"  />:',
-                     anchor_cnt, '<br><img title="&#26032;&#24320;&#25773;" style="height:22px" src="/images/new_black.svg" />:', new_live_cnt,
-                     '<br><img title="&#24320;&#25773;&#25968;" style="height:22px" src="/images/181537.svg"  />:', live_cnt,
-                     '<br><img title="" style="height:22px" src="/images/181538.svg"  />:', aweme_cnt,
-                     '<br><img title="&#27969;&#27700;" style="height:22px" src="/images/181569.svg"  />:', ROUND(revenue / 10, 0)),
-              '')) AS day7
-FROM (SELECT dt,
-             WEEK(dt - INTERVAL 5 DAY, 0)        AS xjl_week,
-             WEEKDAY(dt - INTERVAL 4 DAY)        AS xjl_day_of_week,
-             IFNULL(recruit_team, '未知')          AS recruit_team,
-             COUNT(anchor_uid)                   AS anchor_cnt,
-             SUM(IF(duration > 3600, 1, 0))      AS live_cnt,
-             SUM(IF(first_live_date = dt, 1, 0)) AS new_live_cnt,
-             SUM(IFNULL(aweme_cnt, 0))           AS aweme_cnt,
-             SUM(IFNULL(revenue, 0))             AS revenue
-      FROM warehouse.dw_dy_xjl_day_anchor_live_recruit
-      GROUP BY recruit_team, dt) t
-GROUP BY xjl_week, recruit_team;
-
-
-DELETE
-FROM rpt_day_dy_xjl_recruit_compare;
-
-INSERT INTO rpt_day_dy_xjl_recruit_compare
-SELECT dt,
-       '&#26032;&#24320;&#25773;'                                           AS idx,
-       SUM(IF(recruit_team = '招募1组', new_live_cnt, 0)) AS team1,
-       SUM(IF(recruit_team = '招募2组', new_live_cnt, 0)) AS team2,
-       SUM(IF(recruit_team = '招募3组', new_live_cnt, 0)) AS team3,
-       SUM(IF(recruit_team = '招募4组', new_live_cnt, 0)) AS team4,
-       SUM(IF(recruit_team = '招募5组', new_live_cnt, 0)) AS team5,
-       SUM(IF(recruit_team = '招募6组', new_live_cnt, 0)) AS team6,
-       SUM(IF(recruit_team = '招募7组', new_live_cnt, 0)) AS team7,
-       SUM(IF(recruit_team = '未知', new_live_cnt, 0))   AS unknow
-FROM (SELECT dt,
-             IFNULL(recruit_team, '未知')          AS recruit_team,
-             COUNT(anchor_uid)                   AS anchor_cnt,
-             SUM(IF(duration > 3600, 1, 0))      AS live_cnt,
-             SUM(IF(first_live_date = dt, 1, 0)) AS new_live_cnt,
-             SUM(IFNULL(aweme_cnt, 0))           AS aweme_cnt,
-             SUM(IFNULL(revenue, 0))             AS revenue
-      FROM warehouse.dw_dy_xjl_day_anchor_live_recruit
-      GROUP BY recruit_team, dt) t
-GROUP BY dt;
-
-INSERT INTO rpt_day_dy_xjl_recruit_compare
-SELECT dt,
-       '&#27969;&#27700;'                                                      AS idx,
-       SUM(IF(recruit_team = '招募1组', ROUND(revenue / 10, 0), 0)) AS team1,
-       SUM(IF(recruit_team = '招募2组', ROUND(revenue / 10, 0), 0)) AS team2,
-       SUM(IF(recruit_team = '招募3组', ROUND(revenue / 10, 0), 0)) AS team3,
-       SUM(IF(recruit_team = '招募4组', ROUND(revenue / 10, 0), 0)) AS team4,
-       SUM(IF(recruit_team = '招募5组', ROUND(revenue / 10, 0), 0)) AS team5,
-       SUM(IF(recruit_team = '招募6组', ROUND(revenue / 10, 0), 0)) AS team6,
-       SUM(IF(recruit_team = '招募7组', ROUND(revenue / 10, 0), 0)) AS team7,
-       SUM(IF(recruit_team = '未知', ROUND(revenue / 10, 0), 0))   AS unknow
-FROM (SELECT dt,
-             IFNULL(recruit_team, '未知')          AS recruit_team,
-             COUNT(anchor_uid)                   AS anchor_cnt,
-             SUM(IF(duration > 3600, 1, 0))      AS live_cnt,
-             SUM(IF(first_live_date = dt, 1, 0)) AS new_live_cnt,
-             SUM(IFNULL(aweme_cnt, 0))           AS aweme_cnt,
-             SUM(IFNULL(revenue, 0))             AS revenue
-      FROM warehouse.dw_dy_xjl_day_anchor_live_recruit
-      GROUP BY recruit_team, dt) t
-GROUP BY dt;
-
-INSERT INTO rpt_day_dy_xjl_recruit_compare
-SELECT dt,
-       '&#30701;&#35270;&#39057;数'                                       AS idx,
-       SUM(IF(recruit_team = '招募1组', aweme_cnt, 0)) AS team1,
-       SUM(IF(recruit_team = '招募2组', aweme_cnt, 0)) AS team2,
-       SUM(IF(recruit_team = '招募3组', aweme_cnt, 0)) AS team3,
-       SUM(IF(recruit_team = '招募4组', aweme_cnt, 0)) AS team4,
-       SUM(IF(recruit_team = '招募5组', aweme_cnt, 0)) AS team5,
-       SUM(IF(recruit_team = '招募6组', aweme_cnt, 0)) AS team6,
-       SUM(IF(recruit_team = '招募7组', aweme_cnt, 0)) AS team7,
-       SUM(IF(recruit_team = '未知', aweme_cnt, 0))   AS unknow
-FROM (SELECT dt,
-             IFNULL(recruit_team, '未知')          AS recruit_team,
-             COUNT(anchor_uid)                   AS anchor_cnt,
-             SUM(IF(duration > 3600, 1, 0))      AS live_cnt,
-             SUM(IF(first_live_date = dt, 1, 0)) AS new_live_cnt,
-             SUM(IFNULL(aweme_cnt, 0))           AS aweme_cnt,
-             SUM(IFNULL(revenue, 0))             AS revenue
-      FROM warehouse.dw_dy_xjl_day_anchor_live_recruit
-      GROUP BY recruit_team, dt) t
-GROUP BY dt;
 
 
 
-DELETE
-FROM bireport.rpt_day_dy_xjl_recruit_detail;
-INSERT INTO bireport.rpt_day_dy_xjl_recruit_detail
 SELECT dt,
        record_date,
        first_live_date,
@@ -205,7 +59,7 @@ SELECT MIN(dt)     AS dt,
        recruit_team,
        CONCAT('<br><img title="&#32047;&#35745;&#20027;&#25773;&#25968;" style="height:22px" src="/images/181548.svg" />:', MAX(anchor_cnt),
               '<br><img title="&#24635;&#26032;&#24320;&#25773;" style="height:22px" src="/images/new_black.svg" />:', SUM(new_live_cnt),
-              '<br><img title="日均开播" style="height:22px" src="/images/181537.svg"  />:', ROUND(AVG(live_cnt), 0),
+              '<br><img title="&#x65e5;&#x5747;&#x5f00;&#x64ad;" style="height:22px" src="/images/181537.svg"  />:', ROUND(AVG(live_cnt), 0),
               '<br><img title="总&#30701;&#35270;&#39057;" style="height:22px" src="/images/181538.svg"  />:', SUM(aweme_cnt),
               '<br><img title="总&#27969;&#27700;" style="height:22px" src="/images/181569.svg"  />:', ROUND(SUM(revenue) / 10, 0))
                    AS total,
@@ -259,8 +113,8 @@ SELECT MIN(dt)     AS dt,
                      '<br><img title="&#27969;&#27700;" style="height:22px" src="/images/181569.svg"  />:', ROUND(revenue / 10, 0)),
               '')) AS day7
 FROM (SELECT dt,
-             WEEK(dt - INTERVAL 5 DAY, 0)        AS xjl_week,
-             WEEKDAY(dt - INTERVAL 4 DAY)        AS xjl_day_of_week,
+             WEEK(dt , 0)        AS xjl_week,
+             WEEKDAY(dt + INTERVAL 1 DAY)        AS xjl_day_of_week,
              IFNULL(recruit_team, '未知')          AS recruit_team,
              COUNT(anchor_uid)                   AS anchor_cnt,
              SUM(IF(duration > 3600, 1, 0))      AS live_cnt,
@@ -278,12 +132,12 @@ FROM rpt_day_dy_xjl_recruit_compare;
 INSERT INTO rpt_day_dy_xjl_recruit_compare
 SELECT dt,
        '新开播'                                           AS idx,
-       SUM(IF(recruit_team = '招募1组', new_live_cnt, 0)) AS team1,
-       SUM(IF(recruit_team = '招募2组', new_live_cnt, 0)) AS team2,
-       SUM(IF(recruit_team = '招募3组', new_live_cnt, 0)) AS team3,
-       SUM(IF(recruit_team = '招募4组', new_live_cnt, 0)) AS team4,
-       SUM(IF(recruit_team = '招募5组', new_live_cnt, 0)) AS team5,
-       SUM(IF(recruit_team = '招募6组', new_live_cnt, 0)) AS team6,
+       SUM(IF(recruit_team = '小白鲸', new_live_cnt, 0)) AS team1,
+       SUM(IF(recruit_team = '小海豚', new_live_cnt, 0)) AS team2,
+       SUM(IF(recruit_team = '大白鲨', new_live_cnt, 0)) AS team3,
+       SUM(IF(recruit_team = '小海星', new_live_cnt, 0)) AS team4,
+       SUM(IF(recruit_team = '金枪鱼', new_live_cnt, 0)) AS team5,
+       SUM(IF(recruit_team = '胖头鲨', new_live_cnt, 0)) AS team6,
        SUM(IF(recruit_team = '招募7组', new_live_cnt, 0)) AS team7,
        SUM(IF(recruit_team = '未知', new_live_cnt, 0))   AS unknow
 FROM (SELECT dt,
@@ -300,12 +154,12 @@ GROUP BY dt;
 INSERT INTO rpt_day_dy_xjl_recruit_compare
 SELECT dt,
        '流水'                                                      AS idx,
-       SUM(IF(recruit_team = '招募1组', ROUND(revenue / 10, 0), 0)) AS team1,
-       SUM(IF(recruit_team = '招募2组', ROUND(revenue / 10, 0), 0)) AS team2,
-       SUM(IF(recruit_team = '招募3组', ROUND(revenue / 10, 0), 0)) AS team3,
-       SUM(IF(recruit_team = '招募4组', ROUND(revenue / 10, 0), 0)) AS team4,
-       SUM(IF(recruit_team = '招募5组', ROUND(revenue / 10, 0), 0)) AS team5,
-       SUM(IF(recruit_team = '招募6组', ROUND(revenue / 10, 0), 0)) AS team6,
+       SUM(IF(recruit_team = '小白鲸', ROUND(revenue / 10, 0), 0)) AS team1,
+       SUM(IF(recruit_team = '小海豚', ROUND(revenue / 10, 0), 0)) AS team2,
+       SUM(IF(recruit_team = '大白鲨', ROUND(revenue / 10, 0), 0)) AS team3,
+       SUM(IF(recruit_team = '小海星', ROUND(revenue / 10, 0), 0)) AS team4,
+       SUM(IF(recruit_team = '金枪鱼', ROUND(revenue / 10, 0), 0)) AS team5,
+       SUM(IF(recruit_team = '胖头鲨', ROUND(revenue / 10, 0), 0)) AS team6,
        SUM(IF(recruit_team = '招募7组', ROUND(revenue / 10, 0), 0)) AS team7,
        SUM(IF(recruit_team = '未知', ROUND(revenue / 10, 0), 0))   AS unknow
 FROM (SELECT dt,
@@ -322,12 +176,12 @@ GROUP BY dt;
 INSERT INTO rpt_day_dy_xjl_recruit_compare
 SELECT dt,
        '短视频数'                                       AS idx,
-       SUM(IF(recruit_team = '招募1组', aweme_cnt, 0)) AS team1,
-       SUM(IF(recruit_team = '招募2组', aweme_cnt, 0)) AS team2,
-       SUM(IF(recruit_team = '招募3组', aweme_cnt, 0)) AS team3,
-       SUM(IF(recruit_team = '招募4组', aweme_cnt, 0)) AS team4,
-       SUM(IF(recruit_team = '招募5组', aweme_cnt, 0)) AS team5,
-       SUM(IF(recruit_team = '招募6组', aweme_cnt, 0)) AS team6,
+       SUM(IF(recruit_team = '小白鲸', aweme_cnt, 0)) AS team1,
+       SUM(IF(recruit_team = '小海豚', aweme_cnt, 0)) AS team2,
+       SUM(IF(recruit_team = '大白鲨', aweme_cnt, 0)) AS team3,
+       SUM(IF(recruit_team = '小海星', aweme_cnt, 0)) AS team4,
+       SUM(IF(recruit_team = '金枪鱼', aweme_cnt, 0)) AS team5,
+       SUM(IF(recruit_team = '胖头鲨', aweme_cnt, 0)) AS team6,
        SUM(IF(recruit_team = '招募7组', aweme_cnt, 0)) AS team7,
        SUM(IF(recruit_team = '未知', aweme_cnt, 0))   AS unknow
 FROM (SELECT dt,
