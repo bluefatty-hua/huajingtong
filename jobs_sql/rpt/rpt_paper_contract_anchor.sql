dfb48d346f90e9235b3f74e6f8fb673ab3f661e7
Rockie_Yuen <rockie_yuen@outlook.com>
2020-03-11 20:54:30 +0800

    up

--- jobs_sql/rpt/rpt_paper_contract_anchor.sql
+++ jobs_sql/rpt/rpt_paper_contract_anchor.sql
@@ -29,31 +29,31 @@ SELECT '{month}'                                                                
        al.real_name,
        al.contract_start_date,
        al.contract_end_date,
-       IFNULL(al1.dt, '{month}' - INTERVAL 3 MONTH)                              AS dt_t3,
-       IFNULL(al1.live_days, 0)                                                  AS live_days_t3,
-       IFNULL(al1.revenue_rmb, 0)                                                AS revenue_t3,
-       DATEDIFF(LAST_DAY(al1.dt), al1.dt) + 1 - al1.live_days                    AS unlive_days_t3,
+       IFNULL(al3.dt, '{month}' - INTERVAL 3 MONTH)                              AS dt_t3,
+       IFNULL(al3.live_days, 0)                                                  AS live_days_t3,
+       ROUND(IFNULL(al3.revenue_rmb, 0), 0)                                      AS revenue_t3,
+       DATEDIFF(LAST_DAY(al3.dt), al3.dt) + 1 - al3.live_days                    AS unlive_days_t3,
 
        IFNULL(al2.dt, '{month}' - INTERVAL 2 MONTH)                              AS dt_t2,
        IFNULL(al2.live_days, 0)                                                  AS live_days_t2,
        ROUND(IFNULL(al2.revenue_rmb, 0), 0)                                      AS revenue_rmb_t2,
        DATEDIFF(LAST_DAY(al2.dt), al2.dt) + 1 - al2.live_days                    AS unlive_days_t2,
 
-       IFNULL(al3.dt, '{month}' - INTERVAL 1 MONTH)                              AS dt_t1,
-       IFNULL(al3.live_days, 0)                                                  AS live_days_t1,
-       ROUND(IFNULL(al3.revenue_rmb, 0), 0)                                      AS revenue_rmb_t1,
-       DATEDIFF(LAST_DAY(al3.dt), al3.dt) + 1 - al3.live_days                    AS unlive_days_t1,
+       IFNULL(al1.dt, '{month}' - INTERVAL 1 MONTH)                              AS dt_t1,
+       IFNULL(al1.live_days, 0)                                                  AS live_days_t1,
+       ROUND(IFNULL(al1.revenue_rmb, 0), 0)                                      AS revenue_rmb_t1,
+       DATEDIFF(LAST_DAY(al1.dt), al3.dt) + 1 - al1.live_days                    AS unlive_days_t1,
 
-       IFNULL(al4.dt, '{month}')                                                 AS dt_t,
-       IFNULL(al4.live_days, 0)                                                  AS live_days_t,
-       ROUND(IFNULL(al4.revenue_rmb, 0), 0)                                      AS revenue_rmb_t,
+       IFNULL(al0.dt, '{month}')                                                 AS dt_t,
+       IFNULL(al0.live_days, 0)                                                  AS live_days_t,
+       ROUND(IFNULL(al0.revenue_rmb, 0), 0)                                      AS revenue_rmb_t,
        DATEDIFF(CASE
                     WHEN DATE_FORMAT('{cur_date}', '%Y-%m-01') = '{month}' THEN '{cur_date}' -- 判断是否当前月，当月数据以t-1计算
-                    ELSE LAST_DAY('{month}') END, '{month}') + 1 - al4.live_days AS unlive_days_t,
+                    ELSE LAST_DAY('{month}') END, '{month}') + 1 - al0.live_days AS unlive_days_t,
        CASE
            WHEN (DATEDIFF(CASE
                               WHEN DATE_FORMAT('{cur_date}', '%Y-%m-01') = '{month}' THEN '{cur_date}'
-                              ELSE LAST_DAY('{month}') END, '{month}') + 1 - al4.live_days) >= 10 THEN '开播异常'
+                              ELSE LAST_DAY('{month}') END, '{month}') + 1 - al0.live_days) >= 10 THEN '开播异常'
            ELSE '' END                                                           AS live_comment,
        aml.month_cnt                                                             AS month_cnt_t,
        ROUND(aml.revenue, 0)                                                     AS revenue_t
@@ -69,20 +69,20 @@ FROM (SELECT DISTINCT al.platform_name,
 --         AND al.contract_start_date >= al.dt
 --         AND al.contract_start_date < '{month}' + INTERVAL 1 MONTH
      ) al
-         LEFT JOIN warehouse.dw_paper_contract_anchor_month_live al1
-                   ON al.platform_name = al1.platform_name AND al.anchor_no = al1.anchor_no AND
-                      al1.dt = '{month}' - INTERVAL 3 MONTH
+         LEFT JOIN warehouse.dw_paper_contract_anchor_month_live al3
+                   ON al.platform_name = al3.platform_name AND al.anchor_no = al3.anchor_no AND
+                      al3.dt = '{month}' - INTERVAL 3 MONTH
          LEFT JOIN warehouse.dw_paper_contract_anchor_month_live al2
                    ON al.platform_name = al2.platform_name AND al.anchor_no = al2.anchor_no AND
                       al2.dt = '{month}' - INTERVAL 2 MONTH
-         LEFT JOIN warehouse.dw_paper_contract_anchor_month_live al3
-                   ON al.platform_name = al3.platform_name AND al.anchor_no = al3.anchor_no AND
-                      al3.dt = '{month}' - INTERVAL 1 MONTH
-         LEFT JOIN warehouse.dw_paper_contract_anchor_month_live al4
-                   ON al.platform_name = al4.platform_name AND al.anchor_no = al4.anchor_no AND
-                      al4.dt = '{month}'
+         LEFT JOIN warehouse.dw_paper_contract_anchor_month_live al1
+                   ON al.platform_name = al1.platform_name AND al.anchor_no = al1.anchor_no AND
+                      al1.dt = '{month}' - INTERVAL 1 MONTH
+         LEFT JOIN warehouse.dw_paper_contract_anchor_month_live al0
+                   ON al.platform_name = al0.platform_name AND al.anchor_no = al0.anchor_no AND
+                      al0.dt = '{month}'
          LEFT JOIN stage.stage_paper_contract_anchor_month_live aml
-                   ON al4.dt = aml.dt AND al4.platform_name = aml.platform_name AND aml.id_card = al.id_card AND
+                   ON al0.dt = aml.dt AND al0.platform_name = aml.platform_name AND aml.id_card = al.id_card AND
                       aml.anchor_no = al.anchor_no
 ;
 
