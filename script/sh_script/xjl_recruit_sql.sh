c4225dbd4114515a6e23b86e8fa8eee95970c7d3
Page <4061207@qq.com>
2020-03-12 19:07:09 +0800

    recruit day job shell

---
+++ script/sh_script/xjl_recruit_sql.sh
@@ -0,0 +1,2 @@
+/usr/bin/python /services/xjl_etl/script/py_script/run_sql.py  -f dw/dw_dy_xjl_day_recruit.sql
+/usr/bin/python /services/xjl_etl/script/py_script/run_sql.py  -f rpt/rpt_dy_xjl_day_recruit.sql
