cded54031aad235bddbadf509a47f8b91e09ccea
Page <4061207@qq.com>
2020-03-12 19:08:00 +0800

    shell add

--- script/sh_script/dy_xjl_sql.sh
+++ script/sh_script/dy_xjl_sql.sh
@@ -8,3 +8,9 @@
 
 /usr/bin/python /services/xjl_etl/script/py_script/run_sql.py -f rpt/rpt_dy_xjl_day.sql
 /usr/bin/python /services/xjl_etl/script/py_script/run_sql.py -f rpt/rpt_dy_xjl_month.sql
+
+
+
+
+/usr/bin/python /services/xjl_etl/script/py_script/run_sql.py  -f dw/dw_dy_xjl_day_recruit.sql
+/usr/bin/python /services/xjl_etl/script/py_script/run_sql.py  -f rpt/rpt_dy_xjl_day_recruit.sql
