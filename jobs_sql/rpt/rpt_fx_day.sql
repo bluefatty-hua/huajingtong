22c09caabfb341f0deb82da10bc70e7e3e74f847
Rockie_Yuen <rockie_yuen@outlook.com>
2020-03-16 11:02:44 +0800

    FX 添加主播数据

--- jobs_sql/rpt/rpt_fx_day.sql
+++ jobs_sql/rpt/rpt_fx_day.sql
@@ -314,3 +314,32 @@ FROM (SELECT dt,
         AND live_cnt > 0) t
 ;
 
+
+-- 主播数据 ---
+DELETE
+FROM bireport.rpt_day_fx_anchor
+WHERE dt BETWEEN '{start_date}' AND '{end_date}';
+INSERT INTO bireport.rpt_day_fx_anchor
+SELECT al.dt,
+       al.backend_account_id,
+       al.min_live_dt                   AS first_live_date,
+       al.min_sign_dt                   AS sign_date,
+       al.newold_state,
+       al1.duration / 3600              AS duration_lastmonth,
+       al1.live_days                    AS live_days_lastmonth,
+       al.active_state,
+       al1.revenue / 125               AS revenue_lastmonth,
+       al.revenue_level,
+       al.anchor_no                     AS anchor_uid,
+       al.anchor_no,
+       al.anchor_nick_name,
+       al.duration / 3600               AS duration,
+       IF(al.live_status = 1, '是', '否') AS live_status,
+       al.anchor_income / 125 / 0.4      AS revenue
+FROM warehouse.dw_fx_day_anchor_live al
+         LEFT JOIN warehouse.dw_fx_month_anchor_live al1
+                   ON al1.dt = DATE_FORMAT(al.dt - INTERVAL 1 MONTH, '%Y-%m-01') AND
+                      al.backend_account_id = al1.backend_account_id AND
+                      al.anchor_no = al1.anchor_no
+WHERE al.dt BETWEEN '{start_date}' AND '{end_date}'
+;
