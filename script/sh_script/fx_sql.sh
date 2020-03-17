94b37e19cf7fea1e1471f8696ca8dcc580800004
Rockie_Yuen <rockie_yuen@outlook.com>
2020-03-17 21:15:34 +0800

    FX del dw_fx_anchor_tags.sql

--- script/sh_script/fx_sql.sh
+++ script/sh_script/fx_sql.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 /usr/bin/python /services/xjl_etl/script/py_script/run_sql.py -f ods/ods_fx.sql
 
-/usr/bin/python /services/xjl_etl/script/py_script/run_sql.py -f dw/dw_fx_anchor_tags.sql
+# /usr/bin/python /services/xjl_etl/script/py_script/run_sql.py -f dw/delete_dw_fx_anchor_tags.sql
 
 /usr/bin/python /services/xjl_etl/script/py_script/run_sql.py -f dw/dw_fx_day.sql
 /usr/bin/python /services/xjl_etl/script/py_script/run_sql.py -f dw/dw_fx_month.sql
