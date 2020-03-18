7fa2c396c9ad51f96564ad5a93ccdf179b3465f1
Rockie_Yuen <rockie_yuen@outlook.com>
2020-03-18 21:18:12 +0800

    BB

--- script/sh_script/bb_sql.sh
+++ script/sh_script/bb_sql.sh
@@ -2,7 +2,7 @@
 /usr/bin/python /services/xjl_etl/script/py_script/run_sql.py -f ods/ods_bb.sql
 
 # /usr/bin/python /services/xjl_etl/script/py_script/run_sql.py -f dw/delete_dw_bb_anchor_tags.sql
-/usr/bin/python /services/xjl_etl/script/py_script/run_his_sql.py -f dw/dw_bb_day.sql
+/usr/bin/python /services/xjl_etl/script/py_script/run_sql.py -f dw/dw_bb_day.sql
 /usr/bin/python /services/xjl_etl/script/py_script/run_sql.py -f dw/dw_bb_month.sql
 
 /usr/bin/python /services/xjl_etl/script/py_script/run_sql.py -f rpt/rpt_bb_day.sql
