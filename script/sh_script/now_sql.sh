1b836fa38c20053a89fe68707b8bebe1d9a6bd03
Rockie_Yuen <rockie_yuen@outlook.com>
2020-03-18 14:50:34 +0800

    NOW UP

--- script/sh_script/now_sql.sh
+++ script/sh_script/now_sql.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 /usr/bin/python /services/xjl_etl/script/py_script/run_sql.py -f ods/ods_now.sql
 
-/usr/bin/python /services/xjl_etl/script/py_script/run_sql.py -f dw/dw_now_anchor_tags.sql
+# /usr/bin/python /services/xjl_etl/script/py_script/run_sql.py -f dw/delete_dw_now_anchor_tags.sql
 
 /usr/bin/python /services/xjl_etl/script/py_script/run_sql.py -f dw/dw_now_day.sql
 /usr/bin/python /services/xjl_etl/script/py_script/run_sql.py -f dw/dw_now_month.sql
