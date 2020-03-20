185950f00697fe96d38034c9f5d3b8f82f5047a7
Rockie_Yuen <rockie_yuen@outlook.com>
2020-03-20 14:34:17 +0800

    YY UP

--- script/sh_script/yy_sql.sh
+++ script/sh_script/yy_sql.sh
@@ -3,8 +3,8 @@
 
 # /usr/bin/python /services/xjl_etl/script/py_script/run_sql.py -f dw/delete_dw_yy_anchor_tags.sql
 
-/usr/bin/python /services/xjl_etl/script/py_script/run_his_sql.py -f dw/dw_yy_day.sql 
-/usr/bin/python /services/xjl_etl/script/py_script/run_his_sql.py -f dw/dw_yy_month.sql 
+/usr/bin/python /services/xjl_etl/script/py_script/run_sql.py -f dw/dw_yy_day.sql
+/usr/bin/python /services/xjl_etl/script/py_script/run_sql.py -f dw/dw_yy_month.sql
 
-/usr/bin/python /services/xjl_etl/script/py_script/run_his_sql.py -f rpt/rpt_yy_month.sql 
-/usr/bin/python /services/xjl_etl/script/py_script/run_his_sql.py -f rpt/rpt_yy_day.sql 
+/usr/bin/python /services/xjl_etl/script/py_script/run_sql.py -f rpt/rpt_yy_month.sql
+/usr/bin/python /services/xjl_etl/script/py_script/run_sql.py -f rpt/rpt_yy_day.sql
